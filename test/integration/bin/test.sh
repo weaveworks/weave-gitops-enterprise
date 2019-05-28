76b4d9bb99f9d5b3933232759ff3fe3810b60955
Michał Flendrich <michal@flendrich.pro>
2019-05-28 10:51:54 +0200

    clean up image-tag usage

--- test/integration/bin/test.sh
+++ test/integration/bin/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+IMGTAG=$(./tools/image-tag)
 
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD" quay.io
 export GOROOT=/home/circleci/go-${GOVERSION}
@@ -7,4 +8,4 @@ export PATH=~/go-${GOVERSION}/bin:$PATH
 # Work around for broken docker package in RHEL
 # See https://github.com/weaveworks/wks/issues/235
 export DOCKER_VERSION='1.13.1-75*'
-go test -failfast -v -timeout 1h ./test/integration -args -run.interactive -cmd /tmp/workspace/cmd/wksctl/wksctl -tags.wks-k8s-krb5-server=$(./tools/image-tag) -tags.wks-mock-authz-server=$(./tools/image-tag)
+go test -failfast -v -timeout 1h ./test/integration/test -args -run.interactive -cmd /tmp/workspace/cmd/wksctl/wksctl -tags.wks-k8s-krb5-server=$IMGTAG -tags.wks-mock-authz-server=$IMGTAG
