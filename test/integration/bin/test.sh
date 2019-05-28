f406685ab165973d98255020a59a31c01865c4f0
Michał Flendrich <michal@flendrich.pro>
2019-05-28 13:16:02 +0200

    call provision/destroy scripts directly, without docker
    remove the chown that's not necessary if we run docker executor instead of machine executor
    
    fix GOROOT

--- test/integration/bin/test.sh
+++ test/integration/bin/test.sh
@@ -3,11 +3,9 @@
 set -e
 
 # Install go
+export GOROOT=$HOME/go-$GOVERSION
 (cd ~ && curl -O https://dl.google.com/go/go${GOVERSION}.linux-amd64.tar.gz)
-mkdir ~/go-${GOVERSION} && tar xf ~/go${GOVERSION}.linux-amd64.tar.gz -C ~/go-${GOVERSION} --strip-components 1
-
-# Make circleci own /root (XXX)
-sudo chown -R circleci:circleci /root # Make the VM SSH keys readable to the circleci user
+mkdir ~/go-${GOVERSION} && tar xf ~/go${GOVERSION}.linux-amd64.tar.gz -C $GOROOT --strip-components 1
 
 # Initialise Kerberos
 KERBEROS_IP=$(jq -r '.public_ips.value[0]' /tmp/terraform_output.json)
@@ -20,8 +18,7 @@ sudo cp /tmp/workspace/kubectl /usr/bin/kubectl
 IMGTAG=$(./tools/image-tag)
 
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD" quay.io
-export GOROOT=/home/circleci/go-${GOVERSION}
-export PATH=~/go-${GOVERSION}/bin:$PATH
+export PATH=$GOROOT/bin:$PATH
 # Work around for broken docker package in RHEL
 # See https://github.com/weaveworks/wks/issues/235
 export DOCKER_VERSION='1.13.1-75*'
--- test/integration/bin/up.sh
+++ test/integration/bin/up.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
-docker run --env-file <(env|grep -E 'CIRCLE|SECRET|SRCDIR') -v /root:/root -v /tmp:/tmp -v /home/circleci:/home/circleci --entrypoint=$SRCDIR/test/integration/bin/provision_test_vms.sh quay.io/wks/build:latest
+$SRCDIR/test/integration/bin/provision_test_vms.sh
 
