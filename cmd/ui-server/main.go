f18625217a0ec996b29c3c7b55e4bdce19250824
Simon Howe <footless@gmail.com>
2019-09-02 10:18:24 +0200

    Adds list-branches endpoint to api server

--- cmd/ui-server/main.go
+++ cmd/ui-server/main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
+type paramSet struct {
+	port        int
+	privKeyFile string
+	gitURL      string
+}
+
 func main() {
+	var params paramSet
+	flag.IntVar(&params.port, "port", 80, "")
+	flag.StringVar(&params.privKeyFile, "git-private-key-file", "private-key", "")
+	flag.StringVar(&params.gitURL, "git-url", "git@github.com:fbarl/test-wkp.git", "")
+	flag.Parse()
+
+	// Serve up the branch list!
+	handleBranchesRequest, pollGitBranches := BranchesRequestHandler(params.gitURL, params.privKeyFile)
+	go pollGitBranches()
+	http.HandleFunc("/api/repo/branches", handleBranchesRequest)
+
 	// Serve the UI
 	fs := http.FileServer(http.Dir("html"))
 	http.Handle("/", http.StripPrefix("/", fs))
 
-	// TODO: Implement this as a part of https://github.com/weaveworks/wkp-ui/issues/175.
-	http.HandleFunc("/api/repo/branches", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-
-		fmt.Fprintf(w, `{"branches": ["master", "foo-branch"]}`)
-	})
-
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Println("Listening on", params.port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%d", params.port), nil))
 }
