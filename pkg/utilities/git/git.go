2ce5ca055c54f74dfa9fd2cc56dd5202478f3fcb
Jerry R. Jackson <jrryjcksn@gmail.com>
2019-10-22 09:13:18 -0600

    Semantic diff backend (#909)
    * Generate JSON diffs; fix identifiers for top-level objects; ensure that the diff correctly handles arrays'; improve tests
    
    * move diff and git operations to pkgs so we can use them externally
    
    * Add file diff functions and more diff testing
    
    * return before and after files along with the semantic diff

--- pkg/utilities/git/git.go
+++ pkg/utilities/git/git.go
@@ -1,11 +1,15 @@
 package git
 
 import (
+	"errors"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"time"
 
-	"github.com/pkg/errors"
+	gerrors "github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/weaveworks/wks/pkg/github/hub"
 	"github.com/weaveworks/wksctl/pkg/utilities/ssh"
@@ -18,7 +22,12 @@ import (
 const (
 	DefaultAuthor = "Weaveworks Kubernetes Platform"
 	DefaultEmail  = "support@weave.works"
+
+	ComponentsFileName  = "components.yaml"
+	GeneratorScriptName = "config.js"
 )
+
+type Set map[string]bool
 
 type GitRepo struct {
 	worktreeDir string
@@ -33,11 +42,11 @@ func (gr *GitRepo) WorktreeDir() string {
 func (gr *GitRepo) RemoteURL(name string) (string, error) {
 	remote, err := gr.repo.Remote(name)
 	if err != nil {
-		return "", errors.Wrap(err, "get remote 'origin'")
+		return "", gerrors.Wrap(err, "get remote 'origin'")
 	}
 	remoteCfg := remote.Config()
 	if len(remoteCfg.URLs) < 1 {
-		return "", errors.New("remote has no URLs")
+		return "", gerrors.New("remote has no URLs")
 	}
 	return remoteCfg.URLs[0], nil
 }
@@ -46,37 +55,37 @@ func NewGithubRepoToTempDir(parentDir, repoName string) (*GitRepo, *ssh.KeyPair,
 	log.Infof("Creating a temp directory...")
 	gitDir, err := ioutil.TempDir(parentDir, "git-")
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "TempDir")
+		return nil, nil, gerrors.Wrap(err, "TempDir")
 	}
 	log.Infof("Temp directory %q created.", gitDir)
 
 	log.Infof("Initializing an empty Git repository in %q...", gitDir)
 	repo, err := git.PlainInit(gitDir, false)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "git init")
+		return nil, nil, gerrors.Wrap(err, "git init")
 	}
 
 	log.Infof("Creating the GitHub repository %q...", repoName)
 	// XXX: hub.Create succeeds if the remote repo already exists.
 	if _, err := hub.Create(gitDir, true, repoName); err != nil {
-		return nil, nil, errors.Wrap(err, "create github repo")
+		return nil, nil, gerrors.Wrap(err, "create github repo")
 	}
 
 	bits := 4096
 	log.Infof("Generating a %d-bit SSH key pair...", bits)
 	keyPair, err := ssh.GenerateKeyPair(bits)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "ssh generate key pair")
+		return nil, nil, gerrors.Wrap(err, "ssh generate key pair")
 	}
 
 	log.Infof("Registering the SSH deploy key with the GitHub repository %q...", repoName)
 	if err := hub.RegisterDeployKey(repoName, "wkp-gitops-key", string(keyPair.PublicRSA), false); err != nil {
-		return nil, nil, errors.Wrap(err, "register deploy key")
+		return nil, nil, gerrors.Wrap(err, "register deploy key")
 	}
 
 	auth, err := gitssh.NewPublicKeys("git", keyPair.PrivatePEM, "")
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "private key read")
+		return nil, nil, gerrors.Wrap(err, "private key read")
 	}
 
 	gr := &GitRepo{
@@ -87,11 +96,11 @@ func NewGithubRepoToTempDir(parentDir, repoName string) (*GitRepo, *ssh.KeyPair,
 
 	log.Infof("Pushing an initial (empty) commit to the Git repository %q...", repoName)
 	if err := gr.CommitAll(DefaultAuthor, DefaultEmail, "initial commit"); err != nil {
-		return nil, nil, errors.Wrap(err, "initial commit")
+		return nil, nil, gerrors.Wrap(err, "initial commit")
 	}
 
 	if err := gr.Push(); err != nil {
-		return nil, nil, errors.Wrap(err, "git push")
+		return nil, nil, gerrors.Wrap(err, "git push")
 	}
 
 	return gr, keyPair, nil
@@ -101,14 +110,14 @@ func CloneToTempDir(parentDir, gitURL, branch string, privKey []byte) (*GitRepo,
 	log.Infof("Creating a temp directory...")
 	gitDir, err := ioutil.TempDir(parentDir, "git-")
 	if err != nil {
-		return nil, errors.Wrap(err, "TempDir")
+		return nil, gerrors.Wrap(err, "TempDir")
 	}
 	log.Infof("Temp directory %q created.", gitDir)
 
 	log.Infof("Cloning the Git repository %q to %q...", gitURL, gitDir)
 	auth, err := gitssh.NewPublicKeys("git", privKey, "")
 	if err != nil {
-		return nil, errors.Wrap(err, "private key read")
+		return nil, gerrors.Wrap(err, "private key read")
 	}
 
 	repo, err := git.PlainClone(gitDir, false, &git.CloneOptions{
@@ -121,7 +130,7 @@ func CloneToTempDir(parentDir, gitURL, branch string, privKey []byte) (*GitRepo,
 		Depth:        10,
 	})
 	if err != nil {
-		return nil, errors.Wrap(err, "git clone")
+		return nil, gerrors.Wrap(err, "git clone")
 	}
 
 	return &GitRepo{
@@ -134,11 +143,11 @@ func CloneToTempDir(parentDir, gitURL, branch string, privKey []byte) (*GitRepo,
 func (gr *GitRepo) CommitAll(name, email, msg string) error {
 	wt, err := gr.repo.Worktree()
 	if err != nil {
-		return errors.Wrap(err, "worktree")
+		return gerrors.Wrap(err, "worktree")
 	}
 
 	if _, err := wt.Add("."); err != nil {
-		return errors.Wrap(err, "git add")
+		return gerrors.Wrap(err, "git add")
 	}
 
 	co := git.CommitOptions{
@@ -149,7 +158,7 @@ func (gr *GitRepo) CommitAll(name, email, msg string) error {
 		},
 	}
 	if _, err := wt.Commit(msg, &co); err != nil {
-		return errors.Wrap(err, "commit")
+		return gerrors.Wrap(err, "commit")
 	}
 
 	return nil
@@ -159,12 +168,110 @@ func (gr *GitRepo) Push() error {
 	log.Infof("Pushing local commits to the Git remote...")
 	po := git.PushOptions{Auth: gr.auth}
 	if err := po.Validate(); err != nil {
-		return errors.Wrap(err, "push options validate")
+		return gerrors.Wrap(err, "push options validate")
 	}
-	return errors.Wrap(gr.repo.Push(&po), "git push")
+	return gerrors.Wrap(gr.repo.Push(&po), "git push")
 }
 
 func (gr *GitRepo) Close() error {
 	log.Infof("Deleting the temp directory %q...", gr.worktreeDir)
-	return errors.Wrapf(os.RemoveAll(gr.worktreeDir), "deleting directory %q", gr.worktreeDir)
+	return gerrors.Wrapf(os.RemoveAll(gr.worktreeDir), "deleting directory %q", gr.worktreeDir)
+}
+
+// Operations used in git actions for policy checking
+func MakeErrorWrapper(context string) func(error) error {
+	return func(err error) error {
+		return gerrors.Wrap(err, context)
+	}
+}
+
+func GetFileType(filename string) string {
+	return strings.TrimLeft(filepath.Ext(filename), ".")
+}
+
+func IsYAMLFile(filename string) bool {
+	ftype := GetFileType(filename)
+	return ftype == "yaml" || ftype == "yml"
+}
+
+func CheckExist(filePath, commit string) bool {
+	_, noExistErr := exec.Command("git", "cat-file", "-e", commit+":"+filePath).Output()
+	return noExistErr == nil
+}
+
+// FileLinesToSet splits command output containing file names at newlines and creates a set of the
+// file names
+func FileLinesToSet(lines []byte) Set {
+	setval := Set{}
+	for _, str := range strings.Split(string(lines), "\n") {
+		if strings.TrimSpace(str) != "" {
+			setval[str] = true
+		}
+	}
+	return setval
+}
+
+func ExecError(err error) error {
+	return errors.New(string(err.(*exec.ExitError).Stderr))
+}
+
+func GeneratorCmd(srcDir string) []string {
+	return []string{"jk", "generate", "-o", srcDir, "-f", filepath.Join(srcDir, ComponentsFileName),
+		filepath.Join(srcDir, GeneratorScriptName)}
+}
+
+func CopyRepoAtCommit(targetDir, repoDir, commit string) error {
+	if err := exec.Command("git", "clone", repoDir, targetDir).Run(); err != nil {
+		return err
+	}
+	if err := exec.Command("git", "-C", targetDir, "checkout", commit).Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ListTree(repoDir, commit string) (Set, error) {
+	wrap := MakeErrorWrapper("gitListTree")
+	empty := Set{}
+	existingDir, err := os.Getwd()
+	if err != nil {
+		return empty, wrap(err)
+	}
+	defer func() { os.Chdir(existingDir) }()
+	os.Chdir(repoDir)
+	out, err := exec.Command("git", "ls-tree", "--name-only", "-r", commit).Output()
+	if err != nil {
+		return empty, wrap(ExecError(err))
+	}
+	return FileLinesToSet(out), nil
+}
+
+func DiffTree(repoDir, oldCommit, newCommit string) (Set, error) {
+	log.Debug("Comparing git commits to find new and deleted files...")
+	wrap := MakeErrorWrapper("gitDiffTree")
+	empty := Set{}
+	out, err := exec.Command("git", "-C", repoDir, "diff-tree", "--no-commit-id", "--name-only", "-r", oldCommit, newCommit).Output()
+	if err != nil {
+		return empty, wrap(ExecError(err))
+	}
+	return FileLinesToSet(out), nil
+}
+
+func ReadFile(repoDir, filePath, commit string) ([]byte, error) {
+	log.Debugf("Reading file %q from git commit %q...", filePath, commit)
+	wrap := MakeErrorWrapper("gitReadFile")
+	existingDir, err := os.Getwd()
+	if err != nil {
+		return nil, wrap(err)
+	}
+	defer func() { os.Chdir(existingDir) }()
+	os.Chdir(repoDir)
+	data, err := exec.Command("git", "show", commit+":"+filePath).Output()
+	if err != nil {
+		if !CheckExist(filePath, commit) {
+			return []byte(""), nil
+		}
+		return nil, MakeErrorWrapper("gitReadFile")(ExecError(err))
+	}
+	return data, nil
 }
