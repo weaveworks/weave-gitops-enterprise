8a907c6fbdda7a7b43d45f14a97306549710f1ed
Simon Howe <footless@gmail.com>
2019-11-06 13:25:51 +0100

    Review feedback, goisms

--- cmd/gitops-repo-broker/main.go
+++ cmd/gitops-repo-broker/main.go
@@ -14,36 +14,42 @@ import (
 )
 
 var cmd = &cobra.Command{
-	Use:           "gitops-repo-broker",
-	Short:         "HTTP server for playing w/ git",
-	RunE:          runServer,
+	Use:   "gitops-repo-broker",
+	Short: "HTTP server for playing w/ git",
+	RunE: func(_ *cobra.Command, _ []string) error {
+		return runServer(globalParams)
+	},
 	SilenceUsage:  true,
 	SilenceErrors: true,
 }
 
 type paramSet struct {
-	privKeyFile string
-	gitURL      string
-	gitBranch   string
-	gitPath     string
+	privKeyFile      string
+	gitURL           string
+	gitBranch        string
+	gitPath          string
+	httpReadTimeout  time.Duration
+	httpWriteTimeout time.Duration
 }
 
-var params paramSet
+var globalParams paramSet
 
 func init() {
-	cmd.Flags().StringVar(&params.privKeyFile, "git-private-key-file", "", "Path to a SSH private key that is authorized for pull/push from/to the git repo specified by --git-url")
+	cmd.Flags().StringVar(&globalParams.privKeyFile, "git-private-key-file", "", "Path to a SSH private key that is authorized for pull/push from/to the git repo specified by --git-url")
 	cobra.MarkFlagRequired(cmd.Flags(), "private-key-file")
 
-	cmd.Flags().StringVar(&params.gitURL, "git-url", "", "Remote URL of the GitOps repository. Only the SSH protocol is supported. No HTTP/HTTPS.")
+	cmd.Flags().StringVar(&globalParams.gitURL, "git-url", "", "Remote URL of the GitOps repository. Only the SSH protocol is supported. No HTTP/HTTPS.")
 	cobra.MarkFlagRequired(cmd.Flags(), "git-url")
 
-	cmd.Flags().StringVar(&params.gitBranch, "git-branch", "master", "Branch that will be used by GitOps")
+	cmd.Flags().StringVar(&globalParams.gitBranch, "git-branch", "master", "Branch that will be used by GitOps")
 	cobra.MarkFlagRequired(cmd.Flags(), "git-branch")
 
-	cmd.Flags().StringVar(&params.gitPath, "git-path", "/", "Subdirectory of the GitOps repository where configuration as code can be found.")
+	cmd.Flags().StringVar(&globalParams.gitPath, "git-path", "/", "Subdirectory of the GitOps repository where configuration as code can be found.")
+	cmd.Flags().DurationVar(&globalParams.httpReadTimeout, "http-read-timeout", 30*time.Second, "ReadTimeout is the maximum duration for reading the entire request, including the body.")
+	cmd.Flags().DurationVar(&globalParams.httpWriteTimeout, "http-write-timeout", 30*time.Second, "WriteTimeout is the maximum duration before timing out writes of the response.")
 }
 
-func runServer(cmd *cobra.Command, args []string) error {
+func runServer(params paramSet) error {
 	privKey, err := ioutil.ReadFile(params.privKeyFile)
 	if err != nil {
 		return nil
@@ -64,8 +70,8 @@ func runServer(cmd *cobra.Command, args []string) error {
 		Handler: r,
 		Addr:    "0.0.0.0:8000",
 		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 30 * time.Second,
-		ReadTimeout:  30 * time.Second,
+		WriteTimeout: params.httpWriteTimeout,
+		ReadTimeout:  params.httpReadTimeout,
 	}
 
 	logrus.Info("Server listening...")
