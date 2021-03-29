eeaa9713723346f7395b160f7966b3df2ba751be
Sara El-Zayat <sara@weave.works>
2021-03-29 13:22:25 +0200

    [WKP-1996] Acceptance tests for Connection wizard field validation (#1787)
    * Validate cluster name input and acceptances tests
    
    - Assert cluster name validation cases
    - Assert button is disabled
    - Select save and next button
    - Assert max length
    - Select cluster name
    - Assert cluster name exceeding limit wasn't added
    
    * Print cluster name

--- test/acceptance/test/pages/cluster_connection_page.go
+++ test/acceptance/test/pages/cluster_connection_page.go
@@ -6,25 +6,27 @@ import (
 
 //ClusterConnectionPage elements
 type ClusterConnectionPage struct {
-	ClusterConnectionPopup *agouti.Selection
-	ClusterName            *agouti.Selection
-	ClusterIngressURL      *agouti.Selection
-	ClusterSaveAndNext     *agouti.Selection
-	ConnectionInstructions *agouti.Selection
-	ConnectionStatus       *agouti.Selection
-	ButtonClose            *agouti.Selection
+	ClusterConnectionPopup   *agouti.Selection
+	ClusterName              *agouti.Selection
+	ClusterIngressURL        *agouti.Selection
+	ClusterSaveAndNext       *agouti.Selection
+	ButtonClusterSaveAndNext *agouti.Selection
+	ConnectionInstructions   *agouti.Selection
+	ConnectionStatus         *agouti.Selection
+	ButtonClose              *agouti.Selection
 }
 
 //GetClusterConnectionPage initialises the webDriver object
 func GetClusterConnectionPage(webDriver *agouti.Page) *ClusterConnectionPage {
 	clusterConnPage := ClusterConnectionPage{
-		ClusterConnectionPopup: webDriver.Find(`#connection-popup`),
-		ClusterName:            webDriver.Find(`#Name-input`),
-		ClusterIngressURL:      webDriver.FindByXPath(`//*[@id="Ingress URL-input"]`),
-		ClusterSaveAndNext:     webDriver.FindByXPath(`//*[@id="connection-popup"]/div[2]/div/form/div[2]/button/span`),
-		ConnectionInstructions: webDriver.Find(`#instructions`),
-		ConnectionStatus:       webDriver.FindByXPath(`//*[@id="connection-popup"]/div[2]/div/form/div[1]/div/div[2]/div[2]`),
-		ButtonClose:            webDriver.Find(`#connection-popup button[type="submit"]`),
+		ClusterConnectionPopup:   webDriver.Find(`#connection-popup`),
+		ClusterName:              webDriver.Find(`#Name-input`),
+		ClusterIngressURL:        webDriver.FindByXPath(`//*[@id="Ingress URL-input"]`),
+		ClusterSaveAndNext:       webDriver.FindByXPath(`//*[@id="connection-popup"]/div[2]/div/form/div[2]/button/span`),
+		ButtonClusterSaveAndNext: webDriver.FindByXPath(`//*[@id="connection-popup"]/div[2]/div/form/div[2]/button`),
+		ConnectionInstructions:   webDriver.Find(`#instructions`),
+		ConnectionStatus:         webDriver.FindByXPath(`//*[@id="connection-popup"]/div[2]/div/form/div[1]/div/div[2]/div[2]`),
+		ButtonClose:              webDriver.Find(`#connection-popup button[type="submit"]`),
 	}
 
 	return &clusterConnPage
