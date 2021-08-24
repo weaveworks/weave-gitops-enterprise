2f631cce06fbd96ecd05a814be1ac53d7e698cc0
Simon Howe <footless@gmail.com>
2021-08-24 21:18:59 +0200

    Fixes path after rebase

--- common/database/utils/utils_test.go
+++ common/database/utils/utils_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/weaveworks/wks/common/database/models"
+	"github.com/weaveworks/weave-gitops-enterprise/common/database/models"
 	"gorm.io/gorm"
 )
 
