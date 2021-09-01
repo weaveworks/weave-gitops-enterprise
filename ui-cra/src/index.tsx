ed8d63e7f728957f9574d9bb275fe26abf1232fc
Simon <footless@gmail.com>
2021-09-01 20:03:56 +0200

    [WKP-2269] Adds server/ui version to the UI (#30)
    * Adds server/ui version to the UI
    
    * Fix ui linting
    
    * Moves help/version outside the white content boxes

--- ui-cra/src/index.tsx
+++ ui-cra/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log('weave-gitops-enterprise ui:', process.env.REACT_APP_VERSION);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
