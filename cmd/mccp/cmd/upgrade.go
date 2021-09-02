e857afd8b9d755529f751a3877a88513eed26832
Sara El-Zayat <st.elzayat@gmail.com>
2021-09-02 21:31:48 +0200

    Add add profile

--- cmd/mccp/cmd/upgrade.go
+++ cmd/mccp/cmd/upgrade.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
+	"github.com/weaveworks/weave-gitops-enterprise/cmd/mccp/pkg/upgrade"
 )
 
 func upgradeCmd(client *resty.Client) *cobra.Command {
@@ -10,9 +11,69 @@ func upgradeCmd(client *resty.Client) *cobra.Command {
 		Use:           "upgrade",
 		Short:         "Upgrade to WGE",
 		Example:       `mccp upgrade`,
+		RunE:          upgradeCmdRun(client),
 		SilenceUsage:  true,
 		SilenceErrors: true,
 	}
 
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.RepositoryURL, "pr-repo", "", "The repository to open a pull request against")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.Remote, "pr-remote", "origin", "The remote to push the branch to")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.BaseBranch, "pr-base", "main", "The base branch to open the pull request against")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.HeadBranch, "pr-branch", "", "The branch to create the pull request from")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.Title, "pr-title", "Upgrade to WGE", "The title of the pull request")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.Description, "pr-description", "PR to upgrade to WGE", "The description of the pull request")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.CommitMessage, "pr-commit-message", "Upgrade to WGE", "The commit message to use when adding the CAPI template")
+
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.Name, "name", "", "The commit message to use when adding the CAPI template")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.Namespace, "namespace", "default", "The namespace to use for generating resources")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.ProfileBranch, "profile-branch", "main", "The branch to use on the repository in which the profile is")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.ConfigMap, "configmap", "", "The name of the ConfigMap which contains values for this profile")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.Out, "out", ".", "Optional location to create the profile installation folder in. This should be relative to the current working directory")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.ProfileRepoURL, "profile-repo-url", "", "The URL of the repository that contains the profile to be added")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.ProfilePath, "profile-path", "", "The path to a profile when url is provided")
+	cmd.PersistentFlags().StringVar(&upgradeCmdFlags.GitRepository, "git-repository", "", "The namespace and name of the GitRepository object governing the flux repo")
+
 	return cmd
+}
+
+type upgradeFlags struct {
+	RepositoryURL  string
+	Remote         string
+	BaseBranch     string
+	HeadBranch     string
+	Title          string
+	Description    string
+	CommitMessage  string
+	Name           string
+	Namespace      string
+	ProfileBranch  string
+	ConfigMap      string
+	Out            string
+	ProfileRepoURL string
+	ProfilePath    string
+	GitRepository  string
+}
+
+var upgradeCmdFlags upgradeFlags
+
+func upgradeCmdRun(client *resty.Client) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		return upgrade.Upgrade(upgrade.UpgradeParams{
+			RepositoryURL:  upgradeCmdFlags.RepositoryURL,
+			Remote:         upgradeCmdFlags.Remote,
+			HeadBranch:     upgradeCmdFlags.HeadBranch,
+			BaseBranch:     upgradeCmdFlags.BaseBranch,
+			Title:          upgradeCmdFlags.Title,
+			Description:    upgradeCmdFlags.Description,
+			CommitMessage:  upgradeCmdFlags.CommitMessage,
+			Name:           upgradeCmdFlags.Name,
+			Namespace:      upgradeCmdFlags.Namespace,
+			ProfileBranch:  upgradeCmdFlags.ProfileBranch,
+			ConfigMap:      upgradeCmdFlags.ConfigMap,
+			Out:            upgradeCmdFlags.Out,
+			ProfileRepoURL: upgradeCmdFlags.ProfileRepoURL,
+			ProfilePath:    upgradeCmdFlags.ProfilePath,
+			GitRepository:  upgradeCmdFlags.GitRepository,
+		})
+	}
 }
--- cmd/mccp/pkg/upgrade/upgrade.go
+++ cmd/mccp/pkg/upgrade/upgrade.go
@@ -2,27 +2,153 @@ package upgrade
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/weaveworks/pctl/pkg/bootstrap"
 	"github.com/weaveworks/pctl/pkg/catalog"
+	"github.com/weaveworks/pctl/pkg/client"
 	"github.com/weaveworks/pctl/pkg/git"
+	"github.com/weaveworks/pctl/pkg/install"
 	"github.com/weaveworks/pctl/pkg/runner"
 )
 
-func Upgrade() error {
-
+type UpgradeParams struct {
+	RepositoryURL  string
+	Remote         string
+	HeadBranch     string
+	BaseBranch     string
+	Title          string
+	Description    string
+	CommitMessage  string
+	Name           string
+	Namespace      string
+	ProfileBranch  string
+	ConfigMap      string
+	Out            string
+	ProfileRepoURL string
+	ProfilePath    string
+	GitRepository  string
 }
 
-func removeProfile() {}
+func Upgrade(params UpgradeParams) error {
+	installationDirectory, err := addProfile(params)
+	if err != nil {
+		return err
+	}
 
-func addProfile() {}
+	if err := createPullRequest(params, installationDirectory); err != nil {
+		return err
 
-func createPullRequest(installationDirectory string) error {
-	branch := ""
-	repo := ""
-	base := ""
-	remote := ""
-	directory := ""
-	message := ""
+	}
+	return nil
+}
+
+func removeProfile() error {
+	return nil
+}
+
+func addProfile(params UpgradeParams) (string, error) {
+	var (
+		err           error
+		catalogClient *client.Client
+		profilePath   string
+		catalogName   string
+		profileName   string
+		version       = "latest"
+	)
+
+	url := params.ProfileRepoURL
+	branch := params.ProfileBranch
+	subName := params.Name
+	namespace := params.Namespace
+	configMap := params.ConfigMap
+	dir := params.Out
+	path := params.ProfilePath
+	message := params.CommitMessage
+
+	parts := strings.Split(profilePath, "/")
+
+	if len(parts) == 3 {
+		version = parts[2]
+	}
+	catalogName, profileName = parts[0], parts[1]
+
+	r := &runner.CLIRunner{}
+	g := git.NewCLIGit(git.CLIGitConfig{
+		Message: message,
+	}, r)
+
+	gitRepoNamespace, gitRepoName, err := getGitRepositoryNamespaceAndName(params.GitRepository)
+	if err != nil {
+		return "", err
+	}
+
+	installationDirectory := filepath.Join(dir, subName)
+	installer := install.NewInstaller(install.Config{
+		GitClient:        g,
+		RootDir:          installationDirectory,
+		GitRepoNamespace: gitRepoNamespace,
+		GitRepoName:      gitRepoName,
+	})
+
+	cfg := catalog.InstallConfig{
+		Clients: catalog.Clients{
+			CatalogClient: catalogClient,
+			Installer:     installer,
+		},
+		Profile: catalog.Profile{
+			ProfileConfig: catalog.ProfileConfig{
+				CatalogName:   catalogName,
+				ConfigMap:     configMap,
+				Namespace:     namespace,
+				Path:          path,
+				ProfileBranch: branch,
+				ProfileName:   profileName,
+				SubName:       subName,
+				URL:           url,
+				Version:       version,
+			},
+			GitRepoConfig: catalog.GitRepoConfig{
+				Namespace: gitRepoNamespace,
+				Name:      gitRepoName,
+			},
+		},
+	}
+	manager := &catalog.Manager{}
+	err = manager.Install(cfg)
+
+	return installationDirectory, err
+}
+
+func getGitRepositoryNamespaceAndName(gitRepository string) (string, string, error) {
+	if gitRepository != "" {
+		split := strings.Split(gitRepository, "/")
+		if len(split) != 2 {
+			return "", "", fmt.Errorf("git-repository must in format <namespace>/<name>; was: %s", gitRepository)
+		}
+		return split[0], split[1], nil
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", "", fmt.Errorf("failed to fetch current working directory: %w", err)
+	}
+	config, err := bootstrap.GetConfig(wd)
+	if err == nil && config != nil {
+		return config.GitRepository.Namespace, config.GitRepository.Name, nil
+	}
+	return "", "", fmt.Errorf("flux git repository not provided, please provide the --git-repository flag or use the pctl bootstrap functionality")
+}
+
+func createPullRequest(params UpgradeParams, installationDirectory string) error {
+	branch := params.HeadBranch
+	repo := params.RepositoryURL
+	base := params.BaseBranch
+	remote := params.Remote
+	directory := params.Out
+	message := params.CommitMessage
 
 	r := &runner.CLIRunner{}
 	g := git.NewCLIGit(git.CLIGitConfig{
