9a0dc6ff50c3ac5034b530bf89f24faaa25c3dee
Yiannis <yiannis@weave.works>
2021-09-04 12:45:48 +0100

    Create PR for new entitlement

--- cmd/entitlements/cmd/generate.go
+++ cmd/entitlements/cmd/generate.go
@@ -1,13 +1,16 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/weaveworks/weave-gitops-enterprise/pkg/entitlement"
-
+	"github.com/fluxcd/go-git-providers/gitprovider"
 	"github.com/spf13/cobra"
 	"github.com/weaveworks/libgitops/pkg/serializer"
+	"github.com/weaveworks/weave-gitops-enterprise/cmd/capi-server/pkg/git"
+	"github.com/weaveworks/weave-gitops-enterprise/pkg/entitlement"
 	"k8s.io/kubectl/pkg/scheme"
 )
 
@@ -23,20 +26,51 @@ var generateCmd = &cobra.Command{
 		token, err := entitlement.CreateEntitlement(privateKeyFilename, durationYears, durationMonths, durationDays, customerEmail)
 		cobra.CheckErr(err)
 		secret := entitlement.WrapEntitlementInSecret(token, secretName, secretNamespace)
+
+		var buf bytes.Buffer
 		s := serializer.NewSerializer(scheme.Scheme, nil)
-		fw := serializer.NewYAMLFrameWriter(os.Stdout)
+		fw := serializer.NewYAMLFrameWriter(&buf)
 		s.Encoder().Encode(fw, secret)
+
+		gs := git.NewGitProviderService()
+
+		path := fmt.Sprintf("entitlements/%s.yaml", customerEmail)
+		content := buf.String()
+		res, err := gs.WriteFilesToBranchAndCreatePullRequest(cmd.Context(), git.WriteFilesToBranchAndCreatePullRequestRequest{
+			GitProvider: git.GitProvider{
+				Token:    githubToken,
+				Type:     "github",
+				Hostname: "github.com",
+			},
+			RepositoryURL: githubRepo,
+			HeadBranch:    customerEmail,
+			BaseBranch:    githubRepoBaseBranch,
+			Title:         fmt.Sprintf("New entitlement for %s", customerEmail),
+			Description:   fmt.Sprintf("Adds a new entitlement for %s", customerEmail),
+			CommitMessage: fmt.Sprintf("Add new entitlement for %s", customerEmail),
+			Files: []gitprovider.CommitFile{
+				{
+					Path:    &path,
+					Content: &content,
+				},
+			},
+		})
+		cobra.CheckErr(err)
+		fmt.Printf("New entitlement PR created at: %s\n", res.WebURL)
 	},
 }
 
 var (
-	privateKeyFilename string
-	customerEmail      string
-	durationYears      int
-	durationMonths     int
-	durationDays       int
-	secretName         string
-	secretNamespace    string
+	privateKeyFilename   string
+	customerEmail        string
+	durationYears        int
+	durationMonths       int
+	durationDays         int
+	secretName           string
+	secretNamespace      string
+	githubToken          string
+	githubRepo           string
+	githubRepoBaseBranch string
 )
 
 func init() {
@@ -51,4 +85,7 @@ func init() {
 	generateCmd.Flags().IntVarP(&durationDays, "duration-days", "d", 0, "Number of days to use for the duration of the entitlement")
 	generateCmd.Flags().StringVarP(&secretName, "name", "n", "wego-ee-entitlement", "The name of the secret")
 	generateCmd.Flags().StringVar(&secretNamespace, "namespace", "default", "The namespace of the secret")
+	generateCmd.Flags().StringVar(&githubToken, "github-token", os.Getenv("GITHUB_TOKEN"), "The GitHub token to use to create a PR that adds the generated entitlement to the entitlements repository")
+	generateCmd.Flags().StringVar(&githubRepo, "github-repo", "https://github.com/weaveworks/weave-gitops-enterprise-entitlements", "The GitHub entitlements repository")
+	generateCmd.Flags().StringVar(&githubRepoBaseBranch, "github-repo-base-branch", "main", "The GitHub entitlements repository base branch")
 }
--- cmd/entitlements/main.go
+++ cmd/entitlements/main.go
@@ -1,6 +1,21 @@
 package main
 
-import "github.com/weaveworks/weave-gitops-enterprise/cmd/entitlements/cmd"
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/weaveworks/weave-gitops-enterprise/cmd/entitlements/cmd"
+)
+
+func init() {
+	if os.Getenv("LOG_LEVEL") == "DEBUG" {
+		// Only log the warning severity or above.
+		log.SetLevel(log.DebugLevel)
+	} else if os.Getenv("LOG_LEVEL") == "WARN" || os.Getenv("LOG_LEVEL") == "" {
+		// Only log the warning severity or above.
+		log.SetLevel(log.WarnLevel)
+	}
+}
 
 func main() {
 	cmd.Execute()
