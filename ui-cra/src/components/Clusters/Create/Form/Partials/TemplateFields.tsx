f089252acf39d6cc61c0e3626cf7781be6da089b
AlinaGoaga <goaga.alina@gmail.com>
2021-11-19 15:37:26 +0000

    WG273 Update form styling - updated

--- ui-cra/src/components/Clusters/Create/Form/Partials/TemplateFields.tsx
+++ ui-cra/src/components/Clusters/Create/Form/Partials/TemplateFields.tsx
@@ -32,8 +32,10 @@ const FormWrapper = styled(Form)`
   .form-group {
     padding-top: ${base};
     .step-child {
+      // btn-class should kick in only if the first child is clicked as we need all the children to be on the page
       .step-child-btn-CLUSTER_NAME {
         color: red;
+        // display: none;
       }
     }
   }
