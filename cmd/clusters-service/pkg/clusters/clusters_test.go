1a49c7b31205f86c330b7f18aa5636e8e023bc07
Sara El-Zayat <sara@weave.works>
2022-03-22 19:01:27 +0200

    Add makeTestCluster

--- cmd/clusters-service/pkg/clusters/clusters_test.go
+++ cmd/clusters-service/pkg/clusters/clusters_test.go
@@ -2,37 +2,48 @@ package clusters
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/go-logr/logr"
 	"github.com/google/go-cmp/cmp"
 	capiv1 "github.com/weaveworks/weave-gitops-enterprise/cmd/clusters-service/api/v1alpha1"
 	"github.com/weaveworks/weave-gitops/pkg/kube/kubefakes"
-	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-const testdata1 = `
-apiVersion: capi.weave.works/v1alpha1
-kind: WeaveCluster
-metadata:
-  name: weave-cluster
-spec:
-  label: foo
-`
+func TestGetClusterFromCRDs(t *testing.T) {
+	c1 := makeTestCluster("weave-cluster", "foo")
+	c2 := makeTestCluster("weave-cluster2", "bar")
+	lib := CRDLibrary{Log: logr.Discard(), ClientGetter: kubefakes.NewFakeClientGetter(makeClient(t, c1, c2))}
+	result, err := lib.Get(context.Background(), "weave-cluster2")
+	if err != nil {
+		t.Fatalf("On no, error: %v", err)
+	}
+	if diff := cmp.Diff(c2, result); diff != "" {
+		t.Fatalf("On no, diff clusters: %v", diff)
+	}
+}
 
-const testdata2 = `
-apiVersion: capi.weave.works/v1alpha1
-kind: WeaveCluster
-metadata:
-  name: weave-cluster2
-spec:
-  label: bar
-`
+func TestListClusterFromCRDs(t *testing.T) {
+	c1 := makeTestCluster("weave-cluster", "foo")
+	c2 := makeTestCluster("weave-cluster2", "bar")
+	lib := CRDLibrary{Log: logr.Discard(), ClientGetter: kubefakes.NewFakeClientGetter(makeClient(t, c1, c2))}
+	result, err := lib.List(context.Background())
+	if err != nil {
+		t.Fatalf("On no, error: %v", err)
+	}
+	want := map[string]*capiv1.WeaveCluster{
+		"weave-cluster":  c1,
+		"weave-cluster2": c2,
+	}
+	if diff := cmp.Diff(want, result); diff != "" {
+		t.Fatalf("On no, diff clusters: %v", diff)
+	}
+}
 
 func makeClient(t *testing.T, clusterState ...runtime.Object) client.Client {
 	scheme := runtime.NewScheme()
@@ -51,44 +62,21 @@ func makeClient(t *testing.T, clusterState ...runtime.Object) client.Client {
 		Build()
 }
 
-func TestGetClusterFromCRDs(t *testing.T) {
-	c1 := mustParseCluster(t, testdata1)
-	c2 := mustParseCluster(t, testdata2)
-	lib := CRDLibrary{Log: logr.Discard(), ClientGetter: kubefakes.NewFakeClientGetter(makeClient(t, c1, c2))}
-	result, err := lib.Get(context.Background(), "weave-cluster2")
-	if err != nil {
-		t.Fatalf("On no, error: %v", err)
+func makeTestCluster(name, label string, opts ...func(*capiv1.WeaveCluster)) *capiv1.WeaveCluster {
+	c := &capiv1.WeaveCluster{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "capi.weave.works/v1alpha1",
+			Kind:       "WeaveCluster",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: capiv1.WeaveClusterSpec{
+			Label: label,
+		},
 	}
-	if diff := cmp.Diff(c2, result); diff != "" {
-		t.Fatalf("On no, diff clusters: %v", diff)
+	for _, o := range opts {
+		o(c)
 	}
-}
-
-func TestListClusterFromCRDs(t *testing.T) {
-	c1 := mustParseCluster(t, testdata1)
-	c2 := mustParseCluster(t, testdata2)
-	lib := CRDLibrary{Log: logr.Discard(), ClientGetter: kubefakes.NewFakeClientGetter(makeClient(t, c1, c2))}
-	result, err := lib.List(context.Background())
-	if err != nil {
-		t.Fatalf("On no, error: %v", err)
-	}
-	want := map[string]*capiv1.WeaveCluster{
-		"weave-cluster":  c1,
-		"weave-cluster2": c2,
-	}
-	if diff := cmp.Diff(want, result); diff != "" {
-		t.Fatalf("On no, diff clusters: %v", diff)
-	}
-}
-
-func mustParseCluster(t *testing.T, data string) *capiv1.WeaveCluster {
-	t.Helper()
-
-	var c capiv1.WeaveCluster
-	err := yaml.Unmarshal([]byte(data), &c)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println(c)
-	return &c
+	return c
 }
