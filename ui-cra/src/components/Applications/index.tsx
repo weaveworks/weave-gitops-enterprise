9063ec40a18c33a5b36eeb69ef7c4821a81c4647
Robin Sonefors <robin.sonefors@weave.works>
2022-04-04 13:44:09 +0100

    chart: Remove flux CLI from clusters-service

--- ui-cra/src/components/Applications/index.tsx
+++ ui-cra/src/components/Applications/index.tsx
@@ -3,7 +3,7 @@ import { PageTemplate } from '../Layout/PageTemplate';
 import { SectionHeader } from '../Layout/SectionHeader';
 import { ContentWrapper } from '../Layout/ContentWrapper';
 import { useApplicationsCount } from './utils';
-import { AutomationsTable, useListAutomations, Automation } from '@weaveworks/weave-gitops';
+import { AutomationsTable, useListAutomations } from '@weaveworks/weave-gitops';
 
 const WGApplicationsDashboard: FC = () => {
   const { data: automations } = useListAutomations();
