891a3aa18b6482382488cc2185807d866c5c05cf
Simon Howe <footless@gmail.com>
2022-04-11 08:22:20 +0200

    Explore login UI timing issue in CI

--- test/acceptance/test/utils_login.go
+++ test/acceptance/test/utils_login.go
@@ -40,7 +40,7 @@ func initUserCredentials() UserCredentials {
 func loginUser() {
 	loginPage := pages.GetLoginPage(webDriver)
 
-	if pages.ElementExist(loginPage.LoginOIDC, 10) {
+	if pages.ElementExist(loginPage.LoginOIDC, 30) {
 		Eventually(loginPage.LoginOIDC).Should(BeVisible())
 
 		switch userCredentials.UserType {
