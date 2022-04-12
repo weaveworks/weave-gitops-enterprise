6a087bb556fd2b1bc90eedf3a56d32d98fc0cf50
Simon Howe <footless@gmail.com>
2022-04-12 19:48:17 +0200

    Restore util helpers
    - ui_applications was deleted, but I have it locally still, confused

--- test/acceptance/test/utils_login.go
+++ test/acceptance/test/utils_login.go
@@ -2,9 +2,11 @@ package acceptance
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fluxcd/go-git-providers/gitlab"
 	. "github.com/onsi/gomega"
+	"github.com/sclevine/agouti"
 	. "github.com/sclevine/agouti/matchers"
 	"github.com/weaveworks/weave-gitops-enterprise/test/acceptance/test/pages"
 )
@@ -112,4 +114,120 @@ func logoutUser() {
 		account := pages.GetAccount(webDriver)
 		Eventually(account.Logout.Click).Should(Succeed())
 	}
+}
+
+func AuthenticateWithGitProvider(webDriver *agouti.Page, gitProvider, gitProviderHostname string) {
+	if gitProvider == GitProviderGitHub {
+		authenticate := pages.AuthenticateWithGithub(webDriver)
+
+		if pages.ElementExist(authenticate.AuthenticateGithub) {
+			Expect(authenticate.AuthenticateGithub.Click()).To(Succeed())
+			AuthenticateWithGitHub(webDriver)
+
+			// Sometimes authentication failed to get the github device code, it may require revalidation with new access code
+			if pages.ElementExist(authenticate.AuthorizationError) {
+				logger.Info("Error getting github device code, requires revalidating...")
+				Expect(authenticate.Close.Click()).To(Succeed())
+				Eventually(authenticate.AuthenticateGithub.Click).Should(Succeed())
+				AuthenticateWithGitHub(webDriver)
+			}
+
+			Eventually(authenticate.AuthroizeButton).ShouldNot(BeFound())
+		}
+	} else if gitProvider == GitProviderGitLab {
+		var authenticate *pages.AuthenticateGitlab
+		if gitProviderHostname == gitlab.DefaultDomain {
+			authenticate = pages.AuthenticateWithGitlab(webDriver)
+		} else {
+			authenticate = pages.AuthenticateWithOnPremGitlab(webDriver)
+		}
+
+		if pages.ElementExist(authenticate.AuthenticateGitlab) {
+			Expect(authenticate.AuthenticateGitlab.Click()).To(Succeed())
+
+			if !pages.ElementExist(authenticate.Username) {
+				if pages.ElementExist(authenticate.CheckBrowser) {
+					setGitlabBrowserCompatibility(webDriver)
+					Eventually(authenticate.CheckBrowser, ASSERTION_30SECONDS_TIME_OUT).ShouldNot(BeFound())
+					TakeScreenShot("gitlab_browser_compatibility")
+				}
+
+				if pages.ElementExist(authenticate.AcceptCookies, 10) {
+					Eventually(authenticate.AcceptCookies.Click).Should(Succeed())
+				}
+			}
+
+			TakeScreenShot("gitlab_cookies_accepted")
+			if pages.ElementExist(authenticate.Username) {
+				Eventually(authenticate.Username).Should(BeVisible())
+				Expect(authenticate.Username.SendKeys(gitProviderEnv.Username)).To(Succeed())
+				Expect(authenticate.Password.SendKeys(gitProviderEnv.Password)).To(Succeed())
+				Expect(authenticate.Signin.Submit()).To(Succeed())
+			} else {
+				logger.Info("Login not found, assuming already logged in")
+			}
+
+			if pages.ElementExist(authenticate.Authorize) {
+				Expect(authenticate.Authorize.Click()).To(Succeed())
+			}
+		}
+	}
+}
+
+func setGitlabBrowserCompatibility(webDriver *agouti.Page) {
+	// opening the gitlab in a separate window not controlled by webdriver seems to redirect gitlab to login
+	pages.OpenNewWindow(webDriver, `http://`+gitProviderEnv.Hostname+`/users/sign_in`, "gitlab")
+	// Make sure weave-gitops-enterprise application window is still active window
+	Expect(webDriver.SwitchToWindow(WGE_WINDOW_NAME)).ShouldNot(HaveOccurred(), "Failed to switch to wego application window")
+}
+
+func AuthenticateWithGitHub(webDriver *agouti.Page) {
+
+	authenticate := pages.AuthenticateWithGithub(webDriver)
+
+	Eventually(authenticate.AccessCode).Should(BeVisible())
+	accessCode, _ := authenticate.AccessCode.Text()
+	Expect(authenticate.AuthroizeButton.Click()).To(Succeed())
+	accessCode = strings.Replace(accessCode, "-", "", 1)
+	logger.Info(accessCode)
+
+	// Move to device activation window
+	TakeScreenShot("application_authentication")
+	Expect(webDriver.NextWindow()).ShouldNot(HaveOccurred(), "Failed to switch to github authentication window")
+	TakeScreenShot("github_authentication")
+
+	activate := pages.ActivateDeviceGithub(webDriver)
+
+	if pages.ElementExist(activate.Username) {
+		Eventually(activate.Username).Should(BeVisible())
+		Expect(activate.Username.SendKeys(gitProviderEnv.Username)).To(Succeed())
+		Expect(activate.Password.SendKeys(gitProviderEnv.Password)).To(Succeed())
+		Expect(activate.Signin.Click()).To(Succeed())
+	} else {
+		logger.Info("Login not found, assuming already logged in")
+		TakeScreenShot("login_skipped")
+	}
+
+	if pages.ElementExist(activate.AuthCode) {
+		Eventually(activate.AuthCode).Should(BeVisible())
+		// Generate 6 digit authentication OTP for MFA
+		authCode, _ := runCommandAndReturnStringOutput("totp-cli instant")
+		Expect(activate.AuthCode.SendKeys(authCode)).To(Succeed())
+	} else {
+		logger.Info("OTP not found, assuming already logged in")
+		TakeScreenShot("otp_skipped")
+	}
+
+	Eventually(activate.Continue).Should(BeVisible())
+	Expect(activate.UserCode.At(0).SendKeys(accessCode)).To(Succeed())
+	Expect(activate.Continue.Click()).To(Succeed())
+
+	Eventually(activate.AuthroizeWeaveworks).Should(BeEnabled())
+	Expect(activate.AuthroizeWeaveworks.Click()).To(Succeed())
+
+	Eventually(activate.ConnectedMessage).Should(BeVisible())
+	Expect(webDriver.CloseWindow()).ShouldNot(HaveOccurred())
+
+	// Device is connected, now move back to application window
+	Expect(webDriver.SwitchToWindow(WGE_WINDOW_NAME)).ShouldNot(HaveOccurred(), "Failed to switch to wego application window")
 }
