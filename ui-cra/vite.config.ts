61782516c4c8f88bd8d40e86aeeeada639baa010
Simon Howe <footless@gmail.com>
2022-04-15 16:08:59 +0200

    vite again

--- ui-cra/vite.config.ts
+++ ui-cra/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 
 const DEFAULT_PROXY_HOST = 'http://34.67.250.163:30080/';
