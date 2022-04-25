1bd00f3b2e7da58f3f178b69b2865555a6b98fab
Claudia <claudiaberesford@gmail.com>
2022-04-25 13:36:06 +0100

    feat: Add MultiClusterFetcher (#654)
    This commit adds a component which fulfills the Fetcher interface
    defined in Core[1].
    
    Likely this is an initial implementation. Since the code is very similar
    to what the cluster-controller is doing, I did consider building an
    in-memory cache there and then exposing it via an internal service, but
    held off until we know what we want to do around efficiency here.
    Querying the secrets and building the list with every call of `Fetch` may
    not be what we want long-term. As it stands the caller of `Fetch` in
    core will be caching the results (with a TTL), so the situation is not
    too dire.
    
    I did consider wiring this Fetcher to use a CRDLibrary[2] to List
    clusters, but held off initially as, a) it did not save me anything
    (indeed I do not need a map) and b) as illustrated above we may not want to
    keep all of this mechanism here.
    
    I have not wired in this Fetcher in the server yet, as there are some
    final things which need to be done in core. Once those are merged and
    the version of Core is bumped here I will PR to add in the wiring.
    
    [1] https://github.com/weaveworks/weave-gitops/blob/main/core/clustersmngr/clustersmngr.go#L52-L56
    [2] https://github.com/weaveworks/weave-gitops-enterprise/blob/main/cmd/clusters-service/pkg/clusters/clusters.go#L29-L33

---
+++ pkg/cluster/fetcher/multi.go
@@ -0,0 +1,132 @@
+package fetcher
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	gitopsv1alpha1 "github.com/weaveworks/cluster-controller/api/v1alpha1"
+	mngr "github.com/weaveworks/weave-gitops/core/clustersmngr"
+	"github.com/weaveworks/weave-gitops/pkg/kube"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	dataKey     = "value"
+	yamlDataKey = "value.yaml"
+)
+
+type multiClusterFetcher struct {
+	log          logr.Logger
+	cfg          *rest.Config
+	clientGetter kube.ClientGetter
+	namespace    string
+}
+
+func NewMultiClusterFetcher(log logr.Logger, config *rest.Config, cg kube.ClientGetter, namespace string) (mngr.ClusterFetcher, error) {
+	return multiClusterFetcher{
+		log:          log.WithName("multi-cluster-fetcher"),
+		cfg:          config,
+		clientGetter: cg,
+		namespace:    namespace,
+	}, nil
+}
+
+func (f multiClusterFetcher) Fetch(ctx context.Context) ([]mngr.Cluster, error) {
+	clusters := []mngr.Cluster{f.self()}
+
+	res, err := f.leafClusters(ctx)
+	if err != nil {
+		f.log.Error(err, "unable to collect GitOps Clusters")
+		return clusters, nil
+	}
+
+	return append(clusters, res...), nil
+}
+
+func (f *multiClusterFetcher) self() mngr.Cluster {
+	return mngr.Cluster{
+		Name:        mngr.DefaultCluster,
+		Server:      f.cfg.Host,
+		BearerToken: f.cfg.BearerToken,
+		TLSConfig:   f.cfg.TLSClientConfig,
+	}
+}
+
+func (f multiClusterFetcher) leafClusters(ctx context.Context) ([]mngr.Cluster, error) {
+	clusters := []mngr.Cluster{}
+
+	cl, err := f.clientGetter.Client(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	goClusters := &gitopsv1alpha1.GitopsClusterList{}
+
+	if err := cl.List(ctx, goClusters, client.InNamespace(f.namespace)); err != nil {
+		return nil, err
+	}
+
+	for _, cluster := range goClusters.Items {
+		var secretRef string
+
+		if cluster.Spec.CAPIClusterRef != nil {
+			secretRef = fmt.Sprintf("%s-kubeconfig", cluster.Spec.CAPIClusterRef.Name)
+		}
+
+		if secretRef == "" && cluster.Spec.SecretRef != nil {
+			secretRef = cluster.Spec.SecretRef.Name
+		}
+
+		if secretRef == "" {
+			continue
+		}
+
+		key := types.NamespacedName{
+			Name:      secretRef,
+			Namespace: cluster.Namespace,
+		}
+
+		var secret v1.Secret
+		if err := cl.Get(ctx, key, &secret); err != nil {
+			f.log.Error(err, "unable to fetch secret for GitOps Cluster", "cluster", cluster.Name)
+
+			continue
+		}
+
+		var data []byte
+
+		for k := range secret.Data {
+			if k == dataKey || k == yamlDataKey {
+				data = secret.Data[k]
+
+				break
+			}
+		}
+
+		if len(data) == 0 {
+			continue
+		}
+
+		restCfg, err := clientcmd.RESTConfigFromKubeConfig([]byte(data))
+		if err != nil {
+			f.log.Error(err, "unable to create kubconfig from GitOps Cluster secret data", "cluster", cluster.Name)
+
+			continue
+		}
+
+		clusters = append(clusters,
+			mngr.Cluster{
+				Name:        cluster.Name,
+				Server:      restCfg.Host,
+				BearerToken: restCfg.BearerToken,
+				TLSConfig:   restCfg.TLSClientConfig,
+			})
+	}
+
+	return clusters, nil
+}
