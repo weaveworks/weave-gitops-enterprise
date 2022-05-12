0dce7c2bf29f27b10a3696ebae4bef7c0f0eeb0d
AlinaGoaga <goaga.alina@gmail.com>
2022-05-12 14:42:19 +0100

    Cleanup - 2

--- test/acceptance/test/pages/clusters_page.go
+++ test/acceptance/test/pages/clusters_page.go
@@ -2,7 +2,6 @@ package pages
 
 import (
 	"fmt"
-	"strings"
 
 	. "github.com/onsi/gomega"
 	"github.com/sclevine/agouti"
@@ -13,9 +12,7 @@ type ClusterInformation struct {
 	Checkbox         *agouti.Selection
 	ShowStatusDetail *agouti.Selection
 	Name             *agouti.Selection
-	// Icon             *agouti.Selection
 	Status *agouti.Selection
-	// EditCluster      *agouti.Selection
 }
 
 type ClusterStatus struct {
@@ -33,22 +30,11 @@ type DeletePullRequestPopup struct {
 	GitCredentials      *agouti.Selection
 }
 
-type AlertInformation struct {
-	Severity    *agouti.Selection
-	Message     *agouti.Selection
-	ClusterName *agouti.Selection
-	TimeStamp   *agouti.Selection
-}
-
 //ClustersPage elements
 type ClustersPage struct {
 	ClusterCount                                *agouti.Selection
 	ConnectClusterButton                        *agouti.Selection
 	PRDeleteClusterButton                       *agouti.Selection
-	NoFiringAlertMessage                        *agouti.Selection
-	FiringAlertsSection                         *agouti.Selection
-	FiringAlertsHeader                          *agouti.Selection
-	FiringAlertsNavCtl                          *agouti.Selection
 	ClustersListSection                         *agouti.Selection
 	ClustersListHeader                          *agouti.Selection
 	FiringAlertsPerPage                         *agouti.Selection
@@ -57,16 +43,9 @@ type ClustersPage struct {
 	HeaderName                                  *agouti.Selection
 	HeaderIcon                                  *agouti.Selection
 	HeaderStatus                                *agouti.Selection
-	NoClusterConfigured                         *agouti.Selection
 	ClustersList                                *agouti.Selection
 	Tooltip                                     *agouti.Selection
 	SupportEmailLink                            *agouti.Selection
-	ClustersListPaginationNext                  *agouti.Selection
-	ClustersListPaginationPrevious              *agouti.Selection
-	ClustersListPaginationLast                  *agouti.Selection
-	ClustersListPaginationFirst                 *agouti.Selection
-	ClustersListPaginationPerPageDropdown       *agouti.Selection
-	ClustersListPaginationPerPageDropdownSecond *agouti.Selection
 	MessageBar                                  *agouti.Selection
 	Version                                     *agouti.Selection
 }
@@ -83,9 +62,7 @@ func FindClusterInList(clustersPage *ClustersPage, clusterName string) *ClusterI
 		Checkbox:         cluster.FindByXPath(`td[1]`),
 		ShowStatusDetail: cluster.FindByXPath(`td[2]`).Find(`svg`),
 		Name:             cluster.FindByXPath(`td[2]`),
-		// Icon:             cluster.FindByXPath(`td[3]`).Find(`svg`),
 		Status: cluster.FindByXPath(`td[5]`),
-		// EditCluster:      cluster.FindByXPath(`td[5]`).Find("button"),
 	}
 }
 
@@ -112,60 +89,21 @@ func GetDeletePRPopup(webDriver *agouti.Page) *DeletePullRequestPopup {
 	return &deletePRPopup
 }
 
-func FindAlertInFiringAlertsWidget(clustersPage *ClustersPage, alertName string) *AlertInformation {
-	for _, a := range AlertsFiringInAlertsWidget(clustersPage) {
-		text, _ := a.Message.Text()
-		if strings.Contains(text, alertName) {
-			return a
-		}
-	}
-	return nil
-}
-
-func AlertsFiringInAlertsWidget(clustersPage *ClustersPage) []*AlertInformation {
-	alertInfos := []*AlertInformation{}
-	count, _ := clustersPage.FiringAlerts.Count()
-	for i := 0; i < count; i++ {
-		alert := clustersPage.FiringAlerts.At(i)
-		alertInfos = append(alertInfos, &AlertInformation{
-			Severity:    alert.FindByXPath(`td[1]`),
-			Message:     alert.FindByXPath(`td[2]`),
-			ClusterName: alert.FindByXPath(`td[3]`),
-			TimeStamp:   alert.FindByXPath(`td[4]`),
-		})
-
-	}
-	return alertInfos
-}
-
 //GetClustersPage initialises the webDriver object
 func GetClustersPage(webDriver *agouti.Page) *ClustersPage {
 	clustersPage := ClustersPage{
 		ClusterCount:          webDriver.Find(`.count-header .section-header-count`),
 		ConnectClusterButton:  webDriver.Find(`#connect-cluster`),
 		PRDeleteClusterButton: webDriver.Find(`#delete-cluster`),
-		NoFiringAlertMessage:  webDriver.Find(`#firing-alerts caption`),
-		FiringAlertsSection:   webDriver.Find(`#firing-alerts`),
-		FiringAlertsHeader:    webDriver.FindByXPath(`//*[@id="firing-alerts"]/div/div/div[1]/div`),
-		FiringAlertsNavCtl:    webDriver.FindByXPath(`//*[@id="firing-alerts"]/div/div/div[2]/div/p/span/span[2]`),
-		FiringAlertsPerPage:   webDriver.FindByXPath(`//*[@id="firing-alerts"]/div/div/div[3]`),
-		FiringAlerts:          webDriver.All(`#firing-alerts tbody tr`),
 		ClustersListSection:   webDriver.Find(`#clusters-list`),
-		ClustersListHeader:    webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/thead`),
-		HeaderCheckbox:        webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/thead/tr/th[1]/span`),
-		HeaderName:            webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/thead/tr/th[2]/span`),
-		HeaderIcon:            webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/thead/tr/th[3]/span`),
-		HeaderStatus:          webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/thead/tr/th[4]/span`),
-		// NoClusterConfigured:                   webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/caption`),
+		ClustersListHeader:    webDriver.FindByXPath(`//*[@id="clusters-list"]/div/div[2]/div[1]/div[1]/table/thead`),
+		HeaderCheckbox:        webDriver.FindByXPath(`//*[@id="clusters-list"]/div/div[2]/div[1]/div[1]/table/thead/tr/th[1]/span`),
+		HeaderName:            webDriver.FindByXPath(`//*[@id="clusters-list"]/div/div[2]/div[1]/div[1]/table/thead/tr/th[2]/span`),
+		HeaderIcon:            webDriver.FindByXPath(`//*[@id="clusters-list"]/div/div[2]/div[1]/div[1]/table/thead/tr/th[3]/span`),
+		HeaderStatus:          webDriver.FindByXPath(`//*[@id="clusters-list"]/div/div[2]/div[1]/div[1]/table/thead/tr/th[4]/span`),
 		ClustersList:     webDriver.Find(`#clusters-list > div > div[2] > div[1] > div[1] > table > tbody`),
 		Tooltip:          webDriver.Find(`div[role="tooltip"]`),
 		SupportEmailLink: webDriver.FindByLink(`support@weave.works`),
-		// ClustersListPaginationNext:            webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/tfoot/tr/td/div/div[3]/button[3]`),
-		// ClustersListPaginationPrevious:        webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/tfoot/tr/td/div/div[3]/button[2]`),
-		// ClustersListPaginationLast:            webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/tfoot/tr/td/div/div[3]/button[4]`),
-		// ClustersListPaginationFirst:           webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/tfoot/tr/td/div/div[3]/button[1]`),
-		// ClustersListPaginationPerPageDropdown: webDriver.FindByXPath(`//*[@id="clusters-list"]/div/table/tfoot/tr/td/div/div[2]/div`),
-		// ClustersListPaginationPerPageDropdownSecond: webDriver.FindByXPath(`//*[@id="menu-"]/div[3]/ul/li[2]`),
 		MessageBar: webDriver.FindByXPath(`//div[@id="root"]/div/main/div[2]`),
 		Version:    webDriver.FindByXPath(`//div[starts-with(text(), "Weave GitOps Enterprise")]`),
 	}
