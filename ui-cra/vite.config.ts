1c476e299c6597cc93ff120ed0d85e1687ae06ab
Jordan Pellizzari <jordan@weave.works>
2022-05-16 09:42:47 -0700

    Supress ts import and install dev deps

--- ui-cra/vite.config.ts
+++ ui-cra/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+// @ts-ignore
 import svgrPlugin from 'vite-plugin-svgr';
 
 const DEFAULT_PROXY_HOST = 'http://34.67.250.163:30080/';
