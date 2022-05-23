3217c67226c3fb4f386790818ae0d30c755dfc08
Ahmed Magdy <ahmed.magdy@magalix.com>
2022-05-23 13:42:53 +0200

    update input directory

--- ui-cra/src/contexts/EnterpriseClient/index.tsx
+++ ui-cra/src/contexts/EnterpriseClient/index.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import { ClustersService } from '../../cluster-services/clusters_grpc_pb';
+import { ClustersService } from '../../cluster-services/cluster_services.pb';
 
 export type EnterpriseClientContextType = {
   api: typeof ClustersService;
