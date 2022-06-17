c55c291f645f3641dd90cde842e5b9d391fc3b0d
Jordan Pellizzari <jordan@weave.works>
2022-06-17 12:33:25 -0700

    Add test infra and Applications test example

---
+++ ui-cra/src/components/Applications/__tests__/index.test.tsx
@@ -0,0 +1,78 @@
+import Applications from '../';
+
+import { MuiThemeProvider } from '@material-ui/core';
+import { act, render, screen } from '@testing-library/react';
+import { CoreClientContextProvider, theme } from '@weaveworks/weave-gitops';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { MemoryRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import ClustersProvider from '../../../contexts/Clusters/Provider';
+import EnterpriseClientProvider from '../../../contexts/EnterpriseClient/Provider';
+import NotificationsProvider from '../../../contexts/Notifications/Provider';
+import RequestContextProvider from '../../../contexts/Request';
+import TemplatesProvider from '../../../contexts/Templates/Provider';
+import VersionsProvider from '../../../contexts/Versions/Provider';
+import { muiTheme } from '../../../muiTheme';
+import {
+  CoreClientMock,
+  EnterpriseClientMock,
+  withContext,
+} from '../../../utils/test-utils';
+
+describe('Applications index test', () => {
+  let wrap: (el: JSX.Element) => JSX.Element;
+  let api: CoreClientMock;
+
+  beforeEach(() => {
+    api = new CoreClientMock();
+    wrap = withContext([
+      [ThemeProvider, { theme: theme }],
+      [MuiThemeProvider, { theme: muiTheme }],
+      [
+        RequestContextProvider,
+        { fetch: () => new Promise(accept => accept(null)) },
+      ],
+      [QueryClientProvider, { client: new QueryClient() }],
+      [
+        EnterpriseClientProvider,
+        {
+          api: new EnterpriseClientMock(),
+        },
+      ],
+      [CoreClientContextProvider, { api }],
+      [MemoryRouter],
+      [NotificationsProvider],
+      [VersionsProvider],
+      [TemplatesProvider],
+      [ClustersProvider],
+      [VersionsProvider],
+    ]);
+  });
+  it('renders table rows', async () => {
+    api.ListKustomizationsReturns = {
+      kustomizations: [
+        {
+          namespace: 'my-ns',
+          name: 'my-kustomization',
+          path: './',
+          sourceRef: {},
+          interval: {},
+          conditions: [],
+          lastAppliedRevision: '',
+          lastAttemptedRevision: '',
+          inventory: [],
+          suspended: false,
+          clusterName: 'my-cluster',
+        },
+      ],
+    };
+    api.ListHelmReleasesReturns = [] as any;
+
+    await act(async () => {
+      const c = wrap(<Applications />);
+      render(c);
+    });
+
+    expect(await screen.findByText('my-kustomization')).toBeTruthy();
+  });
+});
---
+++ ui-cra/src/utils/test-utils.tsx
@@ -0,0 +1,70 @@
+import {
+  ListHelmReleasesResponse,
+  ListKustomizationsResponse,
+} from '@weaveworks/weave-gitops/ui/lib/api/core/core.pb';
+import _ from 'lodash';
+import React from 'react';
+import {
+  GetConfigResponse,
+  ListTemplatesResponse,
+} from '../cluster-services/cluster_services.pb';
+import Compose from '../components/ProvidersCompose';
+
+export const withContext = (contexts: any[]) => {
+  return (component: React.ReactElement) => {
+    const tree = _.reduce(
+      contexts,
+      (r: any[], c) => {
+        const [Ctx, props] = c;
+
+        r.push((otherProps: any) => <Ctx {...props} {...otherProps} />);
+        return r;
+      },
+      [],
+    );
+
+    return <Compose components={tree}>{component}</Compose>;
+  };
+};
+
+const promisify = <R, E>(res: R, errRes?: E) =>
+  new Promise<R>((accept, reject) => {
+    if (errRes) {
+      return reject(errRes);
+    }
+    accept(res);
+  });
+
+export class EnterpriseClientMock {
+  constructor() {
+    this.GetConfig = this.GetConfig.bind(this);
+    this.ListTemplates = this.ListTemplates.bind(this);
+  }
+  GetConfigReturns: GetConfigResponse = {};
+  ListTemplatesReturns: ListTemplatesResponse = {};
+
+  GetConfig() {
+    return promisify(this.GetConfigReturns);
+  }
+
+  ListTemplates() {
+    return promisify(this.ListTemplatesReturns);
+  }
+}
+
+export class CoreClientMock {
+  constructor() {
+    this.ListKustomizations = this.ListKustomizations.bind(this);
+    this.ListHelmReleases = this.ListHelmReleases.bind(this);
+  }
+  ListKustomizationsReturns: ListKustomizationsResponse = {};
+  ListHelmReleasesReturns: ListHelmReleasesResponse = {};
+
+  ListKustomizations() {
+    return promisify(this.ListKustomizationsReturns);
+  }
+
+  ListHelmReleases() {
+    return promisify(this.ListHelmReleasesReturns);
+  }
+}
