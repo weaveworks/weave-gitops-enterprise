e3dd9de939231163e37d108eeb7a4b04febe8eab
Jordan Pellizzari <jordan@weave.works>
2022-06-23 14:26:02 -0700

    Add ListEvents endpoint

---
+++ cmd/clusters-service/pkg/server/events.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	capiv1_proto "github.com/weaveworks/weave-gitops-enterprise/cmd/clusters-service/pkg/protos"
+	"github.com/weaveworks/weave-gitops/pkg/server/auth"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func (s *server) ListEvents(ctx context.Context, msg *capiv1_proto.ListEventsRequest) (*capiv1_proto.ListEventsResponse, error) {
+	c, err := s.clientsFactory.GetImpersonatedClient(ctx, auth.Principal(ctx))
+	if err != nil {
+		return nil, fmt.Errorf("error getting impersonating client: %s", err)
+	}
+
+	if msg.ClusterName == "" {
+		return nil, requiredClusterNameErr
+	}
+
+	scoped, err := c.Scoped(msg.ClusterName)
+	if err != nil {
+		return nil, fmt.Errorf("scoping to cluster %q: %w", msg.ClusterName, err)
+	}
+
+	fmt.Printf("%#v", msg)
+
+	fields := client.MatchingFields{
+		"involvedObject.kind":      msg.InvolvedObject.Kind,
+		"involvedObject.name":      msg.InvolvedObject.Name,
+		"involvedObject.namespace": msg.InvolvedObject.Namespace,
+	}
+
+	l := &corev1.EventList{}
+	if err := scoped.List(ctx, l, fields); err != nil {
+		return nil, fmt.Errorf("listing events: %w", err)
+	}
+
+	result := []*capiv1_proto.Event{}
+	for _, e := range l.Items {
+		result = append(result, &capiv1_proto.Event{
+			Type:      e.Type,
+			Component: e.Source.Component,
+			Name:      e.ObjectMeta.Name,
+			Reason:    e.Reason,
+			Message:   e.Message,
+			Timestamp: e.LastTimestamp.Format(time.RFC3339),
+			Host:      e.Source.Host,
+		})
+	}
+
+	return &capiv1_proto.ListEventsResponse{Events: result}, nil
+}
