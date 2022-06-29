b75c3bafc7e2e9d2b9558ec216f0016b94d9e503
AlinaGoaga <goaga.alina@gmail.com>
2022-06-29 12:54:29 +0100

    Update test - 2

--- test/acceptance/test/cli_upgrade.go
+++ test/acceptance/test/cli_upgrade.go
@@ -313,8 +313,8 @@ func DescribeCliUpgrade(gitopsTestRunner GitopsTestRunner) {
 					Eventually(profile.Values.Click).Should(Succeed())
 					valuesYaml := pages.GetValuesYaml(webDriver)
 
-					Eventually(valuesYaml.Title.Text).Should(MatchRegexp("podinfo"))
-					Eventually(valuesYaml.TextArea.Text).Should(MatchRegexp("tag: 6.0.0"))
+					Eventually(valuesYaml.Title.Text, 30*time.Second).Should(MatchRegexp("podinfo"))
+					Eventually(valuesYaml.TextArea.Text,30*time.Second).Should(MatchRegexp("tag: 6.0.0"))
 					Eventually(valuesYaml.Cancel.Click).Should(Succeed())
 				})
 
