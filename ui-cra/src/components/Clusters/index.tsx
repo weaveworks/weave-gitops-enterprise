969d308e9b0fc1fe547f03354c56d9cf224c07b7
Simon Howe <footless@gmail.com>
2022-06-30 12:34:07 +0100

    Pass namespace through when deleting clusters

--- ui-cra/src/components/Clusters/index.tsx
+++ ui-cra/src/components/Clusters/index.tsx
@@ -33,6 +33,7 @@ import { Checkbox, withStyles } from '@material-ui/core';
 import { GitopsClusterEnriched } from '../../types/custom';
 import { DashboardsList } from './DashboardsList';
 import { useListConfig } from '../../hooks/versions';
+import { ClusterNamespacedName } from '../../cluster-services/cluster_services.pb';
 
 interface Size {
   size?: 'small';
@@ -86,6 +87,44 @@ export const PRdefaults = {
   commitMessage: 'Deletes capi cluster(s)',
 };
 
+const IndividualCheckbox = withStyles({
+  root: {
+    color: theme.colors.primary,
+    '&$checked': {
+      color: theme.colors.primary,
+    },
+    '&$disabled': {
+      color: theme.colors.neutral20,
+    },
+  },
+  checked: {},
+  disabled: {},
+})(Checkbox);
+
+const ClusterRowCheckbox = ({
+  name,
+  namespace,
+  checked,
+  onChange,
+}: ClusterNamespacedName & { checked: boolean; onChange: any }) => (
+  <IndividualCheckbox
+    checked={checked}
+    onChange={useCallback(
+      ev => onChange({ name, namespace }, ev),
+      [name, namespace, onChange],
+    )}
+    name={name}
+  />
+);
+
+interface FormData {
+  url: string | null;
+  branchName: string;
+  pullRequestTitle: string;
+  commitMessage: string;
+  pullRequestDescription: string;
+}
+
 const MCCP: FC = () => {
   const { clusters, isLoading, count, selectedClusters, setSelectedClusters } =
     useClusters();
@@ -101,19 +140,13 @@ const MCCP: FC = () => {
   );
   let selectedCapiClusters = useMemo(
     () =>
-      selectedClusters.filter(cls => capiClusters.find(c => c.name === cls)),
+      selectedClusters.filter(({ name, namespace }) =>
+        capiClusters.find(c => c.name === name && c.namespace === namespace),
+      ),
     [capiClusters, selectedClusters],
   );
 
   const authRedirectPage = `/clusters`;
-
-  interface FormData {
-    url: string | null;
-    branchName: string;
-    pullRequestTitle: string;
-    commitMessage: string;
-    pullRequestDescription: string;
-  }
 
   let initialFormData = {
     ...PRdefaults,
@@ -153,12 +186,15 @@ const MCCP: FC = () => {
 
   useEffect(() => {
     if (!callbackState) {
+      const prTitle = `Delete clusters: ${selectedCapiClusters
+        .map(c => `${c.namespace}/${c.name}`)
+        .join(', ')}`;
       setFormData((prevState: FormData) => ({
         ...prevState,
         url: repositoryURL,
-        pullRequestDescription: `Delete clusters: ${selectedCapiClusters
-          .map(c => c)
-          .join(', ')}`,
+        commitMessage: prTitle,
+        pullRequestTitle: prTitle,
+        pullRequestDescription: prTitle,
       }));
     }
 
@@ -179,46 +215,40 @@ const MCCP: FC = () => {
 
   const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
-      const newSelected =
-        clusters.map((cluster: GitopsClusterEnriched) => cluster.name || '') ||
-        [];
-      setSelectedClusters(newSelected);
+      setSelectedClusters(
+        clusters.map(({ name, namespace }: GitopsClusterEnriched) => ({
+          name,
+          namespace,
+        })),
+      );
       return;
     }
     setSelectedClusters([]);
   };
 
-  const handleIndividualClick = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    if (event.target.checked === true) {
-      setSelectedClusters((prevState: string[]) => [
-        ...prevState,
-        event.target.name,
-      ]);
-    } else {
-      setSelectedClusters((prevState: string[]) =>
-        prevState.filter(cls => event.target.name !== cls),
-      );
-    }
-  };
+  const handleIndividualClick = useCallback(
+    (
+      { name, namespace }: ClusterNamespacedName,
+      event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+      if (event.target.checked === true) {
+        setSelectedClusters((prevState: ClusterNamespacedName[]) => [
+          ...prevState,
+          { name, namespace },
+        ]);
+      } else {
+        setSelectedClusters((prevState: ClusterNamespacedName[]) =>
+          prevState.filter(
+            cls => cls.name !== name && cls.namespace !== namespace,
+          ),
+        );
+      }
+    },
+    [setSelectedClusters],
+  );
 
   const numSelected = selectedClusters.length;
   const rowCount = clusters.length || 0;
-
-  const IndividualCheckbox = withStyles({
-    root: {
-      color: theme.colors.primary,
-      '&$checked': {
-        color: theme.colors.primary,
-      },
-      '&$disabled': {
-        color: theme.colors.neutral20,
-      },
-    },
-    checked: {},
-    disabled: {},
-  })(Checkbox);
 
   useEffect(() => {
     repositoryURL &&
@@ -340,14 +370,18 @@ const MCCP: FC = () => {
                           }}
                         />
                       ),
-                      value: (c: GitopsClusterEnriched) => (
-                        <IndividualCheckbox
-                          checked={
-                            selectedClusters.indexOf(c.name ? c.name : '') !==
-                            -1
-                          }
+                      value: ({ name, namespace }: GitopsClusterEnriched) => (
+                        <ClusterRowCheckbox
+                          name={name}
+                          namespace={namespace}
                           onChange={handleIndividualClick}
-                          name={c.name}
+                          checked={Boolean(
+                            selectedClusters.find(
+                              cls =>
+                                cls.name === name &&
+                                cls.namespace === namespace,
+                            ),
+                          )}
                         />
                       ),
                       maxWidth: 25,
