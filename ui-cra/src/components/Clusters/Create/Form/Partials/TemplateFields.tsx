30f6766d82c5020fe4c23132404f32c81dd0616e
AlinaGoaga <goaga.alina@gmail.com>
2022-07-07 13:56:02 +0100

    Build new create cluster form - WIP

--- ui-cra/src/components/Clusters/Create/Form/Partials/TemplateFields.tsx
+++ ui-cra/src/components/Clusters/Create/Form/Partials/TemplateFields.tsx
@@ -1,10 +1,15 @@
-import React, { FC, useMemo, Dispatch } from 'react';
+import React, { FC, Dispatch } from 'react';
 import { theme as weaveTheme, Button } from '@weaveworks/weave-gitops';
 import styled from 'styled-components';
 import {
   Template,
   TemplateObject,
 } from '../../../../../cluster-services/cluster_services.pb';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 const base = weaveTheme.spacing.base;
 const small = weaveTheme.spacing.small;
@@ -29,84 +34,51 @@ const TemplateFields: FC<{
   formData: any;
   setFormData: Dispatch<React.SetStateAction<any>>;
 }> = ({ activeTemplate, onPRPreview, formData, setFormData }) => {
-  const parameters = useMemo(() => {
-    return (
-      activeTemplate?.parameters?.map(param => {
-        const { options } = param;
-        const name = param.name as string;
-        if (options?.length !== 0) {
-          return {
-            [name]: {
-              type: 'string',
-              title: `${name}`,
-              enum: options,
-            },
-          };
-        } else {
-          return {
-            [name]: {
-              type: 'string',
-              title: `${name}`,
-            },
-          };
-        }
-      }) || []
-    );
-  }, [activeTemplate]);
-
-  console.log(parameters);
-
-  // }, [sections, addUserSelectedFields, userSelectedFields]);
-
-  // const credentialsItems = [
-  //   ...credentials.map((credential: Credential) => {
-  //     const { kind, namespace, name } = credential;
-  //     return (
-  //       <MenuItem key={name} value={name || ''}>
-  //         {`${kind}/${namespace || 'default'}/${name}`}
-  //       </MenuItem>
-  //     );
-  //   }),
-  //   <MenuItem key="None" value="None">
-  //     <em>None</em>
-  //   </MenuItem>,
-  // ];
+  console.log(formData);
+  const handleFormData = () => {
+    // update formdata
+  };
 
   return (
-    /* TO DO: if enum show select else show the normal input */
-
-    // formData structure
-
-    // { ... parameter_values":{"url":"https://github.com/wkp-example-org/capd-demo-reloaded","provider":"GitHub","branchName":"create-clusters-branch-ded8a","pullRequestTitle":"Creates capi cluster",""commitMessage":"Creates capi cluster","pullRequestDescription":"This PR creates a new cluster","CLUSTER_NAME":"testali111","NAMESPACE":"default","CONTROL_PLANE_MACHINE_COUNT":"1","KUBERNETES_VERSION":"1.19.11","WORKER_MACHINE_COUNT":"1"} ... ]}
-
     <FormWrapper>
-      {/* <div className="profile-namespace">
-        <span>Namespace</span>
-        <FormControl>
-          <Input
-            id="profile-namespace"
-            value={namespace}
-            placeholder="flux-system"
-            onChange={handleChangeNamespace}
-            error={!isNamespaceValid}
-          />
-        </FormControl>
-      </div> */}
-
-      {/* <div className="credentials">
-        <span>Infrastructure provider credentials:</span>
-        <FormControl>
-          <Select
-            disabled={isLoading}
-            value={infraCredential?.name || 'None'}
-            onChange={handleSelectCredentials}
-            autoWidth
-            label="Credentials"
-          >
-            {credentialsItems}
-          </Select>
-        </FormControl>
-      </div> */}
+      {activeTemplate?.parameters?.map(param => {
+        const name = param.name || '';
+        const options = param?.options || [];
+        if (options.length > 0) {
+          return (
+            <div>
+              <FormControl>
+                <Select
+                  id={param.name}
+                  value={formData.name}
+                  onChange={handleFormData}
+                  autoWidth
+                  label={param.name}
+                >
+                  {options?.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+                <FormHelperText>{param.description}</FormHelperText>
+              </FormControl>
+            </div>
+          );
+        } else
+          return (
+            <div>
+              <FormControl>
+                <Input
+                  id={param.name}
+                  value={formData.name}
+                  onChange={handleFormData}
+                />
+                <FormHelperText>{param.description}</FormHelperText>
+              </FormControl>
+            </div>
+          );
+      })}
       <div className="previewCTA">
         <Button type="submit">PREVIEW PR</Button>
       </div>
