be648ef3eb544203d9f703c6a3611d788b1069c7
AlinaGoaga <goaga.alina@gmail.com>
2022-07-20 12:37:48 +0100

    Update Delete PR form to match Create PR style

--- ui-cra/src/components/Clusters/Delete.tsx
+++ ui-cra/src/components/Clusters/Delete.tsx
@@ -5,6 +5,7 @@ import {
   DialogTitle,
   Typography,
 } from '@material-ui/core';
+import styled from 'styled-components';
 import { CloseIconButton } from '../../assets/img/close-icon-button';
 import useClusters from '../../contexts/Clusters';
 import useNotifications from '../../contexts/Notifications';
@@ -23,6 +24,12 @@ import { isUnauthenticated, removeToken } from '../../utils/request';
 import GitAuth from './Create/Form/Partials/GitAuth';
 import { PRdefaults } from '.';
 import { ClusterNamespacedName } from '../../cluster-services/cluster_services.pb';
+
+const FormWrapper = styled.form`
+  .form-section {
+    width: 100%;
+  }
+`;
 
 interface Props {
   formData: any;
@@ -137,24 +144,28 @@ export const DeleteClusterDialog: FC<Props> = ({
         </DialogTitle>
         <DialogContent>
           {!loading ? (
-            <>
+            <FormWrapper>
               <Input
-                label="Create branch"
+                className="form-section"
+                label="CREATE BRANCH"
                 placeholder={formData.branchName}
                 onChange={handleChangeBranchName}
               />
               <Input
-                label="Pull request title"
+                className="form-section"
+                label="PULL REQUEST TITLE"
                 placeholder={formData.pullRequestTitle}
                 onChange={handleChangePullRequestTitle}
               />
               <Input
-                label="Commit message"
+                className="form-section"
+                label="COMMIT MESSAGE"
                 placeholder={formData.commitMessage}
                 onChange={handleChangeCommitMessage}
               />
               <Input
-                label="Pull request description"
+                className="form-section"
+                label="PULL REQUEST DESCRIPTION"
                 placeholder={formData.pullRequestDescription}
                 onChange={handleChangePRDescription}
                 multiline
@@ -176,7 +187,7 @@ export const DeleteClusterDialog: FC<Props> = ({
               >
                 REMOVE CLUSTERS FROM THE MCCP
               </Button>
-            </>
+            </FormWrapper>
           ) : (
             <Loader />
           )}
