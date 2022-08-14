29f4b1bfd641d7f50f27167ab5b119d0c2f59957
TheGostKasper <thegostkasper@gmail.com>
2022-08-14 17:55:34 +0200

    Avoid duplication on AlertListError component

--- ui-cra/src/components/Applications/utils.tsx
+++ ui-cra/src/components/Applications/utils.tsx
@@ -1,6 +1,6 @@
 import { useListAutomations } from '@weaveworks/weave-gitops';
 
 export const useApplicationsCount = (): number => {
-  const { data } = useListAutomations();
+  const { data } = useListAutomations(undefined, {})
   return data?.result?.length || 0;
 };
--- ui-cra/src/components/Layout/AlertListErrors.tsx
+++ ui-cra/src/components/Layout/AlertListErrors.tsx
@@ -4,7 +4,7 @@ import { theme } from '@weaveworks/weave-gitops';
 import Alert from '@material-ui/lab/Alert';
 import AlertTitle from '@material-ui/lab/AlertTitle';
 import { createStyles, makeStyles } from '@material-ui/styles';
-import { ListItem } from '@material-ui/core';
+import { ListItem, ListItemText } from '@material-ui/core';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -29,16 +29,19 @@ export const AlertListErrors: FC<{ errors?: ListError[] }> = ({ errors }) => {
   if (!errors || !errors.length) {
     return null;
   }
+  const err = `Something went wrong, User "wego-admin"`;
 
   return (
     <>
       <Alert className={classes.alertWrapper} severity="error">
-        <AlertTitle>
-          There were errors while listing some resources:
-        </AlertTitle>
-        {errors?.map((item: ListError) => (
-          <ListItem key={item.clusterName}>
-            • error='{item.message}' cluster='{item.clusterName}' namespace='{item.namespace}'
+        <AlertTitle>There were errors while listing some resources:</AlertTitle>
+        {[...errors,...errors]?.map((item: ListError, index: number) => (
+          <ListItem key={index} dense={true}>
+            <ListItemText
+              style={{display:'list-item'}}
+              primary={item.message || err}
+              secondary={`${item.clusterName} / ${item.namespace}`}
+            />
           </ListItem>
         ))}
       </Alert>
