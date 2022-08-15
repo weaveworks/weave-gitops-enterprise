95f538cf5a069e5b145444949f97e2fd0dc6e223
Ahmed Magdy <ahussein@outlook.com>
2022-08-15 14:01:19 +0200

    make use of exposed errors from applications and sources APIs (#1203)
    * make use of exposed errors from applications and soruces APIs
    
    * update Sources usage
    
    * create component source count to reduce code duplication
    
    * fix variable name
    
    * update uniquness by clusater + mesage
    
    * move filteredErrors from content wrapper to ListErrors component
    
    Co-authored-by: Ahmed Magdy <ahmed.magdy@magalix.com>

--- ui-cra/src/components/Applications/utils.tsx
+++ ui-cra/src/components/Applications/utils.tsx
@@ -1,6 +1,13 @@
-import { useListAutomations } from '@weaveworks/weave-gitops';
+import { useListAutomations, useListSources } from '@weaveworks/weave-gitops';
 
 export const useApplicationsCount = (): number => {
-  const { data } = useListAutomations();
-  return data?.result?.length || 0;
+  const { data: automations } = useListAutomations();
+
+  return automations?.result?.length || 0;
+};
+
+export const useSourcesCount = (): number => {
+  const { data: sources } = useListSources();
+
+  return sources?.result?.length || 0;
 };
