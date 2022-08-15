c519943ef25e49cf0d93d032faa1d60dc6bdeb83
Casper <TheGostKasper@gmail.com>
2022-08-15 19:02:21 +0200

    Avoid duplication on AlertListError component (#1287)
    * Avoid duplication on AlertListError component

--- ui-cra/src/components/Applications/utils.tsx
+++ ui-cra/src/components/Applications/utils.tsx
@@ -1,13 +1,11 @@
 import { useListAutomations, useListSources } from '@weaveworks/weave-gitops';
 
 export const useApplicationsCount = (): number => {
-  const { data: automations } = useListAutomations();
-
+  const { data: automations } = useListAutomations(undefined, {});
   return automations?.result?.length || 0;
 };
 
 export const useSourcesCount = (): number => {
-  const { data: sources } = useListSources();
-
+  const { data: sources } = useListSources(undefined,undefined, {});
   return sources?.result?.length || 0;
 };
