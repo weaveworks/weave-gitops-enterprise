8f068da6eb785b02bb7b2b3bc9c68602e6bb41bd
Simon <footless@gmail.com>
2022-08-17 08:58:33 +0200

    fix: ui: error deduplication and displaying on sources/runtime (#1293)
    * fix: ui: error deduplication and displaying on sources/runtime
    
    - Some small errors that might have crept in when merging some stuff
      recently
    
    * unused import

--- ui-cra/src/components/Layout/AlertListErrors.tsx
+++ ui-cra/src/components/Layout/AlertListErrors.tsx
@@ -5,8 +5,7 @@ import Alert from '@material-ui/lab/Alert';
 import AlertTitle from '@material-ui/lab/AlertTitle';
 import { createStyles, makeStyles } from '@material-ui/styles';
 import { ListItem, ListItemText } from '@material-ui/core';
-import { MultiRequestError } from '@weaveworks/weave-gitops/ui/lib/types';
-import { uniqBy } from 'lodash';
+import { uniqBy, sortBy } from 'lodash';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -25,30 +24,39 @@ const useStyles = makeStyles(() =>
   }),
 );
 
+function errorInfo(item: ListError): string {
+  const msg = `Cluster: ${item.clusterName}`;
+  if (!item.namespace) {
+    return msg;
+  }
+  return `${msg}, Namespace: ${item.namespace}`;
+}
+
 export const AlertListErrors: FC<{ errors?: ListError[] }> = ({ errors }) => {
   const classes = useStyles();
 
   if (!errors || !errors.length) {
     return null;
   }
-  const filteredErrors = uniqBy(errors, error => {
-    [error.clusterName, error.message].join();
-  }) as MultiRequestError[];
+
+  // still not ideal
+  const filteredErrors = sortBy(
+    uniqBy(errors, error => [error.clusterName, error.message].join()),
+    [v => v.clusterName, v => v.namespace, v => v.message],
+  );
 
   return (
-    <>
-      <Alert className={classes.alertWrapper} severity="error">
-        <AlertTitle>There were errors while listing some resources:</AlertTitle>
-        {filteredErrors?.map((item: ListError, index: number) => (
-          <ListItem key={index} dense={true}>
-            <ListItemText
-              className={classes.listItems}
-              primary={item.message}
-              secondary={`Cluster: ${item.clusterName} , Namespace: ${item.namespace}`}
-            />
-          </ListItem>
-        ))}
-      </Alert>
-    </>
+    <Alert className={classes.alertWrapper} severity="error">
+      <AlertTitle>There were errors while listing some resources:</AlertTitle>
+      {filteredErrors?.map((item: ListError, index: number) => (
+        <ListItem key={index} dense={true}>
+          <ListItemText
+            className={classes.listItems}
+            primary={item.message}
+            secondary={errorInfo(item)}
+          />
+        </ListItem>
+      ))}
+    </Alert>
   );
 };
