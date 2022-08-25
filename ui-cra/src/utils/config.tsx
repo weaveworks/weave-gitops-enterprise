45a775f1ad811417ad90128742324f7c60185b20
Simon <footless@gmail.com>
2022-08-25 10:00:57 +0200

    Fixes adding apps to flux-system namespace (#1351)
    * Refactor automations endpoint out of clusters file
    
    * Fixes adding apps to flux-system namespace
    
    - ./flux-system has a kustomization.yaml in it after boostrapping
    - we don't update this when adding an app
    - don't add apps to this folder, prefix the file with $namespace-
      instead of placing it under $namespace/
    
    * Default values for kustomization and helmrelease namespace
    
    * Switch filename of add'd app

---
+++ ui-cra/src/utils/config.tsx
@@ -0,0 +1,4 @@
+export const DEFAULT_FLUX_KUSTOMIZATION_NAMESPACE = 'flux-system';
+export const DEFAULT_PROFILE_NAMESPACE = 'flux-system';
+export const FLUX_BOOSTRAP_KUSTOMIZATION_NAME = 'flux-system';
+export const FLUX_BOOSTRAP_KUSTOMIZATION_NAMESPACE = 'flux-system';
