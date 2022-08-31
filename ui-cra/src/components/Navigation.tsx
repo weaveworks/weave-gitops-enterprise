479a7a33140b36be76e537dba8b769ec6fa44ffe
TheGostKasper <thegostkasper@gmail.com>
2022-08-31 13:49:12 +0200

    update navigation

--- ui-cra/src/components/Navigation.tsx
+++ ui-cra/src/components/Navigation.tsx
@@ -158,18 +158,13 @@ export const Navigation: FC = () => {
         {
           name: 'Pipelines',
           link: '/applications/pipelines',
-          isVisible: true,
+          isVisible: !!flagsRes?.flags?.WEAVE_GITOPS_FEATURE_PIPELINES,
         },
         {
           name: 'Delivery',
           link: '/applications/delivery',
           isVisible:
             process.env.REACT_APP_DISABLE_PROGRESSIVE_DELIVERY !== 'true',
-        },
-        {
-          name: 'Pipelines',
-          link: '/applications/pipelines',
-          isVisible: !!flagsRes?.flags?.WEAVE_GITOPS_FEATURE_PIPELINES,
         },
       ],
     },
