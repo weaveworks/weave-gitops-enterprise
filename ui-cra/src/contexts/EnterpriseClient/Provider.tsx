d9609c43b18f14f9e01436f75675198449bc851e
Simon Howe <simon@weave.works>
2022-09-02 08:42:52 +0200

    Type tricks

--- ui-cra/src/contexts/EnterpriseClient/Provider.tsx
+++ ui-cra/src/contexts/EnterpriseClient/Provider.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { EnterpriseClientContext } from './index';
-import qs from 'query-string';
-
 import { ClustersService } from '../../cluster-services/cluster_services.pb';
+import { UnAuthorizedInterceptor } from '@weaveworks/weave-gitops';
+import { Core } from '@weaveworks/weave-gitops/ui/lib/api/core/core.pb';
 
 type Props = {
   api: typeof ClustersService;
@@ -16,36 +16,10 @@ export enum AuthRoutes {
   AUTH_PATH_SIGNIN = '/sign_in',
 }
 
-function UnAuthorizedInterceptor(api: any): typeof ClustersService {
-  const wrapped: any = {};
-  //   Wrap each API method in a check that redirects to the signin page if a 401 is returned.
-  for (const method of Object.getOwnPropertyNames(api)) {
-    if (typeof api[method] != 'function') {
-      continue;
-    }
-    wrapped[method] = (req: any, initReq: any) => {
-      return api[method](req as any, initReq as any).catch((err: any) => {
-        if (err.code === 401) {
-          window.location.replace(
-            AuthRoutes.AUTH_PATH_SIGNIN +
-              '?' +
-              qs.stringify({
-                // eslint-disable-next-line no-restricted-globals
-                redirect: location.pathname + location.search,
-              }),
-          );
-        }
-        throw err;
-      });
-    };
-  }
-  return wrapped as typeof ClustersService;
-}
-
 const EnterpriseClientProvider = ({ api, children }: Props) => {
-  const wrapped = UnAuthorizedInterceptor(api);
+  const wrapped = UnAuthorizedInterceptor(api as unknown as typeof Core);
   return (
-    <EnterpriseClientContext.Provider value={{ api: wrapped }}>
+    <EnterpriseClientContext.Provider value={{ api: wrapped as unknown as typeof ClustersService }}>
       {children}
     </EnterpriseClientContext.Provider>
   );
