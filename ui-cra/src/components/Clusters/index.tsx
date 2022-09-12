6a3278216271b19b6f209a048bc844779c03ff20
TheGostKasper <thegostkasper@gmail.com>
2022-09-12 14:54:58 +0200

    fix navigation warning in cluster page

--- ui-cra/src/components/Clusters/index.tsx
+++ ui-cra/src/components/Clusters/index.tsx
@@ -108,6 +108,12 @@ const useStyles = makeStyles(() =>
     externalIcon: {
       marginRight: theme.spacing.small,
     },
+    link: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: theme.colors.primary10,
+    },
   }),
 );
 
@@ -410,19 +416,21 @@ const MCCP: FC = () => {
                   onFinish={() => setOpenConnectInfo(false)}
                 />
               )}
-              <Link
-                target={'_blank'}
-                rel="noopener noreferrer"
-                component={Button}
-                to={{ pathname: repoLink }}
-              >
-                <Icon
-                  className={classes.externalIcon}
-                  type={IconType.ExternalTab}
-                  size="base"
-                />
-                GO TO OPEN PULL REQUESTS
-              </Link>
+              <Button>
+                <Link
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  to={{ pathname: repoLink }}
+                  className={classes.link}
+                >
+                  <Icon
+                    className={classes.externalIcon}
+                    type={IconType.ExternalTab}
+                    size="base"
+                  />
+                  GO TO OPEN PULL REQUESTS
+                </Link>
+              </Button>
             </ActionsWrapper>
           </div>
           {!isLoading ? (
