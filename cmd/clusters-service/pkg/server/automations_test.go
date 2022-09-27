7d409481b0fdf55d29f03abfb3cb33cad3e70b08
AlinaGoaga <goaga.alina@gmail.com>
2022-09-27 12:51:52 +0200

    Update test - 2

--- cmd/clusters-service/pkg/server/automations_test.go
+++ cmd/clusters-service/pkg/server/automations_test.go
@@ -310,6 +310,7 @@ spec:
         name: weaveworks-charts
         namespace: default
   interval: 10m0s
+  targetNamespace: flux-system
   values:
     foo: bar
 status: {}
@@ -333,6 +334,7 @@ spec:
         name: weaveworks-charts
         namespace: default
   interval: 10m0s
+  targetNamespace: flux-system
   values: null
 status: {}
 `,
@@ -560,6 +562,7 @@ spec:
         name: weaveworks-charts
         namespace: default
   interval: 10m0s
+  targetNamespace: flux-system
   values: null
 status: {}
 `,
