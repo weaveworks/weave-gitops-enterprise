57756a31589eb028550a5c292036ea8b0ba729fa
Simon Howe <simon@weave.works>
2022-09-28 10:49:45 +0200

    Simple templating

--- test/selectors/selectors.go
+++ test/selectors/selectors.go
@@ -2,7 +2,6 @@ package selectors
 
 import (
 	_ "embed"
-	"fmt"
 	"log"
 	"testing"
 
@@ -34,29 +33,30 @@ func SetTestContext(test *testing.T) {
 	t = test
 }
 
-func fromPairsToMap(pairs []string) map[string]string {
-	m := map[string]string{}
+func fromPairsToMap(pairs []string) map[string]interface{} {
+	if len(pairs)%2 != 0 {
+		t.Fatalf("key value pairs must be even")
+	}
+	m := make(map[string]interface{})
 	for i := 0; i < len(pairs); i += 2 {
 		m[pairs[i]] = pairs[i+1]
 	}
 	return m
 }
 
-func Get(webDriver *agouti.Page, group, section, name string, keyValuePairs ...[]string) *agouti.Selection {
+func substituteVariables(sel string, keyValuePairs []string) string {
 	engine := liquid.NewEngine()
-	bindings := map[string]interface{}{
-		"page": map[string]string{
-			"title": "Introduction",
-		},
-	}
-	out, err := engine.ParseAndRenderString(template, bindings)
+	bindings := fromPairsToMap(keyValuePairs)
+	out, err := engine.ParseAndRenderString(sel, bindings)
 	if err != nil {
-		log.Fatalln(err)
+		t.Fatalf("error parsing selector as a template: %v", err)
 	}
-	fmt.Println(out)
+	return out
+}
 
+func Get(webDriver *agouti.Page, group, section, name string, keyValuePairs ...string) *agouti.Selection {
 	sel := selectorData[group][section][name]
-	res := get(webDriver, sel)
+	res := get(webDriver, sel, keyValuePairs...)
 	if res == nil {
 		t.Fatalf(
 			"select or selectByXPath not found in selector: %v, at path: %v",
@@ -68,9 +68,9 @@ func Get(webDriver *agouti.Page, group, section, name string, keyValuePairs ...[
 	return res
 }
 
-func GetMulti(webDriver *agouti.Page, group, section, name string) *agouti.MultiSelection {
+func GetMulti(webDriver *agouti.Page, group, section, name string, keyValuePairs ...string) *agouti.MultiSelection {
 	sel := selectorData[group][section][name]
-	res := getMulti(webDriver, sel)
+	res := getMulti(webDriver, sel, keyValuePairs...)
 	if res == nil {
 		t.Fatalf(
 			"selectAll or selectAllByXPath not found in selector: %v, at path: %v",
@@ -82,26 +82,26 @@ func GetMulti(webDriver *agouti.Page, group, section, name string) *agouti.Multi
 	return res
 }
 
-func getMulti(webDriver *agouti.Page, selector map[string]string) *agouti.MultiSelection {
+func getMulti(webDriver *agouti.Page, selector map[string]string, keyValuePairs ...string) *agouti.MultiSelection {
 	selectAllByXPath := selector["selectAllByXPath"]
 	if selectAllByXPath != "" {
-		return webDriver.AllByXPath(selectAllByXPath)
+		return webDriver.AllByXPath(substituteVariables(selectAllByXPath, keyValuePairs))
 	}
 	selectAll := selector["selectAll"]
 	if selectAll != "" {
-		return webDriver.All(selectAll)
+		return webDriver.All(substituteVariables(selectAll, keyValuePairs))
 	}
 	return nil
 }
 
-func get(webDriver *agouti.Page, selector map[string]string) *agouti.Selection {
+func get(webDriver *agouti.Page, selector map[string]string, keyValuePairs ...string) *agouti.Selection {
 	selectByXPath := selector["selectByXPath"]
 	if selectByXPath != "" {
-		return webDriver.FindByXPath(selectByXPath)
+		return webDriver.FindByXPath(substituteVariables(selectByXPath, keyValuePairs))
 	}
 	selectPattern := selector["select"]
 	if selectPattern != "" {
-		return webDriver.Find(selectPattern)
+		return webDriver.Find(substituteVariables(selectPattern, keyValuePairs))
 	}
 	return nil
 }
--- test/selectors/selectors_test.go
+++ test/selectors/selectors_test.go
@@ -14,14 +14,22 @@ var testData = map[string]map[string]map[string]map[string]string{
 			"singleHeader": {
 				"select": "#header",
 			},
+			"singleClusterHeader": {
+				"select": "#{{ cluster }} .header",
+			},
 			"multiRow": {
 				"selectAll": "list-view tr",
+			},
+			"multiClusterRow": {
+				"selectAll": "#{{ cluster }} list-view tr",
 			},
 		},
 	},
 }
 
 func TestGetSelectors(t *testing.T) {
+	SetTestContext(t)
+
 	fakePage := agouti.JoinPage("localhost")
 
 	single := get(fakePage, testData["templates"]["page"]["singleHeader"])
@@ -39,7 +47,23 @@ func TestGetSelectors(t *testing.T) {
 	assert.Nil(t, multi)
 }
 
+func TestTemplating(t *testing.T) {
+	SetTestContext(t)
+
+	fakePage := agouti.JoinPage("localhost")
+
+	// Test template substitution
+	singleTemplated := get(fakePage, testData["templates"]["page"]["singleClusterHeader"], "cluster", "dev")
+	assert.Equal(t, "selection 'CSS: #dev .header [single]'", singleTemplated.String())
+
+	// Test template substitution
+	multiTemplated := getMulti(fakePage, testData["templates"]["page"]["multiClusterRow"], "cluster", "dev")
+	assert.Equal(t, "selection 'CSS: #dev list-view tr'", multiTemplated.String())
+}
+
 func TestGetUnknownSelector(t *testing.T) {
+	SetTestContext(t)
+
 	fakePage := agouti.JoinPage("localhost")
 
 	single := get(fakePage, testData["foo"]["bar"]["baz"])
