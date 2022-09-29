d26c82a42ba47bc37acd1ac65e2cae4db70e6309
Simon <footless@gmail.com>
2022-09-29 16:36:02 +0200

    Fixes up tilt a bit (#1510)
    * fix up tilt a bit
    
    * More bootstrapping
    
    * Reorg files a bit
    
    * formatting
    
    * more formatting
    
    * disable tls

--- tools/reboot.sh
+++ tools/reboot.sh
@@ -13,6 +13,17 @@ export DELETE_GITOPS_DEV_REPO="${DELETE_GITOPS_DEV_REPO:-0}"
 
 TOOLS="$(pwd)/tools/bin"
 
+github_env_check() {
+  if [[ -z "$GITHUB_TOKEN" ]]; then
+    echo '!!! Missing GITHUB_TOKEN env var'
+    exit 1
+  fi
+  if [[ -z "$GITHUB_USER" ]]; then
+    echo '!!! Missing GITHUB_USER env var'
+    exit 1
+  fi
+}
+
 tool_check() {
   if [[ -f "${TOOLS}/${1}" ]]; then
     return
@@ -62,11 +73,32 @@ create_local_values_file(){
     > "$(dirname "$0")/dev-values-local.yaml"
 }
 
+add_files_to_git(){
+  tool_check "gh"
+  # We could use $GITHUB_REPO here, but its rm -rf so we'll be careful
+  rm -rf "/tmp/wge-dev"
+  gh repo clone "$GITHUB_USER/$GITHUB_REPO" "/tmp/$GITHUB_REPO"
+  mkdir -p "/tmp/$GITHUB_REPO/clusters/bases/rbac"
+  cp "$(dirname "$0")/git-files/wego-admin.yaml" "/tmp/$GITHUB_REPO/clusters/bases/rbac/wego-admin.yaml"
+  cd "/tmp/$GITHUB_REPO"
+  git add clusters/bases
+  git commit -m "Add wego-admin role"
+  git push origin main
+}
+
+# Steps we ask you to do in https://docs.gitops.weave.works/docs/cluster-management/getting-started/
+follow_capi_user_guide(){
+  add_files_to_git
+  kubectl create secret generic my-pat --from-literal GITHUB_TOKEN="$GITHUB_TOKEN" --from-literal GITHUB_USER="$GITHUB_USER" --from-literal GITHUB_REPO="$GITHUB_REPO"
+}
+
 main() {
+  github_env_check
   do_kind
   do_capi
   do_flux
   create_local_values_file
+  follow_capi_user_guide
 }
 
 main
