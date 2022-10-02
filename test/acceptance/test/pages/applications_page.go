df5f8a27ab2805be98a45aff6365e778ed64cad9
taghreed86 <tagreed_23@live.com>
2022-10-02 09:22:38 +0200

    add violations message field locator

--- test/acceptance/test/pages/applications_page.go
+++ test/acceptance/test/pages/applications_page.go
@@ -79,6 +79,10 @@ type NoDataRowInApplicationViolationsList struct {
 	NoDataRow *agouti.Selection
 }
 
+type AppViolationsMsgInList struct {
+	AppViolationsMsg *agouti.Selection
+}
+
 type ApplicationViolationsDetailsPage struct {
 	ViolationHeader      *agouti.Selection
 	PolicyName           *agouti.Selection
@@ -203,6 +207,12 @@ func GetApplicationGraph(webDriver *agouti.Page) *ApplicationGraph {
 func GetNoDataRowInApplicationViolationsList(WebDriver *agouti.Page) *NoDataRowInApplicationViolationsList {
 	return &NoDataRowInApplicationViolationsList{
 		NoDataRow: WebDriver.FindByXPath(`//td[contains(.,'No data')]`),
+	}
+}
+
+func GetAppViolationsMsgInList(webDriver *agouti.Page) *AppViolationsMsgInList {
+	return &AppViolationsMsgInList{
+		AppViolationsMsg: webDriver.FindByXPath(``),
 	}
 }
 
