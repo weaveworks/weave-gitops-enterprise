d461004064f11874666bb8470ef746bdf358a7af
Jordan Pellizzari <jordan@weave.works>
2022-10-03 10:19:38 -0700

    Add temporary useListFluxCRDs hook

--- ui-cra/src/components/Applications/FluxRuntime.tsx
+++ ui-cra/src/components/Applications/FluxRuntime.tsx
@@ -1,16 +1,40 @@
 import {
+  coreClient,
   FluxRuntime,
-  useListFluxCrds,
   useListFluxRuntimeObjects,
-} from '@weaveworks/weave-gitops-main';
+} from '@weaveworks/weave-gitops';
+import _ from 'lodash';
 import { FC } from 'react';
+import { useQuery } from 'react-query';
 import { ContentWrapper } from '../Layout/ContentWrapper';
 import { PageTemplate } from '../Layout/PageTemplate';
 
+export function useListFluxCrds(clusterName = 'management') {
+  // Importing the client directly rather than pulling from context.
+  // There was an issue with importing from weave-gitops-main conflicting with the official released package.
+  // Downside here is duplication and the hook/component is not testable.
+  // This should be a temporary fix until the useListFluxCrds hook is exported in an official release.
+  return useQuery('flux_crds', () => coreClient.ListFluxCrds({ clusterName }), {
+    retry: false,
+    refetchInterval: 5000,
+  });
+}
+
 const WGApplicationsFluxRuntime: FC = () => {
-  const { data, isLoading } = useListFluxRuntimeObjects();
-  const { data: crds, isLoading: crdsLoading } = useListFluxCrds();
-  
+  const { data, isLoading, error } = useListFluxRuntimeObjects();
+  const {
+    data: crds,
+    isLoading: crdsLoading,
+    error: crdError,
+  } = useListFluxCrds();
+
+  const errors = _.compact([
+    ...(data?.errors || []),
+    error,
+    ...(crds?.errors || []),
+    crdError,
+  ]);
+
   return (
     <PageTemplate
       documentTitle="Flux Runtime"
@@ -22,8 +46,8 @@ const WGApplicationsFluxRuntime: FC = () => {
         },
       ]}
     >
-      <ContentWrapper errors={data?.errors} loading={isLoading}>
-        <FluxRuntime deployments={data?.deployments} />
+      <ContentWrapper errors={errors as any} loading={isLoading || crdsLoading}>
+        <FluxRuntime deployments={data?.deployments} crds={crds?.crds} />
       </ContentWrapper>
     </PageTemplate>
   );
