4e77bfa205fc64bee80916e3bac8941248225c79
Saeed Fazal <84315051+saeedfazal@users.noreply.github.com>
2022-10-10 09:41:23 +0100

    Fix gitlab browser failures (#1662)
    * Downgraded chrombrowser and chromedriver for gitlab saas test run
    * Make filter selection more robust for flaky tests
    * Saving complete DOM on test failure to debug UI failures

--- test/acceptance/test/pages/search_page.go
+++ test/acceptance/test/pages/search_page.go
@@ -26,15 +26,8 @@ func GetSearchPage(webDriver *agouti.Page) *SearchPage {
 }
 
 func (s SearchPage) SelectFilter(filterType string, filterID string) {
-	gomega.Eventually(func(g gomega.Gomega) {
-		g.Eventually(s.FilterBtn.Click).Should(gomega.Succeed())
-		if ElementExist(s.FilterDialog) {
-			g.Eventually(s.FilterDialog, time.Second*5).Should(matchers.BeVisible())
-		} else {
-			g.Eventually(s.FilterBtn.Click).Should(gomega.Succeed())
-			g.Eventually(s.FilterDialog, time.Second*5).Should(matchers.BeVisible())
-		}
-	}, time.Minute, time.Second*5).Should(gomega.Succeed(), "Failed to select fileter: "+filterType)
+	gomega.Eventually(s.FilterBtn.Click).Should(gomega.Succeed())
+	gomega.Eventually(s.FilterDialog).Should(matchers.BeVisible())
 
 	filters := s.FilterDialog.AllByXPath(`//form/ul/li`)
 	fCount, _ := filters.Count()
@@ -42,13 +35,13 @@ func (s SearchPage) SelectFilter(filterType string, filterID string) {
 	for i := 0; i < fCount; i++ {
 		f := filters.At(i).FindByXPath(fmt.Sprintf(`//li/span[.="%s"]`, filterType))
 		if count, _ := f.Count(); count == 1 {
-
 			gomega.Eventually(func(g gomega.Gomega) {
 				g.Expect(filters.At(i).FindByXPath(fmt.Sprintf(`//input[@id="%s"]`, filterID)).Check()).Should(gomega.Succeed())
-				g.Expect(filters.At(i).FindByXPath(fmt.Sprintf(`//input[@id="%s"]/ancestor::span[contains(@class, "Mui-checked")]`, filterID))).Should(matchers.BeFound())
-			}).Should(gomega.Succeed(), "Failed to select cluster filter: "+filterID)
-
+				g.Eventually(filters.At(i).FindByXPath(fmt.Sprintf(`//input[@id="%s"]/ancestor::span[contains(@class, "Mui-checked")]`, filterID)), time.Second*5).Should(matchers.BeFound())
+			}, time.Second*30, time.Second*5).Should(gomega.Succeed(), "Failed to select cluster filter: "+filterID)
+			break
 		}
 	}
 
+	gomega.Expect(s.FilterBtn.Click()).Should(gomega.Succeed(), "Failed to close filter dialog")
 }
