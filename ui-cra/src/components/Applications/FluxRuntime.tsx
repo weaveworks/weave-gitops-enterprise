3399ea7db4e5a0caa46af58f51d02162f9f78998
Russ Parmer <russell.parmer@weave.works>
2022-10-17 07:46:04 -0700

    use newly exported 'useListFluxCrds' from core

--- ui-cra/src/components/Applications/FluxRuntime.tsx
+++ ui-cra/src/components/Applications/FluxRuntime.tsx
@@ -1,38 +1,22 @@
 import {
-  coreClient,
   FluxRuntime,
   useListFluxRuntimeObjects,
+  useListFluxCrds,
 } from '@weaveworks/weave-gitops';
 import _ from 'lodash';
 import { FC } from 'react';
-import { useQuery } from 'react-query';
 import { ContentWrapper } from '../Layout/ContentWrapper';
 import { PageTemplate } from '../Layout/PageTemplate';
 
-export function useListFluxCrds(clusterName = 'management') {
-  // Importing the client directly rather than pulling from context.
-  // There was an issue with importing from weave-gitops-main conflicting with the official released package.
-  // Downside here is duplication and the hook/component is not testable.
-  // This should be a temporary fix until the useListFluxCrds hook is exported in an official release.
-  return useQuery('flux_crds', () => coreClient.ListFluxCrds({ clusterName }), {
-    retry: false,
-    refetchInterval: 5000,
-  });
-}
-
 const WGApplicationsFluxRuntime: FC = () => {
   const { data, isLoading, error } = useListFluxRuntimeObjects();
-  const {
-    data: crds,
-    isLoading: crdsLoading,
-    error: crdError,
-  } = useListFluxCrds();
+  const { data: crds, isLoading: crdsLoading, error: crdsError } = useListFluxCrds();
 
   const errors = _.compact([
     ...(data?.errors || []),
     error,
     ...(crds?.errors || []),
-    crdError,
+    crdsError,
   ]);
 
   return (
@@ -46,7 +30,7 @@ const WGApplicationsFluxRuntime: FC = () => {
         },
       ]}
     >
-      <ContentWrapper errors={errors as any} loading={isLoading || crdsLoading}>
+      <ContentWrapper errors={errors} loading={isLoading || crdsLoading}>
         <FluxRuntime deployments={data?.deployments} crds={crds?.crds} />
       </ContentWrapper>
     </PageTemplate>
