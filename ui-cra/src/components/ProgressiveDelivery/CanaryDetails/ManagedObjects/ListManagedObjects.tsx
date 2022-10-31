8b39d8673da27fae2b5fe656d2cf6961c544939a
Jordan Pellizzari <jordan@weave.works>
2022-10-31 12:36:06 -0700

    Run formatter

--- ui-cra/src/components/ProgressiveDelivery/CanaryDetails/ManagedObjects/ListManagedObjects.tsx
+++ ui-cra/src/components/ProgressiveDelivery/CanaryDetails/ManagedObjects/ListManagedObjects.tsx
@@ -1,24 +1,25 @@
 import { Alert } from '@material-ui/lab';
 import { LoadingPage } from '@weaveworks/weave-gitops';
-import { useListFlaggerObjects, CanaryParams } from '../../../../contexts/ProgressiveDelivery';
-import { ManagedObjectsTable } from './ManagedObjectsTable';
+import {
+  CanaryParams,
+  useListFlaggerObjects,
+} from '../../../../contexts/ProgressiveDelivery';
 import { AlertListErrors } from '../../../Layout/AlertListErrors';
+import { ManagedObjectsTable } from './ManagedObjectsTable';
 
 type Props = CanaryParams;
 
 const ListManagedObjects = (props: Props) => {
-    const { error, data, isLoading } = useListFlaggerObjects(props);
+  const { error, data, isLoading } = useListFlaggerObjects(props);
 
-    return (
-        <>
-            <AlertListErrors errors={data?.errors}/>
-            {isLoading && <LoadingPage />}
-            {error && <Alert severity="error">{error.message}</Alert>}
-            {data?.objects &&
-                    <ManagedObjectsTable objects={data.objects} />
-            }
-        </>
-    );
+  return (
+    <>
+      <AlertListErrors errors={data?.errors} />
+      {isLoading && <LoadingPage />}
+      {error && <Alert severity="error">{error.message}</Alert>}
+      {data?.objects && <ManagedObjectsTable objects={data.objects} />}
+    </>
+  );
 };
 
 export default ListManagedObjects;
