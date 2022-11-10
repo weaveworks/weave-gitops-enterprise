5e4c30c68553a35c043ba9bcbb05aa4cc6068371
Joshua Israel <joshua.israel@weave.works>
2022-11-10 11:52:49 -0500

    extra url attr

--- ui-cra/src/components/GitOpsRun/index.tsx
+++ ui-cra/src/components/GitOpsRun/index.tsx
@@ -1,17 +1,13 @@
 import { ContentWrapper } from '../Layout/ContentWrapper';
 import { PageTemplate } from '../Layout/PageTemplate';
 
-import { Routes } from '../../utils/nav';
 import { GitOpsRunTable } from './GitOpsRunTable';
 import NoRunsMessage from './NoRunsMessage';
 
 const GitOpsRun = () => {
   const sessions: any[] = [];
   return (
-    <PageTemplate
-      documentTitle="GitOps Run"
-      path={[{ label: 'GitOps Run', url: Routes.GitOpsRun }]}
-    >
+    <PageTemplate documentTitle="GitOps Run" path={[{ label: 'GitOps Run' }]}>
       <ContentWrapper loading={false} errorMessage={undefined}>
         {sessions?.length ? (
           <GitOpsRunTable sessions={sessions} />
