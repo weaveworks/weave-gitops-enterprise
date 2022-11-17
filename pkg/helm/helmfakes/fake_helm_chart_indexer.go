e87929649491c0d7b0fa1c51bb6da6cc15df0bc8
Simon Howe <simon@weave.works>
2022-11-17 10:58:32 +0100

    helmfakes.NewFakeChartCache supports Reader iface too now

--- pkg/helm/helmfakes/fake_helm_chart_indexer.go
+++ pkg/helm/helmfakes/fake_helm_chart_indexer.go
@@ -2,6 +2,8 @@ package helmfakes
 
 import (
 	"context"
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -12,10 +14,11 @@ import (
 // NewFakeChartCache returns a new FakeChartCache
 // Great for testing!
 // Supports returning errors for each method
-// Use the hander ClusterRefToString to get the a key for a chart in the cache
+// Use the helper ClusterRefToString to get the a key for a chart in the cache
 func NewFakeChartCache(opts ...func(*FakeChartCache)) FakeChartCache {
 	fc := FakeChartCache{
-		Charts: make(map[string][]helm.Chart),
+		Charts:      make(map[string][]helm.Chart),
+		ChartValues: make(map[string][]byte),
 	}
 	for _, o := range opts {
 		o(&fc)
@@ -30,9 +33,16 @@ func WithCharts(key string, charts []helm.Chart) func(*FakeChartCache) {
 	}
 }
 
+func WithValues(key string, values []byte) func(*FakeChartCache) {
+	return func(fc *FakeChartCache) {
+		fc.ChartValues[key] = []byte(base64.StdEncoding.EncodeToString(values))
+	}
+}
+
 // fake cache implementation
 type FakeChartCache struct {
 	Charts                         map[string][]helm.Chart
+	ChartValues                    map[string][]byte
 	AddChartError                  error
 	DeleteError                    error
 	DeleteAllChartsForClusterError error
@@ -79,6 +89,49 @@ func (fc FakeChartCache) DeleteAllChartsForCluster(ctx context.Context, clusterR
 		}
 	}
 	return nil
+}
+
+// Reader interface
+
+func (fc FakeChartCache) ListChartsByRepositoryAndCluster(ctx context.Context, clusterRef types.NamespacedName, repoRef helm.ObjectReference, kind string) ([]helm.Chart, error) {
+	charts, ok := fc.Charts[ClusterRefToString(repoRef, clusterRef)]
+	if !ok {
+		return nil, errors.New("no charts found")
+	}
+	// filter by kind
+	var filtered []helm.Chart
+	for _, c := range charts {
+		if c.Kind == kind {
+			filtered = append(filtered, c)
+		}
+	}
+	return filtered, nil
+}
+func (fc FakeChartCache) IsKnownChart(ctx context.Context, clusterRef types.NamespacedName, repoRef helm.ObjectReference, chart helm.Chart) (bool, error) {
+	charts, ok := fc.Charts[ClusterRefToString(repoRef, clusterRef)]
+	if !ok {
+		return false, nil
+	}
+	for _, c := range charts {
+		if c.Name == chart.Name && c.Version == chart.Version {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+func (fc FakeChartCache) GetChartValues(ctx context.Context, clusterRef types.NamespacedName, repoRef helm.ObjectReference, chart helm.Chart) ([]byte, error) {
+	if values, ok := fc.ChartValues[ChartRefToString(repoRef, clusterRef, chart)]; ok {
+		return values, nil
+	}
+	return nil, nil
+}
+func (fc FakeChartCache) UpdateValuesYaml(ctx context.Context, clusterRef types.NamespacedName, repoRef helm.ObjectReference, chart helm.Chart, valuesYaml []byte) error {
+	fc.ChartValues[ChartRefToString(repoRef, clusterRef, chart)] = valuesYaml
+	return nil
+}
+
+func ChartRefToString(or helm.ObjectReference, cr types.NamespacedName, c helm.Chart) string {
+	return fmt.Sprintf("%s_%s_%s_%s_%s_%s_%s", or.Kind, or.Name, or.Namespace, cr.Name, cr.Namespace, c.Name, c.Version)
 }
 
 func ClusterRefToString(or helm.ObjectReference, cr types.NamespacedName) string {
