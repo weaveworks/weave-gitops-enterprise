93928b244b8e37c2ff7e1f820dc172bf79fbb9e6
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2022-11-24 13:53:34 +0200

    capitalize tooltip txt

--- ui-cra/src/components/Policies/Mode.tsx
+++ ui-cra/src/components/Policies/Mode.tsx
@@ -6,6 +6,7 @@ interface IModeProps {
   modeName: string,
   showName?: boolean 
 }
+const capitalizeFirstLetter =( strToCapitalize:string) =>  strToCapitalize.charAt(0).toUpperCase() + strToCapitalize.slice(1);
 
 function Mode({ modeName, showName = false }: IModeProps) {
   switch (modeName.toLocaleLowerCase()) {
@@ -27,7 +28,7 @@ const ModeTooltip = (mode: string, showName: boolean, icon: any) => {
     <>
       {!showName ? (
         <ModeWrapper>
-          <span title={mode}>{icon}</span>
+          <span title={capitalizeFirstLetter(mode)}>{icon}</span>
         </ModeWrapper>
       ) : (
         <ModeWrapper>
