96d8af8d4a94df2456d0f876e0b68b123a635c41
Sara Elzayat <sara@weave.works>
2022-12-02 17:13:59 +0200

    linting

--- pkg/helm/charts.go
+++ pkg/helm/charts.go
@@ -1,15 +1,10 @@
 package helm
 
 import (
-	"context"
 	"fmt"
 	"sort"
 
 	"github.com/Masterminds/semver"
-	sourcev1 "github.com/fluxcd/source-controller/api/v1beta2"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func ReverseSemVerSort(versions []string) ([]string, error) {
@@ -32,23 +27,4 @@ func ReverseSemVerSort(versions []string) ([]string, error) {
 	}
 
 	return result, nil
-}
-
-func credsForRepository(ctx context.Context, kc client.Client, hr *sourcev1.HelmRepository) (string, string, error) {
-	var secret corev1.Secret
-	if err := kc.Get(ctx, types.NamespacedName{Name: hr.Spec.SecretRef.Name, Namespace: hr.Namespace}, &secret); err != nil {
-		return "", "", fmt.Errorf("repository authentication: %w", err)
-	}
-
-	return string(secret.Data["username"]), string(secret.Data["password"]), nil
-}
-
-func hasAnnotation(cm map[string]string, name string) bool {
-	for k := range cm {
-		if k == name {
-			return true
-		}
-	}
-
-	return false
 }
