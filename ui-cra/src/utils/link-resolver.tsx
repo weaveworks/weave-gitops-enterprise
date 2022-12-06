56c68cfb61feb5ab7a62ee99287d18e9aa5db0da
Casper <ahmed.shabaan@weave.works>
2022-12-06 20:45:13 +0200

    fix:  Clicking on an object in a details table results in error (#2010)
    * fix:  Clicking on an object in a details table results in error
    
    * set resolver return value to ''
    
    * Fix passing the Kind as a path in Application/source pages

--- ui-cra/src/utils/link-resolver.tsx
+++ ui-cra/src/utils/link-resolver.tsx
@@ -1,4 +1,5 @@
 // FIXME: remove this when core fixes requiring a linkResolver function
 export const resolver = (path: string, params?: any) => {
-  return path;
+  // Fix Kind as a path 
+  return path.includes('/') ? path : '';
 };
