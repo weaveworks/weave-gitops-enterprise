21a732ac452a6a0df98a5f16762867801d0e6c33
Sara Elzayat <sara@weave.works>
2022-12-13 23:55:28 +0200

    Linting.

--- cmd/gitops/app/create/templates/cmd_test.go
+++ cmd/gitops/app/create/templates/cmd_test.go
@@ -41,7 +41,7 @@ func Test_parseTemplate(t *testing.T) {
 					},
 					ResourceTemplates: []templates.ResourceTemplate{
 						{
-							runtime.RawExtension{
+							RawExtension: runtime.RawExtension{
 								Raw: []byte(`{"apiVersion":"kustomize.toolkit.fluxcd.io/v1beta2","kind":"Kustomization","metadata":{"name":"${RESOURCE_NAME}","namespace":"${NAMESPACE}"},"spec":{"interval":"1h","path":"${TEMPLATE_PATH}","sourceRef":{"kind":"GitRepository","name":"${GIT_REPO_NAME}","namespace":"${GIT_REPO_NAMESPACE}"}}}`),
 							},
 						},
