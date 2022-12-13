7f014cb1fb17696ac75562c5a2728a4901c75417
Casper <ahmed.shabaan@weave.works>
2022-12-13 14:35:05 +0200

    Cleanup content wrapper UI component (#2061)
    * Memoized NavItems to save renders
    
    * clean up contetn wrapper component
    
    * align navigation box with contetn wrapper in height
    
    * Memoized HelpLinkWrapper & move footer to separate component
    
    * snap
    
    * move providers to App to avoid refetching in auth cases

---
+++ ui-cra/src/contexts/ListConfig/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  ListConfigResponse,
+  useListConfig,
+  useListVersion,
+} from '../../hooks/versions';
+
+export const ListConfigContext = React.createContext<ListConfigResponse | null>(
+  null,
+);
+
+export const ListConfigProvider = ({ children }: { children: any }) => {
+  const listConfig = useListConfig();
+  return (
+    <ListConfigContext.Provider value={listConfig}>
+      {children}
+    </ListConfigContext.Provider>
+  );
+};
+
+export const useListConfigContext = () => React.useContext(ListConfigContext);
+
+// Use react contexct to share the Version in footer component to save some renders
+export const VersionContext = React.createContext<
+  | {
+      data: any;
+      entitlement: string | null;
+    }
+  | undefined
+>(undefined);
+
+export const VersionProvider = ({ children }: { children: any }) => {
+  const { data } = useListVersion();
+
+  return (
+    <VersionContext.Provider value={data}>{children}</VersionContext.Provider>
+  );
+};
+
+export const useVersionContext = () => React.useContext(VersionContext);
