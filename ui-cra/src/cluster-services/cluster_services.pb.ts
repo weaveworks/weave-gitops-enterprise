29ae5d1c1cbeac691e3ec3620cba24e345055b23
Ahmad Ali <ahmadsmair1194@gmail.com>
2022-12-18 16:13:03 +0200

    Add missing fields for secret details

--- ui-cra/src/cluster-services/cluster_services.pb.ts
+++ ui-cra/src/cluster-services/cluster_services.pb.ts
@@ -734,11 +734,13 @@ export type GetExternalSecretRequest = {
 
 export type GetExternalSecretResponse = {
   secretName?: string
+  externalSecretName?: string
   clusterName?: string
   namespace?: string
   secretStore?: string
-  externalSecretName?: string
   secretPath?: string
+  property?: string
+  version?: string
 }
 
 export class ClustersService {
