7902c1968e2a248b34f6af44305ef719f3e18a6d
taghreed86 <tagreed_23@live.com>
2022-12-19 18:33:31 +0200

    change the way how to create and delete workspaces

--- test/acceptance/test/ui_workspaces.go
+++ test/acceptance/test/ui_workspaces.go
@@ -11,9 +11,24 @@ import (
 	"github.com/weaveworks/weave-gitops-enterprise/test/acceptance/test/pages"
 )
 
-func installTestWorkspaces(clusterName string) {
-	ginkgo.By(fmt.Sprintf("Add workspaces to the %s cluster", clusterName), func() {
-		createTenant(path.Join(testDataPath, "tenancy", "multiple-tenant.yaml"))
+func installTestWorkspaces(clusterName string, workspacesYaml string) {
+	ginkgo.By(fmt.Sprintf("Add test workspaces to the %s cluster", clusterName), func() {
+		// createTenant(path.Join(testDataPath, "tenancy", "multiple-tenant.yaml"))
+		_, stdErr := runGitopsCommand(fmt.Sprintf(`go run cmd/gitops/main.go create tenants --from-file %s --prune`, workspacesYaml))
+		gomega.Expect(stdErr).Should(gomega.BeEmpty(), fmt.Sprintf("Failed to add test workspaces to cluster '%s'", clusterName))
+		//err := runCommandPassThrough("kubectl", "apply", "-f", workspacesYaml)
+		//gomega.Expect(err).ShouldNot(gomega.HaveOccurred(), fmt.Sprintf("Failed to install test policies to cluster '%s'", clusterName))
+
+	})
+}
+
+func deleteTestWorkspaces(clusterName string, toBeDeletedWorkspacesYaml string) {
+	ginkgo.By(fmt.Sprintf("And Finally delete test workspaces from %s cluster", clusterName), func() {
+		// createTenant(path.Join(testDataPath, "tenancy", "multiple-tenant.yaml"))
+		_, stdErr := runGitopsCommand(fmt.Sprintf(`go run cmd/gitops/main.go create tenants --from-file %s --prune`, toBeDeletedWorkspacesYaml))
+		gomega.Expect(stdErr).Should(gomega.BeEmpty(), fmt.Sprintf("Failed to add test workspaces to cluster '%s'", clusterName))
+		//err := runCommandPassThrough("kubectl", "apply", "-f", workspacesYaml)
+		//gomega.Expect(err).ShouldNot(gomega.HaveOccurred(), fmt.Sprintf("Failed to install test policies to cluster '%s'", clusterName))
 
 	})
 }
@@ -74,24 +89,26 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 	})
 
 	ginkgo.Context("[UI] Workspaces can be configured on management cluster", func() {
-
+		var workspacesYaml string
+		var toBeDeletedWorkspacesYaml string
 		workspaceName := "test-team"
 		workspaceNamespaces := "test-kustomization, test-system"
 		workspaceClusterName := "management"
 
 		ginkgo.JustBeforeEach(func() {
-			installTestWorkspaces("management")
+			workspacesYaml = path.Join(testDataPath, "tenancy/multiple-tenant.yaml")
+			toBeDeletedWorkspacesYaml = path.Join(testDataPath, "tenancy/to-be-deleted-tenant.yaml")
 
 		})
 
 		ginkgo.JustAfterEach(func() {
 
-			defer deleteTenants([]string{getTenantYamlPath()})
+			deleteTestWorkspaces("management", toBeDeletedWorkspacesYaml)
 		})
 
 		ginkgo.FIt("Verify Workspaces can be configured on management cluster and dashboard is updated accordingly", ginkgo.Label("integration", "workspaces"), func() {
+			installTestWorkspaces("management", workspacesYaml)
 			existingWorkspacesCount := getWorkspacesCount()
-			fmt.Printf("existing number of workspaces int the list is '%v'", existingWorkspacesCount)
 
 			pages.NavigateToPage(webDriver, "Workspaces")
 			WorkspacesPage := pages.GetWorkspacesPage(webDriver)
@@ -99,13 +116,13 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 			ginkgo.By("And wait for workspaces to be visibe on the dashboard", func() {
 				gomega.Eventually(WorkspacesPage.WorkspaceHeader).Should(matchers.BeVisible())
 
-				// totalWorkspacesCount := existingWorkspacesCount + 2
-				// gomega.Eventually(func(g gomega.Gomega) int {
-				// 	gomega.Expect(webDriver.Refresh()).ShouldNot(gomega.HaveOccurred())
-				// 	time.Sleep(POLL_INTERVAL_1SECONDS)
-				// 	return WorkspacesPage.CountWorkspaces()
-				// }, ASSERTION_2MINUTE_TIME_OUT, POLL_INTERVAL_3SECONDS).Should(gomega.Equal(totalWorkspacesCount), fmt.Sprintf("There should be '%d' workspaces in Workspaces table but found '%d'", totalWorkspacesCount, existingWorkspacesCount))
-
+				totalWorkspacesCount := existingWorkspacesCount + 2
+				gomega.Eventually(func(g gomega.Gomega) int {
+					gomega.Expect(webDriver.Refresh()).ShouldNot(gomega.HaveOccurred())
+					time.Sleep(POLL_INTERVAL_1SECONDS)
+					return WorkspacesPage.CountWorkspaces()
+				}, ASSERTION_2MINUTE_TIME_OUT, POLL_INTERVAL_3SECONDS).Should(gomega.Equal(totalWorkspacesCount), fmt.Sprintf("There should be '%d' workspaces in Workspaces table but found '%d'", totalWorkspacesCount, existingWorkspacesCount))
+				fmt.Printf("existing number of workspaces int the list is '%v'", existingWorkspacesCount)
 			})
 
 			workspaceInfo := WorkspacesPage.FindWorkspacInList(workspaceName)
@@ -132,6 +149,9 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 		var leafClusterkubeconfig string
 		var clusterBootstrapCopnfig string
 		var gitopsCluster string
+		var workspacesYaml string
+		var toBeDeletedWorkspacesYaml string
+
 		patSecret := "workspace-pat"
 		bootstrapLabel := "bootstrap"
 		leafClusterName := "workspaces-leaf-cluster-test"
@@ -147,6 +167,8 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 			if !pages.ElementExist(pages.Navbar(webDriver).Title, 3) {
 				loginUser()
 			}
+			workspacesYaml = path.Join(testDataPath, "tenancy/multiple-tenant.yaml")
+			toBeDeletedWorkspacesYaml = path.Join(testDataPath, "tenancy/to-be-deleted-tenant.yaml")
 			mgmtClusterContext, _ = runCommandAndReturnStringOutput("kubectl config current-context")
 			createCluster("kind", leafClusterName, "")
 			leafClusterContext, _ = runCommandAndReturnStringOutput("kubectl config current-context")
@@ -161,7 +183,7 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 
 			deleteCluster("kind", leafClusterName, "")
 			// Delete the test workspaces
-			defer deleteTenants([]string{getTenantYamlPath()})
+			deleteTestWorkspaces(leafClusterName, toBeDeletedWorkspacesYaml)
 
 		})
 
@@ -171,7 +193,7 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 
 			// Install policy agent ,and workspaces on leaf cluster
 			installPolicyAgent(leafClusterName)
-			installTestWorkspaces(leafClusterName)
+			installTestWorkspaces(leafClusterName, workspacesYaml)
 
 			useClusterContext(mgmtClusterContext)
 			createPATSecret(leafClusterNamespace, patSecret)
