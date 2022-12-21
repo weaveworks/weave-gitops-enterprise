a53c3a806d31b4d77c908073b73997294949bfb0
taghreed86 <tagreed_23@live.com>
2022-12-21 11:43:13 +0200

    remove F letter

--- test/acceptance/test/ui_workspaces.go
+++ test/acceptance/test/ui_workspaces.go
@@ -105,7 +105,7 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 			deleteTestWorkspaces("management", toBeDeletedWorkspacesYaml)
 		})
 
-		ginkgo.FIt("Verify Workspaces can be configured on management cluster and dashboard is updated accordingly", ginkgo.Label("integration", "workspaces"), func() {
+		ginkgo.It("Verify Workspaces can be configured on management cluster and dashboard is updated accordingly", ginkgo.Label("integration", "workspaces"), func() {
 			existingWorkspacesCount := getWorkspacesCount()
 			// Install test workspaces on management cluster
 			installTestWorkspaces("management", workspacesYaml)
