83c3d1de2651958adc6bd8d74d2c01ebd9f5f580
Hagarmagdyy <Hagar.magdy@magalix.com>
2023-01-10 01:33:36 +0200

    Run the remaining tests

--- test/acceptance/test/ui_workspaces.go
+++ test/acceptance/test/ui_workspaces.go
@@ -124,7 +124,7 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 			deleteWorkspaces("management")
 		})
 
-		ginkgo.FIt("Verify Workspaces can be configured on management cluster and dashboard is updated accordingly", func() {
+		ginkgo.It("Verify Workspaces can be configured on management cluster and dashboard is updated accordingly", func() {
 			existingWorkspacesCount := getWorkspacesCount()
 			// Install workspaces on management cluster
 			installWorkspaces("management", workspacesYaml)
