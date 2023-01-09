c3588818c5a61ef138e26d90b5c543209a0bcc60
serboctor <boctor.sarah@gmail.com>
2023-01-09 15:55:02 +0200

    Modify workspaces selector to select button by xpath

--- test/acceptance/test/pages/workspaces_page.go
+++ test/acceptance/test/pages/workspaces_page.go
@@ -88,7 +88,7 @@ func GetWorkspacesPage(webDriver *agouti.Page) *WorkspacesPage {
 func GetWorkspaceDetailsPage(webDriver *agouti.Page) *WorkspaceDetailsPage {
 	return &WorkspaceDetailsPage{
 		Header:                    webDriver.FindByXPath(`//div[@role="heading"]/a[@href="/workspaces"]/parent::node()/parent::node()/following-sibling::div`),
-		GoToTenantApplicationsBtn: webDriver.FindByClass(`MuiButtonBase-root MuiButton-root MuiButton-outlined Button-sc-1w0plxi-0 fOXjJl jss88 MuiButton-outlinedPrimary MuiButton-disableElevation`),
+		GoToTenantApplicationsBtn: webDriver.FindByXPath(`/html/body/div[1]/div[1]/main/div/div[2]/div[1]/div[1]/div[1]`),
 		WorkspaceName:             webDriver.FindByXPath(`//div[@data-testid='Workspace Name']`),
 		Namespaces:                webDriver.FindByXPath(`//div[@data-testid='Namespaces']`),
 		ServiceAccountsTab:        webDriver.First(`div[role="tablist"] a[href*="/workspaces/details/serviceAccounts?"]`),
