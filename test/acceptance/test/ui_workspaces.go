3149227faa873bdb9efb2b4164e98012cb617d10
Hagarmagdyy <Hagar.magdy@magalix.com>
2023-01-15 04:31:34 +0200

    Run

--- test/acceptance/test/ui_workspaces.go
+++ test/acceptance/test/ui_workspaces.go
@@ -108,7 +108,7 @@ func verifyWrokspaceRoles(workspaceName string, WorkspaceNamespaces string) {
 
 		gomega.Eventually(Role.Name.Text).Should(gomega.MatchRegexp(workspaceName), fmt.Sprintf("Failed to verify '%s' workspace Role 's Name", workspaceName))
 		gomega.Expect(Role.Name.Click()).Should(gomega.Succeed(), fmt.Sprintf("Failed to open '%s' workspace's Roles Name", workspaceName))
-		pages.WaitForPageToLoad(webDriver)
+		// pages.WaitForPageToLoad(webDriver)
 		gomega.Eventually(Role.RoleApi.Text).Should(gomega.Equal("apiVersion"), "Failed to verify Role Manifest's apiVersion ")
 		gomega.Expect(Role.ManifestCloseBtn.Click()).Should(gomega.Succeed(), fmt.Sprintf("Failed to Close '%s' workspace's Roles manifest", workspaceName))
 		gomega.Eventually(Role.Age.Text).ShouldNot(gomega.BeEmpty(), fmt.Sprintf("Failed to verify '%s' workspace Roles's Age", workspaceName))
