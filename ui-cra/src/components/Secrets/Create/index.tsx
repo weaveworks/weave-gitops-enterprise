778071670a3ecb5c80ea91a0529d9e8645e663ee
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2023-01-18 16:37:58 +0200

    add context provider

--- ui-cra/src/components/Secrets/Create/index.tsx
+++ ui-cra/src/components/Secrets/Create/index.tsx
@@ -13,7 +13,6 @@ import GitOps from '../../Templates/Form/Partials/GitOps';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { localEEMuiTheme } from '../../../muiTheme';
 import { useGetSecretStoreDetails } from '../../../contexts/Secrets';
-import { useListConfigContext } from '../../../contexts/ListConfig';
 import { useHistory } from 'react-router-dom';
 import {
   Button,
@@ -23,16 +22,17 @@ import {
   theme,
   GitRepository,
   useListSources,
-
 } from '@weaveworks/weave-gitops';
 import { isUnauthenticated, removeToken } from '../../../utils/request';
-import { AddApplicationRequest } from '../../Applications/utils';
+import { CreateDeploymentObjects } from '../../Applications/utils';
 import {
   getInitialGitRepo,
   getRepositoryUrl,
 } from '../../Templates/Form/utils';
 import { GitRepositoryEnriched } from '../../Templates/Form';
 import { getGitRepos } from '../../Clusters';
+import CallbackStateContextProvider from '../../../contexts/GitAuth/CallbackStateContext';
+import { PageRoute } from '@weaveworks/weave-gitops/ui/lib/types';
 
 const { medium, large } = theme.spacing;
 const { neutral20, neutral10 } = theme.colors;
@@ -131,7 +131,7 @@ const CreateSecret = () => {
   const callbackState = useCallbackState();
   const random = useMemo(() => Math.random().toString(36).substring(7), []);
   const { initialFormData } = getInitialData(callbackState, random);
-
+  const authRedirectPage = `/secrets/create`;
 
   const [loading, setLoading] = useState<boolean>(false);
   const [isclusterSelected, setIsclusterSelected] = useState<boolean>(false);
@@ -143,10 +143,7 @@ const CreateSecret = () => {
   const [enableCreatePR, setEnableCreatePR] = useState<boolean>(false);
 
   const { data } = useListSources();
-  const gitRepos = useMemo(
-    () => getGitRepos(data?.result),
-    [data?.result],
-  );
+  const gitRepos = useMemo(() => getGitRepos(data?.result), [data?.result]);
   const initialGitRepo = getInitialGitRepo(
     null,
     gitRepos,
@@ -178,7 +175,6 @@ const CreateSecret = () => {
       }));
     }
   }, [initialGitRepo, formData.repo]);
-
 
   const handleSelectSecretStore = (event: React.ChangeEvent<any>) => {
     const sercetStore = event.target.value;
@@ -241,8 +237,7 @@ const CreateSecret = () => {
     });
   };
 
-
-  const handleCreateSecret = useCallback(() => {
+  const getClusterAutomations = useCallback(() => {
     let clusterAutomations: ClusterAutomation[] = [];
     clusterAutomations.push({
       cluster: {
@@ -273,16 +268,29 @@ const CreateSecret = () => {
         },
       },
     });
+  }, [
+    cluster_name,
+    cluster_namespace,
+    isControlPlane,
+    secret_name,
+    secret_namespace,
+    secretStoreRef,
+    data_secretKey,
+    data_remoteRef_key,
+    data_remoteRef_property,
+  ]);
+
+  const handleCreateSecret = useCallback(() => {
     const payload = {
       headBranch: formData.branchName,
       title: formData.pullRequestTitle,
       description: formData.pullRequestDescription,
       commitMessage: formData.commitMessage,
-      clusterAutomations: clusterAutomations,
+      clusterAutomations: getClusterAutomations(),
       repositoryUrl: getRepositoryUrl(formData.repo),
     };
     setLoading(true);
-    return AddApplicationRequest(payload, getProviderToken(formData.provider))
+    return CreateDeploymentObjects(payload, getProviderToken(formData.provider))
       .then(response => {
         history.push(Routes.Secrets);
         setNotifications([
@@ -323,118 +331,129 @@ const CreateSecret = () => {
           { label: 'Create new secret' },
         ]}
       >
-        <ContentWrapper>
-          <FormWrapper
-            noValidate
-            onSubmit={event =>
-              validateFormData(
-                event,
-                handleCreateSecret,
-                setFormError,
-                setSubmitType,
-              )
-            }
-          >
-            <div className="group-section">
-              <div className="form-group">
+        <CallbackStateContextProvider
+          callbackState={{
+            page: authRedirectPage as PageRoute,
+            state: {
+              formData,
+            },
+          }}
+        >
+          <ContentWrapper>
+            <FormWrapper
+              noValidate
+              onSubmit={event =>
+                validateFormData(
+                  event,
+                  handleCreateSecret,
+                  setFormError,
+                  setSubmitType,
+                )
+              }
+            >
+              <div className="group-section">
+                <div className="form-group">
+                  <Input
+                    className="form-section"
+                    required
+                    name="secret_name"
+                    label="EXTERNAL SECRET NAME"
+                    value={secret_name}
+                    onChange={event => handleFormData(event, 'secret_name')}
+                    error={formError === 'secret_name' && !secret_name}
+                  />
+                  <Input
+                    className="form-section"
+                    required
+                    name="data_secretKey"
+                    label="TARGET K8s SECRET NAME"
+                    value={data_secretKey}
+                    onChange={event => handleFormData(event, 'data_secretKey')}
+                    error={formError === 'data_secretKey' && !data_secretKey}
+                  />
+                  <Select
+                    className="form-section"
+                    name="cluster_name"
+                    required={true}
+                    label="TARGET CLUSTER"
+                    value={target_Cluster}
+                    onChange={HandleSelectCluster}
+                    error={formError === 'cluster_name' && !cluster_name}
+                  >
+                    {isClustersLoading ? (
+                      <MenuItem disabled={true}>Loading...</MenuItem>
+                    ) : (
+                      clusters?.map((option, index: number) => {
+                        return (
+                          <MenuItem key={index} value={JSON.stringify(option)}>
+                            {option.name}
+                          </MenuItem>
+                        );
+                      })
+                    )}
+                  </Select>
+                </div>
+                {isclusterSelected && (
+                  <SelectSecretStore
+                    cluster={cluster_name}
+                    secretStoreValue={secretStoreValue}
+                    handleSelectSecretStore={handleSelectSecretStore}
+                    formError={formError}
+                    secretStoreRef={secretStoreRef}
+                    secretStoreType={secretStoreType}
+                    secret_namespace={secret_namespace}
+                  />
+                )}
                 <Input
                   className="form-section"
                   required
-                  name="secret_name"
-                  label="EXTERNAL SECRET NAME"
-                  value={secret_name}
-                  onChange={event => handleFormData(event, 'secret_name')}
-                  error={formError === 'secret_name' && !secret_name}
+                  name="data_remoteRef_key"
+                  label="SECRET PATH"
+                  value={data_remoteRef_key}
+                  onChange={event =>
+                    handleFormData(event, 'data_remoteRef_key')
+                  }
+                  error={
+                    formError === 'data_remoteRef_key' && !data_remoteRef_key
+                  }
                 />
                 <Input
                   className="form-section"
                   required
-                  name="data_secretKey"
-                  label="TARGET K8s SECRET NAME"
-                  value={data_secretKey}
-                  onChange={event => handleFormData(event, 'data_secretKey')}
-                  error={formError === 'data_secretKey' && !data_secretKey}
+                  name="data_remoteRef_property"
+                  label="PROPERTY"
+                  value={data_remoteRef_property}
+                  onChange={event =>
+                    handleFormData(event, 'data_remoteRef_property')
+                  }
+                  error={
+                    formError === 'data_remoteRef_property' &&
+                    !data_remoteRef_property
+                  }
                 />
-                <Select
-                  className="form-section"
-                  name="cluster_name"
-                  required={true}
-                  label="TARGET CLUSTER"
-                  value={target_Cluster}
-                  onChange={HandleSelectCluster}
-                  error={formError === 'cluster_name' && !cluster_name}
-                >
-                  {isClustersLoading ? (
-                    <MenuItem disabled={true}>Loading...</MenuItem>
-                  ) : (
-                    clusters?.map((option, index: number) => {
-                      return (
-                        <MenuItem key={index} value={JSON.stringify(option)}>
-                          {option.name}
-                        </MenuItem>
-                      );
-                    })
-                  )}
-                </Select>
               </div>
-              {isclusterSelected && (
-                <SelectSecretStore
-                  cluster={cluster_name}
-                  secretStoreValue={secretStoreValue}
-                  handleSelectSecretStore={handleSelectSecretStore}
-                  formError={formError}
-                  secretStoreRef={secretStoreRef}
-                  secretStoreType={secretStoreType}
-                  secret_namespace={secret_namespace}
-                />
-              )}
-              <Input
-                className="form-section"
-                required
-                name="data_remoteRef_key"
-                label="SECRET PATH"
-                value={data_remoteRef_key}
-                onChange={event => handleFormData(event, 'data_remoteRef_key')}
-                error={
-                  formError === 'data_remoteRef_key' && !data_remoteRef_key
-                }
+              <GitOps
+                formData={formData}
+                setFormData={setFormData}
+                showAuthDialog={showAuthDialog}
+                setShowAuthDialog={setShowAuthDialog}
+                setEnableCreatePR={setEnableCreatePR}
+                formError={formError}
+                enableGitRepoSelection={true}
               />
-              <Input
-                className="form-section"
-                required
-                name="data_remoteRef_property"
-                label="PROPERTY"
-                value={data_remoteRef_property}
-                onChange={event =>
-                  handleFormData(event, 'data_remoteRef_property')
-                }
-                error={
-                  formError === 'data_remoteRef_property' &&
-                  !data_remoteRef_property
-                }
-              />
-            </div>
-            <GitOps
-              formData={formData}
-              setFormData={setFormData}
-              showAuthDialog={showAuthDialog}
-              setShowAuthDialog={setShowAuthDialog}
-              setEnableCreatePR={setEnableCreatePR}
-              formError={formError}
-              enableGitRepoSelection={true}
-            />
 
-            {loading ? (
-              <LoadingPage className="create-loading" />
-            ) : (
-              <div className="create-cta">
-                <Button type="submit" disabled={!enableCreatePR}>
-                  CREATE PULL REQUEST
-                </Button>
-              </div>
-            )}
-          </FormWrapper>
-        </ContentWrapper>
+              {loading ? (
+                <LoadingPage className="create-loading" />
+              ) : (
+                <div className="create-cta">
+                  <Button type="submit" disabled={!enableCreatePR}>
+                    CREATE PULL REQUEST
+                  </Button>
+                </div>
+              )}
+            </FormWrapper>
+          </ContentWrapper>
+        </CallbackStateContextProvider>
       </PageTemplate>
     </ThemeProvider>
   );
