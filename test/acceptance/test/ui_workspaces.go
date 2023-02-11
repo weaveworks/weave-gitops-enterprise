2a7d4bf14826496cf9eb81586c559948ebc8ecc0
Hagarmagdyy <Hagar.magdy@magalix.com>
2023-02-11 17:09:57 +0200

    add workspace namespace

--- test/acceptance/test/ui_workspaces.go
+++ test/acceptance/test/ui_workspaces.go
@@ -253,7 +253,7 @@ var _ = ginkgo.Describe("Multi-Cluster Control Plane Workspaces", ginkgo.Label("
 		}
 
 		workspaceName := "test-team"
-		workspaceNamespaces := "test-kustomization"
+		workspaceNamespaces := "test-kustomization, test-system"
 		workspaceClusterName := leafCluster.Name
 
 		ginkgo.JustBeforeEach(func() {
