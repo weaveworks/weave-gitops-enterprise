75c7f5432e2a7641995845f6746b09b92183987a
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2023-02-14 10:19:24 +0200

    Workspace details unit test

--- ui-cra/src/components/Workspaces/__tests__/WorkspaceDetails.test.tsx
+++ ui-cra/src/components/Workspaces/__tests__/WorkspaceDetails.test.tsx
@@ -1,13 +1,19 @@
-import { act, fireEvent, render, screen } from '@testing-library/react';
+import {
+  act,
+  fireEvent, render,
+  screen
+} from '@testing-library/react';
 import moment from 'moment';
+import { WorkspaceRoleBindingSubject } from '../../../cluster-services/cluster_services.pb';
 import EnterpriseClientProvider from '../../../contexts/EnterpriseClient/Provider';
 import {
-  defaultContexts,
-  WorkspaceClientMock,
-  withContext,
-  TestFilterableTable,
+  defaultContexts, TestFilterableTable, withContext, WorkspaceClientMock
 } from '../../../utils/test-utils';
 import WorkspaceDetails from '../WorkspaceDetails';
+import { PoliciesTab } from '../WorkspaceDetails/Tabs/Policies';
+import { RoleBindingsTab } from '../WorkspaceDetails/Tabs/RoleBindings';
+import { RolesTab } from '../WorkspaceDetails/Tabs/Roles';
+import { ServiceAccountsTab } from '../WorkspaceDetails/Tabs/ServiceAccounts';
 const MockWorkspaceResponse = {
   workspace: {
     name: 'dev-team',
@@ -42,6 +48,88 @@ const mappedServiceAccounts = (serviceAccounts: Array<any>) => {
     moment(e.timestamp).fromNow(),
   ]);
 };
+
+const MockRoles = {
+  name: 'service One',
+  clusterName: 'Managemnet',
+  objects: [
+    {
+      name: 'Role one',
+      namespacs: 'flux',
+      rules: [{ groups: ['group1'], resources: ['res'], verbs: ['verb'] }],
+      manifest: '',
+      timestamp: '2022-07-30T11:23:55Z',
+    },
+  ],
+  total: 1,
+  errors: [],
+};
+const mappedRoles = (roles: Array<any>) => {
+  return roles.map(e => [
+    e.name,
+    e.namespace || '-',
+    'View Rules',
+    moment(e.timestamp).fromNow(),
+  ]);
+};
+
+const MockRoleBinding = {
+  name: 'Role Binding one',
+  clusterName: 'Managemnet',
+  objects: [
+    {
+      name: 'Role Binding one',
+      namespacs: 'flux',
+      manifest: '',
+      timestamp: '2022-07-30T11:23:55Z',
+      role: { apiGroups: 'group1', kind: 'res', name: 'verb' },
+      subjects: [
+        {
+          apiGroup: 'test apiGroup',
+          kind: 'test kind',
+          name: 'test name',
+          namespace: 'flux',
+        },
+      ],
+    },
+  ],
+  total: 1,
+  errors: [],
+};
+const mappedRoleBinding = (roleBinding: Array<any>) => {
+  return roleBinding.map(e => [
+    e.name,
+    e.namespace || '-',
+    e.subjects.map((item: WorkspaceRoleBindingSubject) => item.name).join(', '),
+    e.role.name,
+    moment(e.timestamp).fromNow(),
+  ]);
+};
+
+const MockPolicies = {
+  name: 'Role Binding one',
+  clusterName: 'Managemnet',
+  objects: [
+    {
+      id: '1',
+      name: 'Role Binding one',
+      category: 'cat One',
+      severity: 'High',
+      timestamp: '2022-07-30T11:23:55Z',
+    },
+  ],
+  total: 1,
+  errors: [],
+};
+const mappedPolicies = (policy: Array<any>) => {
+  return policy.map(e => [
+    e.name,
+    e.category || '-',
+    e.severity,
+    moment(e.timestamp).fromNow(),
+  ]);
+};
+
 describe('WorkspaceDetails', () => {
   let wrap: (el: JSX.Element) => JSX.Element;
   let api: WorkspaceClientMock;
@@ -59,19 +147,28 @@ describe('WorkspaceDetails', () => {
     api.GetWorkspaceReturns = MockWorkspaceResponse.workspace;
     await act(async () => {
       const c = wrap(
-        <WorkspaceDetails clusterName="" workspaceName={workspace.name} />,
+        <WorkspaceDetails
+          clusterName={workspace.clusterName}
+          workspaceName={workspace.name}
+        />,
       );
       render(c);
     });
+
+    //check Tabs
+    const tabs = await screen.getAllByRole('tab');
     expect(await screen.getByTitle(workspace.name)).toBeTruthy();
     expect(workspace.clusterName).toBeDefined();
-    expect(await screen.getAllByRole('tab')).toHaveLength(4);
+    expect(tabs).toHaveLength(4);
+    expect(tabs[0]).toHaveTextContent('Service Accounts');
+    expect(tabs[1]).toHaveTextContent('Roles');
+    expect(tabs[2]).toHaveTextContent('Role Bindings');
+    expect(tabs[3]).toHaveTextContent('Policies');
 
-    // Details
     expect(screen.getByTestId('Workspace Name')).toHaveTextContent(
       workspace.name,
     );
-    // Namespaces
+    
     const namespaces = document.querySelectorAll(
       '#workspace-details-header-namespaces span',
     );
@@ -85,18 +182,16 @@ describe('WorkspaceDetails', () => {
     );
     const workspace = MockWorkspaceResponse.workspace;
     const ListServiceAccounts = MockServiceAccounts.objects;
-    api.ListWSServiceAccountsReturns = MockServiceAccounts;
+    api.GetWorkspaceServiceAccountsReturns = MockServiceAccounts;
     await act(async () => {
       const c = wrap(
-        <WorkspaceDetails clusterName="" workspaceName={workspace.name} />,
+        <ServiceAccountsTab
+          clusterName={workspace.clusterName}
+          workspaceName={workspace.name}
+        />,
       );
       render(c);
     });
-
-    const serviceAccountTab = screen
-      .getAllByRole('tab')
-      .filter(tabEle => tabEle.textContent === 'Service Accounts')[0];
-    serviceAccountTab.click();
 
     filterTable.testRenderTable(
       ['Name', 'Namespace', 'Age'],
@@ -105,6 +200,105 @@ describe('WorkspaceDetails', () => {
 
     const sortRowsByAge = mappedServiceAccounts(
       ListServiceAccounts.sort((a, b) => {
+        const t1 = new Date(a.timestamp).getTime();
+        const t2 = new Date(b.timestamp).getTime();
+        return t2 - t1;
+      }),
+    );
+
+    filterTable.testSorthTableByColumn('Age', sortRowsByAge);
+  });
+
+  it('renders Roles tab', async () => {
+    const filterTable = new TestFilterableTable('roles-list', fireEvent);
+    const workspace = MockWorkspaceResponse.workspace;
+    const ListRoles = MockRoles.objects;
+    api.GetWorkspaceRolesReturns = MockRoles;
+    await act(async () => {
+      const c = wrap(
+        <RolesTab
+          clusterName={workspace.clusterName}
+          workspaceName={workspace.name}
+        />,
+      );
+      render(c);
+    });
+
+    filterTable.testRenderTable(
+      ['Name', 'Namespace', 'Rules', 'Age'],
+      ListRoles.length,
+    );
+
+    const sortRowsByAge = mappedRoles(
+      ListRoles.sort((a, b) => {
+        const t1 = new Date(a.timestamp).getTime();
+        const t2 = new Date(b.timestamp).getTime();
+        return t2 - t1;
+      }),
+    );
+
+    filterTable.testSorthTableByColumn('Age', sortRowsByAge);
+  });
+
+  it('renders Role Binding tab', async () => {
+    const filterTable = new TestFilterableTable(
+      'role-bindings-list',
+      fireEvent,
+    );
+    const workspace = MockWorkspaceResponse.workspace;
+    const ListRoleBinding = MockRoleBinding.objects;
+    api.GetWorkspaceRoleBindingsReturns = MockRoleBinding;
+    await act(async () => {
+      const c = wrap(
+        <RoleBindingsTab
+          clusterName={workspace.clusterName}
+          workspaceName={workspace.name}
+        />,
+      );
+      render(c);
+    });
+
+    filterTable.testRenderTable(
+      ['Name', 'Namespace', 'Bindings', 'Role', 'Age'],
+      ListRoleBinding.length,
+    );
+
+    const sortRowsByAge = mappedRoleBinding(
+      ListRoleBinding.sort((a, b) => {
+        const t1 = new Date(a.timestamp).getTime();
+        const t2 = new Date(b.timestamp).getTime();
+        return t2 - t1;
+      }),
+    );
+
+    filterTable.testSorthTableByColumn('Age', sortRowsByAge);
+  });
+
+  it('renders Policies tab', async () => {
+    const filterTable = new TestFilterableTable(
+      'workspace-policy-list',
+      fireEvent,
+    );
+    const workspace = MockWorkspaceResponse.workspace;
+    const ListPolicies = MockPolicies.objects;
+    api.GetWorkspacePoliciesReturn = MockPolicies;
+    await act(async () => {
+      const c = wrap(
+        <PoliciesTab
+          clusterName={workspace.clusterName}
+          workspaceName={workspace.name}
+        />,
+      );
+      render(c);
+    });
+
+    filterTable.testRenderTable(
+      ['Name', 'Category', 'Severity', 'Age'],
+      ListPolicies.length,
+    );
+
+    const sortRowsByAge = mappedPolicies(
+      ListPolicies.sort((a, b) => {
         const t1 = new Date(a.timestamp).getTime();
         const t2 = new Date(b.timestamp).getTime();
         return t2 - t1;
--- ui-cra/src/utils/test-utils.tsx
+++ ui-cra/src/utils/test-utils.tsx
@@ -30,8 +30,8 @@ import {
   Pipelines,
 } from '../api/pipelines/pipelines.pb';
 import {
-  GetTerraformObjectResponse,
   GetTerraformObjectPlanResponse,
+  GetTerraformObjectResponse,
   ListTerraformObjectsResponse,
   ReplanTerraformObjectResponse,
   SyncTerraformObjectResponse,
@@ -43,7 +43,10 @@ import {
   GetExternalSecretResponse,
   GetPolicyResponse,
   GetPolicyValidationResponse,
+  GetWorkspacePoliciesResponse,
   GetWorkspaceResponse,
+  GetWorkspaceRoleBindingsResponse,
+  GetWorkspaceRolesResponse,
   GetWorkspaceServiceAccountsResponse,
   ListExternalSecretsResponse,
   ListGitopsClustersResponse,
@@ -355,23 +358,23 @@ export class TerraformClientMock implements Terraform {
 }
 
 export class WorkspaceClientMock {
-  constructor() {
-    this.ListWorkspaces = this.ListWorkspaces.bind(this);
-    this.GetWorkspace = this.GetWorkspace.bind(this);
-    this.ListWSServiceAccounts = this.ListWSServiceAccounts.bind(this);
-  }
   ListWorkspacesReturns: ListWorkspacesResponse = {};
   GetWorkspaceReturns: GetWorkspaceResponse = {};
-  ListWSServiceAccountsReturns: GetWorkspaceServiceAccountsResponse = {};
-  ListWorkspaces() {
-    return promisify(this.ListWorkspacesReturns);
-  }
-  GetWorkspace() {
-    return promisify(this.GetWorkspaceReturns);
-  }
-  ListWSServiceAccounts() {
-    return promisify(this.GetWorkspaceReturns);
-  }
+  GetWorkspaceServiceAccountsReturns: GetWorkspaceServiceAccountsResponse = {};
+  GetWorkspaceRolesReturns: GetWorkspaceRolesResponse = {};
+  GetWorkspaceRoleBindingsReturns: GetWorkspaceRoleBindingsResponse = {};
+  GetWorkspacePoliciesReturn: GetWorkspacePoliciesResponse = {};
+  ListWorkspaces = () => promisify(this.ListWorkspacesReturns);
+
+  GetWorkspace = () => promisify(this.GetWorkspaceReturns);
+
+  GetWorkspaceServiceAccounts = () =>
+    promisify(this.GetWorkspaceServiceAccountsReturns);
+
+  GetWorkspaceRoles = () => promisify(this.GetWorkspaceRolesReturns);
+  GetWorkspaceRoleBindings = () =>
+    promisify(this.GetWorkspaceRoleBindingsReturns);
+  GetWorkspacePolicies = () => promisify(this.GetWorkspacePoliciesReturn);
 }
 
 export class SecretsClientMock {
