2bb7da94d1dd6e3ba2e6abcebc3aa2b8f8e8dc09
Jordan Pellizzari <jordan@weave.works>
2023-02-15 11:49:58 -0800

    Add tab on PipelineDetail for current PRs

--- ui-cra/src/contexts/Pipelines/index.tsx
+++ ui-cra/src/contexts/Pipelines/index.tsx
@@ -4,10 +4,14 @@ import {
   GetPipelineRequest,
   GetPipelineResponse,
   ListPipelinesResponse,
+  ListPullRequestsResponse,
   Pipelines,
 } from '../../api/pipelines/pipelines.pb';
+import { Pipeline } from '../../api/pipelines/types.pb';
 import { formatError } from '../../utils/formatters';
-import useNotifications from './../../contexts/Notifications';
+import useNotifications, {
+  NotificationData,
+} from './../../contexts/Notifications';
 
 interface Props {
   api: typeof Pipelines;
@@ -24,16 +28,23 @@ export const PipelinesProvider = ({ api, children }: Props) => (
 
 export const usePipelines = () => React.useContext(PipelinesContext);
 
+type SetNotificationsType = React.Dispatch<
+  React.SetStateAction<NotificationData[] | []>
+>;
+
+const errorHandler =
+  (setNotifications: SetNotificationsType) => (error: Error) =>
+    setNotifications(formatError(error));
+
 const PIPELINES_KEY = 'pipelines';
 export const useListPipelines = () => {
   const pipelinsService = usePipelines();
   const { setNotifications } = useNotifications();
-  const onError = (error: Error) => setNotifications(formatError(error));
 
   return useQuery<ListPipelinesResponse, Error>(
     [PIPELINES_KEY],
     () => pipelinsService.ListPipelines({}),
-    { retry: false, onError },
+    { retry: false, onError: errorHandler(setNotifications) },
   );
 };
 
@@ -50,6 +61,33 @@ export const useGetPipeline = (req: GetPipelineRequest, enabled?: boolean) => {
       retry: false,
       onError,
       enabled,
+    },
+  );
+};
+
+export const PIPELINE_PULL_REQUESTS_KEY = 'pipeline_prs';
+
+export const useGetPullRequestsForPipeline = (pipeline?: Pipeline) => {
+  const svc = usePipelines();
+  const { setNotifications } = useNotifications();
+
+  return useQuery<ListPullRequestsResponse, Error>(
+    [PIPELINE_PULL_REQUESTS_KEY, pipeline?.namespace, pipeline?.name],
+    () => {
+      if (!pipeline) {
+        return Promise.resolve(null as any);
+      }
+
+      return svc.ListPullRequests({
+        pipelineName: pipeline?.name,
+        pipelineNamespace: pipeline?.namespace,
+      });
+    },
+
+    {
+      refetchInterval: 30000,
+      retry: false,
+      onError: errorHandler(setNotifications),
     },
   );
 };
