562bd358932b0a45118f48aba19eb469d299a298
Simon Howe <simon@weave.works>
2023-02-20 12:06:16 +0100

    Use tilt by default when running `yarn start`

--- ui-cra/src/setupProxy.js
+++ ui-cra/src/setupProxy.js
@@ -1,44 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const DEFAULT_PROXY_HOST = 'http://34.67.250.163:30080/';
-const proxyHost = process.env.PROXY_HOST || DEFAULT_PROXY_HOST;
-const capiServerHost = process.env.CAPI_SERVER_HOST || proxyHost;
-const wegoServerHost = process.env.WEGO_SERVER_HOST || capiServerHost;
-
-// Localhost is running tls by default now
+const proxyHost = process.env.PROXY_HOST ||  'http://localhost:8000/';
+// Accept self-signed certificates etc (for local development).
+// If you are using demo-01 etc you can set PROXY_SECURE=true and it should work.
 const secure = process.env.PROXY_SECURE === 'true';
 
 module.exports = function (app) {
-  app.use(
-    '/gitops',
-    createProxyMiddleware({
-      target: capiServerHost,
-      changeOrigin: true,
-      secure,
-    }),
-  );
-  app.use(
-    '/v1/applications',
-    createProxyMiddleware({
-      target: wegoServerHost,
-      changeOrigin: true,
-      secure,
-    }),
-  );
-  app.use(
-    '/v1',
-    createProxyMiddleware({
-      target: capiServerHost,
-      changeOrigin: true,
-      secure,
-    }),
-  );
-  app.use(
-    '/oauth2',
-    createProxyMiddleware({
-      target: capiServerHost,
-      changeOrigin: true,
-      secure,
-    }),
-  );
+  const proxyMiddleWare = createProxyMiddleware({
+    target: proxyHost,
+    changeOrigin: true,
+    secure,
+  });
+  app.use('/v1', proxyMiddleWare);
+  app.use('/oauth2', proxyMiddleWare);
 };
