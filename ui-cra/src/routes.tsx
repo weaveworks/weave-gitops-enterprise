8e647c01bb52c26c455c03e48e5fe0b2e89c5594
AlinaGoaga <goaga.alina@gmail.com>
2023-02-22 17:30:04 +0100

    Remove dupe from routes

--- ui-cra/src/routes.tsx
+++ ui-cra/src/routes.tsx
@@ -50,7 +50,6 @@ import WorkspaceDetails from './components/Workspaces/WorkspaceDetails';
 import { Routes } from './utils/nav';
 import GitopsSets from './components/GitopsSets';
 import GitOpsSetDetail from './components/GitopsSets/GitOpsSetDetail';
-import CreateSecret from './components/Secrets/Create';
 import WGUserInfo from './components/UserInfo';
 
 function withSearchParams(Cmp: any) {
@@ -259,17 +258,17 @@ const AppRoutes = () => {
       />
       <Route path={Routes.ImageAutomation} component={ImageAutomationPage} />
       <Route
-          path={V2Routes.ImageAutomationUpdatesDetails}
-          component={withSearchParams(ImageAutomationUpdatesDetails)}
-        />
-        <Route
-          path={V2Routes.ImageAutomationRepositoryDetails}
-          component={withSearchParams(ImageAutomationRepoDetails)}
-        />
-        <Route
-          path={V2Routes.ImagePolicyDetails}
-          component={withSearchParams(ImagePolicyDetails)}
-        />
+        path={V2Routes.ImageAutomationUpdatesDetails}
+        component={withSearchParams(ImageAutomationUpdatesDetails)}
+      />
+      <Route
+        path={V2Routes.ImageAutomationRepositoryDetails}
+        component={withSearchParams(ImageAutomationRepoDetails)}
+      />
+      <Route
+        path={V2Routes.ImagePolicyDetails}
+        component={withSearchParams(ImagePolicyDetails)}
+      />
       <Route exact path={Routes.Policies} component={Policies} />
       <Route
         exact
