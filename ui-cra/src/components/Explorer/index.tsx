c71ae6341539ef92bc3229a71be7a52933cbe5f6
Jordan Pellizzari <jordan@weave.works>
2023-03-02 13:42:22 -0800

    Add query service naive frontend

---
+++ ui-cra/src/components/Explorer/index.tsx
@@ -0,0 +1,32 @@
+import { DataTable } from '@weaveworks/weave-gitops';
+import styled from 'styled-components';
+import { useQueryService } from '../../hooks/query';
+import { ContentWrapper } from '../Layout/ContentWrapper';
+import { PageTemplate } from '../Layout/PageTemplate';
+
+type Props = {
+  className?: string;
+};
+
+function Explorer({ className }: Props) {
+  const { data, error, isLoading } = useQueryService();
+
+  return (
+    <PageTemplate documentTitle="Explorer" path={[{ label: 'Explorer' }]}>
+      <ContentWrapper>
+        <div className={className}>
+          <DataTable
+            fields={[
+              { label: 'Name', value: 'name' },
+              { label: 'Kind', value: 'kind' },
+              { label: 'Cluster', value: 'cluster' },
+            ]}
+            rows={data?.objects}
+          />
+        </div>
+      </ContentWrapper>
+    </PageTemplate>
+  );
+}
+
+export default styled(Explorer).attrs({ className: Explorer.name })``;
