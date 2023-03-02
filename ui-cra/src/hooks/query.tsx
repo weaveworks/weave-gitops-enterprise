325373eb961ff40628b3561d077b2b8fa28d7402
Jordan Pellizzari <jordan@weave.works>
2023-03-02 13:42:22 -0800

    Add query service naive frontend

---
+++ ui-cra/src/hooks/query.tsx
@@ -0,0 +1,8 @@
+import { useQuery } from 'react-query';
+import { Query } from '../api/query/query.pb';
+
+export function useQueryService() {
+  const api = Query;
+
+  return useQuery(['query'], () => api.Run({}));
+}
