cf15de38fb249046276aa21aeaa50c9808fbbc17
Ahmad Samir Ali <ahmadsamir1194@gmail.com>
2023-03-08 13:16:21 +0200

    Add eso secrets endpoint

--- cmd/clusters-service/pkg/server/external_secrets.go
+++ cmd/clusters-service/pkg/server/external_secrets.go
@@ -263,3 +263,29 @@ func getSecretStoreType(provider *esv1beta1.SecretStoreProvider) string {
 		return "Unknown"
 	}
 }
+
+// syncExternalSecret syncs externalSecrets from externalSecret operators
+
+func (s *server) SyncExternalSecret(ctx context.Context, req *capiv1_proto.SyncExternalSecretsRequest) (*capiv1_proto.SyncExternalSecretsResponse, error) {
+
+	clustersClient, err := s.clustersManager.GetImpersonatedClientForCluster(ctx, auth.Principal(ctx), req.ClusterName)
+	if err != nil {
+		return nil, fmt.Errorf("error getting impersonating client: %w", err)
+	}
+
+	if clustersClient == nil {
+		return nil, fmt.Errorf("cluster %s not found", req.ClusterName)
+	}
+
+	var externalSecret esv1beta1.ExternalSecret
+	if err := clustersClient.Get(ctx, req.ClusterName, client.ObjectKey{Name: req.ExternalSecretName, Namespace: req.Namespace}, &externalSecret); err != nil {
+		return nil, fmt.Errorf("error getting external secret %s from cluster %s: %w", req.ExternalSecretName, req.ClusterName, err)
+	} else {
+		externalSecret.Annotations["force-sync"] = time.Now().Format(time.RFC3339)
+		if err := clustersClient.Update(ctx, req.ClusterName, &externalSecret); err != nil {
+			return nil, fmt.Errorf("error updating external secret %s from cluster %s: %w", req.ExternalSecretName, req.ClusterName, err)
+		}
+	}
+
+	return nil, nil
+}
