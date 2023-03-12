cbffaeae3b8af39185f4dee96e57e8858dc60064
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2023-03-12 13:58:19 +0200

    remove navigation when namespace is empty

--- ui-cra/src/components/PolicyConfigs/PolicyConfigDetails/PolicyConfigHeaderSection.tsx
+++ ui-cra/src/components/PolicyConfigs/PolicyConfigDetails/PolicyConfigHeaderSection.tsx
@@ -37,17 +37,21 @@ function PolicyConfigHeaderSection({
       case 'apps':
         return (
           <li key={`${item.name}`}>
-            <Link
-              to={formatURL(getKindRoute(item.kind), {
-                clusterName: clusterName,
-                name: item.name,
-                namespace: item.namespace || null,
-              })}
-            >
-              <span data-testid={`matchItem${item.name}`}>
-                {item.namespace === '' ? '*' : item.namespace}/{item.name}
-              </span>
-            </Link>
+            {item.namespace === '' ? (
+              <span data-testid={`matchItem${item.name}`}>*/{item.name}</span>
+            ) : (
+              <Link
+                to={formatURL(getKindRoute(item.kind), {
+                  clusterName: clusterName,
+                  name: item.name,
+                  namespace: item.namespace || null,
+                })}
+              >
+                <span data-testid={`matchItem${item.name}`}>
+                  {item.namespace}/{item.name}
+                </span>
+              </Link>
+            )}
             <span
               data-testid={`matchItemKind${item.kind}`}
               className={`${classes.targetItemKind} ${classes.capitlize}`}
