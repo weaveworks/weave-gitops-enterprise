3fe4f030a1ce8139748e70aecae7bda637b31016
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2023-04-03 14:35:20 +0200

    add sync button to secret details page

--- ui-cra/src/components/Secrets/SecretDetails/index.tsx
+++ ui-cra/src/components/Secrets/SecretDetails/index.tsx
@@ -5,6 +5,10 @@ import { ContentWrapper } from '../../Layout/ContentWrapper';
 import { PageTemplate } from '../../Layout/PageTemplate';
 import moment from 'moment';
 import SecretDetailsTabs from './SecretDetailsTabs';
+import { request } from '../../../utils/request';
+import { SyncExternalSecretsResponse } from '../../../cluster-services/cluster_services.pb';
+import { Button, theme } from '@weaveworks/weave-gitops';
+import SyncIcon from '@material-ui/icons/Sync';
 
 const SecretDetails = ({
   externalSecretName,
@@ -35,24 +39,44 @@ const SecretDetails = ({
     },
   ];
 
+  const syncSecret = (payload: any): Promise<SyncExternalSecretsResponse> => {
+    return request('POST', `/v1/external-secrets/sync`, {
+      body: JSON.stringify(payload),
+    });
+  };
+
   return (
-      <PageTemplate
-        documentTitle="Secrets"
-        path={[
-          { label: 'Secrets', url: Routes.Secrets },
-          { label: secretDetails?.externalSecretName || '' },
-        ]}
-      >
-        <ContentWrapper loading={isSecretDetailsLoading}>
-          {generateRowHeaders(defaultHeaders)}
-          <SecretDetailsTabs
-            externalSecretName={externalSecretName}
-            clusterName={clusterName}
-            namespace={namespace}
-            secretDetails={secretDetails || {}}
-          />
-        </ContentWrapper>
-      </PageTemplate>
+    <PageTemplate
+      documentTitle="Secrets"
+      path={[
+        { label: 'Secrets', url: Routes.Secrets },
+        { label: secretDetails?.externalSecretName || '' },
+      ]}
+    >
+      <ContentWrapper loading={isSecretDetailsLoading}>
+        <Button
+          id="create-secrets"
+          startIcon={<SyncIcon />}
+          style={{ marginBottom: theme.spacing.medium }}
+          onClick={() =>
+            syncSecret({
+              clusterName,
+              namespace,
+              externalSecretName,
+            })
+          }
+        >
+          SYNC
+        </Button>
+        {generateRowHeaders(defaultHeaders)}
+        <SecretDetailsTabs
+          externalSecretName={externalSecretName}
+          clusterName={clusterName}
+          namespace={namespace}
+          secretDetails={secretDetails || {}}
+        />
+      </ContentWrapper>
+    </PageTemplate>
   );
 };
 
