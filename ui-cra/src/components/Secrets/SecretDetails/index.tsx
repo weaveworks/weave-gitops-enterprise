1d9bc0d8eaa6ec6f10c148128ef795beddaf1fdd
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2023-04-03 15:47:47 +0200

    fix typo

--- ui-cra/src/components/Secrets/SecretDetails/index.tsx
+++ ui-cra/src/components/Secrets/SecretDetails/index.tsx
@@ -66,7 +66,7 @@ const SecretDetails = ({
             })
           }
         >
-          SYNC
+          Sync
         </Button>
         {generateRowHeaders(defaultHeaders)}
         <SecretDetailsTabs
