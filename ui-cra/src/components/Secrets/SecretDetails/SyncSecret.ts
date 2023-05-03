5fa66567424d04d25ccb72313ff54c8d408821e0
AlinaGoaga <35202557+AlinaGoaga@users.noreply.github.com>
2023-05-03 12:28:59 +0200

    [UI] Secrets sync button looks unresponsive (#2771)
    * Updates following the Core changes on reconciledObjects and flags
    
    * Fix sorting of gitrepos
    
    * Improve feedback when running sync secret
    
    * Improve feedback when running sync secret - updated
    
    * Clean up notif on sync click
    
    * Small cleanup
    
    * Remove success notif for sync

--- ui-cra/src/components/Secrets/SecretDetails/SyncSecret.ts
+++ ui-cra/src/components/Secrets/SecretDetails/SyncSecret.ts
@@ -1,24 +1,32 @@
-import { Dispatch } from 'react';
-import { SyncExternalSecretsResponse } from '../../../cluster-services/cluster_services.pb';
-import { request } from '../../../utils/request';
+import { ClustersService } from '../../../cluster-services/cluster_services.pb';
+import { QueryClient, useQueryClient } from 'react-query';
 
-export const syncSecret = async (
-  payload: any,
-  setNotifications: Dispatch<React.SetStateAction<any>>,
-  setIsLoading: Dispatch<React.SetStateAction<any>>,
-): Promise<SyncExternalSecretsResponse> => {
-  setIsLoading(true);
-  const updateEvent = await request('POST', `/v1/external-secrets/sync`, {
-    body: JSON.stringify(payload),
-  }).catch(err => {
-    setNotifications([
-      {
-        message: { text: err.message },
-        severity: 'error',
-        display: 'top',
-      },
-    ]);
-  });
-  setIsLoading(false);
-  return updateEvent;
-};
+interface DetailParams {
+  clusterName: string;
+  namespace: string;
+  externalSecretName: string;
+}
+
+const SECRETS_KEY = 'external-secrets';
+
+function invalidate(
+  qc: QueryClient,
+  { clusterName, namespace, externalSecretName }: DetailParams,
+) {
+  return qc.invalidateQueries([
+    SECRETS_KEY,
+    clusterName,
+    namespace,
+    externalSecretName,
+  ]);
+}
+
+export function useSyncSecret(params: DetailParams) {
+  const qc = useQueryClient();
+
+  return () =>
+    ClustersService.SyncExternalSecrets(params).then(res => {
+      invalidate(qc, params);
+      return res;
+    });
+}
--- ui-cra/src/components/Secrets/SecretDetails/index.tsx
+++ ui-cra/src/components/Secrets/SecretDetails/index.tsx
@@ -1,5 +1,4 @@
-import SyncIcon from '@material-ui/icons/Sync';
-import { Button, theme } from '@weaveworks/weave-gitops';
+import { Button } from '@weaveworks/weave-gitops';
 import moment from 'moment';
 import { useState } from 'react';
 import useNotifications from '../../../contexts/Notifications';
@@ -9,7 +8,7 @@ import { ContentWrapper } from '../../Layout/ContentWrapper';
 import { PageTemplate } from '../../Layout/PageTemplate';
 import { generateRowHeaders, SectionRowHeader } from '../../RowHeader';
 import SecretDetailsTabs from './SecretDetailsTabs';
-import { syncSecret } from './SyncSecret';
+import { useSyncSecret } from './SyncSecret';
 import { Box } from '@material-ui/core';
 
 const SecretDetails = ({
@@ -40,8 +39,29 @@ const SecretDetails = ({
       value: moment(secretDetails?.timestamp).fromNow(),
     },
   ];
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [syncing, setSyncing] = useState(false);
   const { setNotifications } = useNotifications();
+
+  const sync = useSyncSecret({
+    clusterName,
+    namespace,
+    externalSecretName,
+  });
+
+  const handleSyncClick = () => {
+    setSyncing(true);
+    setNotifications([]);
+    return sync()
+      .catch(err => {
+        setNotifications([
+          {
+            message: { text: err?.message },
+            severity: 'error',
+          },
+        ]);
+      })
+      .finally(() => setSyncing(false));
+  };
 
   return (
     <PageTemplate
@@ -52,26 +72,18 @@ const SecretDetails = ({
       ]}
     >
       <ContentWrapper loading={isSecretDetailsLoading}>
-        <Button
-          id="create-secrets"
-          loading={isLoading}
-          startIcon={<SyncIcon />}
-          style={{ marginBottom: theme.spacing.medium }}
-          onClick={() => {
-            syncSecret(
-              {
-                clusterName,
-                namespace,
-                externalSecretName,
-              },
-              setNotifications,
-              setIsLoading,
-            );
-          }}
-        >
-          Sync
-        </Button>
-        <Box paddingBottom={3}>{generateRowHeaders(defaultHeaders)}</Box>
+        <Box paddingBottom={3}>
+          <Button
+            id="sync-secret"
+            loading={syncing}
+            variant="outlined"
+            onClick={handleSyncClick}
+            style={{ marginRight: 0, textTransform: 'uppercase' }}
+          >
+            Sync
+          </Button>
+        </Box>
+        {generateRowHeaders(defaultHeaders)}
         <SecretDetailsTabs
           externalSecretName={externalSecretName}
           clusterName={clusterName}
