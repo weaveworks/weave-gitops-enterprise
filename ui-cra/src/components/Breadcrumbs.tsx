ee66b4f87cd0650eebb636c8f84a7f77b62b39ca
TheGostKasper <thegostkasper@gmail.com>
2023-06-07 15:13:57 +0300

    fix breadcrumb conflict

--- ui-cra/src/components/Breadcrumbs.tsx
+++ ui-cra/src/components/Breadcrumbs.tsx
@@ -1,58 +1,78 @@
-import { Tooltip } from '@material-ui/core';
-import { Flex, Icon, IconType, Link, Text } from '@weaveworks/weave-gitops';
+import { Flex, Icon, IconType, Link } from '@weaveworks/weave-gitops';
+import { isEmpty } from 'lodash';
+import { FC } from 'react';
 import styled from 'styled-components';
 
-const EllipsesText = styled(Text)<{ maxWidth?: string }>`
-  max-width: ${prop => prop.maxWidth || '400px'};
+interface Size {
+  size?: 'small';
+}
+
+const Container = styled(Flex)`
+  font-size: ${20}px;
+  height: 32px;
+`;
+
+const Span = styled.span`
+  text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
-  text-overflow: ellipsis;
+  max-width: 300px;
+  .labelLink {
+    color: ${props => props.theme.colors.black};
+    fontWeight: 700;
+    marginRight: ${props => props.theme.spacing.xs};
+  },
+`;
+
+const Divider = styled.div`
+  display: flex;
+  alignitems: center;
+  justifycontent: center;
+  padding: 0 ${props => props.theme.spacing.small};
+`;
+
+export const Title = styled.div`
+  margin-right: ${props => props.theme.spacing.xxs};
+  white-space: nowrap;
 `;
 export interface Breadcrumb {
   label: string;
   url?: string;
 }
-interface Props {
+interface Props extends Size {
   path: Breadcrumb[];
 }
-export const Breadcrumbs = ({ path = [] }: Props) => {
+
+export const Breadcrumbs: FC<Props> = ({ path }) => {
   return (
-    <Flex align>
-      {path.map(({ label, url }) => {
-        return (
-          <Flex align key={label}>
-            {url ? (
-              <>
-                <Link
-                  data-testid={`link-${label}`}
-                  to={url}
-                  textProps={{ bold: true, size: 'large', color: 'neutral40' }}
-                >
-                  {label}
-                </Link>
-                <Icon
-                  type={IconType.NavigateNextIcon}
-                  size="large"
-                  color="neutral40"
-                />
-              </>
-            ) : (
-              <Tooltip title={label} placement="bottom">
-                <EllipsesText
-                  size="large"
-                  color="neutral40"
-                  className="ellipsis"
-                  data-testid={`text-${label}`}
-                >
-                  {label}
-                </EllipsesText>
-              </Tooltip>
-            )}
-          </Flex>
-        );
-      })}
-    </Flex>
+    <Container align center className="test-id-breadcrumbs">
+      {path.map(({ label, url }, index) => (
+        <Flex key={index} align>
+          {index > 0 && (
+            <Divider>
+              <Icon
+                type={IconType.NavigateNextIcon}
+                size="large"
+                color="neutral40"
+              />
+            </Divider>
+          )}
+          {isEmpty(url) ? (
+            <Span className="labelLink" title={label}>
+              {label}
+            </Span>
+          ) : (
+            <Title role="heading">
+              <Link
+                to={url || ''}
+                textProps={{ color: 'black', size: 'large' }}
+              >
+                {label}
+              </Link>
+            </Title>
+          )}
+        </Flex>
+      ))}
+    </Container>
   );
 };
-
-export default styled(Breadcrumbs).attrs({ className: Breadcrumbs.name })``;
