20c8920f44554e37f9fc3acb002e7c18d8e863a7
AlinaGoaga <goaga.alina@gmail.com>
2023-06-18 18:14:47 +0200

    Update selector for header in apps and sources - 2

--- test/acceptance/test/pages/sources_page.go
+++ test/acceptance/test/pages/sources_page.go
@@ -68,7 +68,7 @@ func (a SourcesPage) CountSources() int {
 
 func GetSourceDetailPage(webDriver *agouti.Page) *SourceDetailPage {
 	detailPage := SourceDetailPage{
-		Header: webDriver.Find(`div[class*=Page__TopToolBar] span`),
+		Header: webDriver.Find(`div[class*=Page__TopToolBar] span[class*=Breadcrumbs]`),
 		Title:  webDriver.Find(`div[class*="SourceDetail"]`),
 	}
 	return &detailPage
