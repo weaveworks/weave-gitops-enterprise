8866b6aa379b9af818b7000a1ed54a1db78da7e5
AlinaGoaga <35202557+AlinaGoaga@users.noreply.github.com>
2023-06-21 19:16:56 +0200

    Use custom ee nav (#2987)
    * Updates following the Core changes on reconciledObjects and flags
    
    * Fix sorting of gitrepos
    
    * Bring back custom nav
    
    * Update with core main
    
    * Update tests
    
    * Update with latest core release

--- ui-cra/src/components/Layout/App.tsx
+++ ui-cra/src/components/Layout/App.tsx
@@ -1,12 +1,4 @@
-import {
-  Layout,
-  Page as WGPage,
-  Flex,
-  useFeatureFlags,
-  NavItem,
-  IconType,
-  V2Routes,
-} from '@weaveworks/weave-gitops';
+import { Layout, Page as WGPage, Flex, Logo } from '@weaveworks/weave-gitops';
 import { ListConfigProvider, VersionProvider } from '../../contexts/ListConfig';
 import AppRoutes from '../../routes';
 import ErrorBoundary from '../ErrorBoundary';
@@ -14,7 +6,8 @@ import { ToastContainer } from 'react-toastify';
 import { Routes } from '../../utils/nav';
 import styled from 'styled-components';
 import { Breadcrumb } from '@weaveworks/weave-gitops/ui/components/Breadcrumbs';
-import { useMemo } from 'react';
+import { useState } from 'react';
+import Navigation from './Navigation';
 
 export type PageProps = {
   className?: string;
@@ -24,12 +17,8 @@ export type PageProps = {
 };
 
 const WGLayout = styled(Layout)`
-  height: 100vh;
-  div[class*='Logo'] {
-    height: 60px;
-  }
   div[class*='Nav__NavContainer'] {
-    height: calc(100% - 60px);
+    height: calc(100vh - 60px);
   }
 
   //TO DO: Remove once dark mode is implemented
@@ -40,112 +29,6 @@ const WGLayout = styled(Layout)`
   }
 `;
 
-function getNavItems(isFlagEnabled: (flag: string) => boolean): NavItem[] {
-  return [
-    {
-      label: 'Platform',
-    },
-    {
-      label: 'Clusters',
-      link: { value: Routes.Clusters },
-      icon: IconType.ClustersIcon,
-    },
-    {
-      label: 'Templates',
-      link: { value: Routes.Templates },
-      icon: IconType.TemplatesIcon,
-    },
-    {
-      label: 'GitOpsSets',
-      link: { value: Routes.GitOpsSets },
-      icon: IconType.GitOpsSetsIcon,
-    },
-    {
-      label: 'Terraform',
-      link: { value: Routes.TerraformObjects },
-      icon: IconType.TerraformIcon,
-      disabled: !isFlagEnabled('WEAVE_GITOPS_FEATURE_TERRAFORM_UI'),
-    },
-    {
-      label: 'Secrets',
-      link: { value: Routes.Secrets },
-      icon: IconType.SecretsIcon,
-    },
-    {
-      label: 'Delivery',
-    },
-    {
-      label: 'Applications',
-      link: { value: V2Routes.Automations },
-      icon: IconType.ApplicationsIcon,
-    },
-    {
-      label: 'Sources',
-      link: { value: V2Routes.Sources },
-      icon: IconType.SourcesIcon,
-    },
-    {
-      label: 'Image Automation',
-      link: { value: Routes.ImageAutomation },
-      icon: IconType.ImageAutomationIcon,
-    },
-    {
-      label: 'Pipelines',
-      link: { value: Routes.Pipelines },
-      icon: IconType.PipelinesIcon,
-      disabled: !isFlagEnabled('WEAVE_GITOPS_FEATURE_PIPELINES'),
-    },
-    {
-      label: 'Delivery ',
-      link: { value: Routes.Canaries },
-      icon: IconType.DeliveryIcon,
-      disabled: process.env.REACT_APP_DISABLE_PROGRESSIVE_DELIVERY === 'true',
-    },
-    {
-      label: 'Flux Runtime',
-      link: { value: V2Routes.FluxRuntime },
-      icon: IconType.FluxIcon,
-    },
-    {
-      label: 'Explorer',
-      link: { value: Routes.Explorer },
-      icon: IconType.ExploreIcon,
-      disabled: !isFlagEnabled('WEAVE_GITOPS_FEATURE_EXPLORER'),
-    },
-    {
-      label: 'Guardrails',
-    },
-    {
-      label: 'Workspaces',
-      link: { value: Routes.Workspaces },
-      icon: IconType.WorkspacesIcon,
-    },
-    {
-      label: 'Policies',
-      link: { value: Routes.Policies },
-      icon: IconType.PoliciesIcon,
-    },
-    {
-      label: 'Policy Configs',
-      link: { value: Routes.PolicyConfigs },
-      icon: IconType.PolicyConfigsIcon,
-    },
-    {
-      label: 'Developer Ex.',
-    },
-    {
-      label: 'GitOps Run',
-      link: { value: Routes.GitOpsRun },
-      icon: IconType.GitOpsRunIcon,
-    },
-    {
-      label: 'Notifications',
-      link: { value: Routes.Notifications },
-      icon: IconType.NotificationsIcon,
-    },
-  ];
-}
-
 export const Page = ({ children, loading, className, path }: PageProps) => (
   <Flex column wide>
     <WGPage loading={loading} className={className} path={path}>
@@ -155,13 +38,14 @@ export const Page = ({ children, loading, className, path }: PageProps) => (
 );
 
 const App = () => {
-  const { isFlagEnabled } = useFeatureFlags();
-  const navItems = useMemo(() => getNavItems(isFlagEnabled), [isFlagEnabled]);
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+  const logo = <Logo collapsed={collapsed} link={Routes.Clusters} />;
+  const nav = <Navigation collapsed={collapsed} setCollapsed={setCollapsed} />;
 
   return (
     <ListConfigProvider>
       <VersionProvider>
-        <WGLayout logoLink={Routes.Clusters} navItems={navItems}>
+        <WGLayout logo={logo} nav={nav}>
           <ErrorBoundary>
             <AppRoutes />
           </ErrorBoundary>
