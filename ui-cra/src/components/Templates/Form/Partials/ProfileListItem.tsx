e7a922b4e132b7e9c7d3e27fc888bbad206c7b39
AlinaGoaga <goaga.alina@gmail.com>
2023-06-22 14:39:54 +0200

    Put back profile namespace

--- ui-cra/src/components/Templates/Form/Partials/ProfileListItem.tsx
+++ ui-cra/src/components/Templates/Form/Partials/ProfileListItem.tsx
@@ -151,7 +151,7 @@ const ProfilesListItem: FC<{
             </SelectSetWidth>
           </FormControl>
         </SpacedDiv>
-        <SpacedDiv>
+        <SpacedDiv className="profile-namespace">
           <FormControl>
             <Input
               id="profile-namespace"
