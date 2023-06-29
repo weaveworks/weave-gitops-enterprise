f8288837c28b527cc32bb201d91d4fea36c21f44
Eneko Fernandez <eneko@weave.works>
2023-06-29 12:03:59 +0200

    added collector type so we could monitor them independently
    removed recorder test as the logic is being tested within the collector lifecycle

--- pkg/query/collector/metrics/recorder.go
+++ pkg/query/collector/metrics/recorder.go
@@ -4,24 +4,26 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-const collectorSubsystem = "collector"
+const (
+	collectorSubsystem = "collector"
+)
 
 var clusterWatcher = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 	Subsystem: collectorSubsystem,
 	Name:      "cluster_watcher",
 	Help:      "number of active cluster watchers by watcher status",
-}, []string{"status"})
+}, []string{"collector", "status"})
 
 func init() {
 	prometheus.MustRegister(clusterWatcher)
 }
 
 // ClusterWatcherDecrease decreases collector_cluster_watcher for status
-func ClusterWatcherDecrease(status string) {
-	clusterWatcher.WithLabelValues(status).Dec()
+func ClusterWatcherDecrease(collector string, status string) {
+	clusterWatcher.WithLabelValues(collector, status).Dec()
 }
 
 // ClusterWatcherIncrease increases collector_cluster_watcher metric for status
-func ClusterWatcherIncrease(status string) {
-	clusterWatcher.WithLabelValues(status).Inc()
+func ClusterWatcherIncrease(collector string, status string) {
+	clusterWatcher.WithLabelValues(collector, status).Inc()
 }
