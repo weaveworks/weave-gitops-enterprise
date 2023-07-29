981797fad4f370324b65038452e14b8f1b31d919
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2023-07-29 23:49:01 +0300

    add tabs to policies list page

--- ui-cra/src/components/Policies/PoliciesListPage.tsx
+++ ui-cra/src/components/Policies/PoliciesListPage.tsx
@@ -1,19 +1,42 @@
 import { NotificationsWrapper } from '../Layout/NotificationsWrapper';
 import { Page } from '../Layout/App';
 import { useListPolicies } from '../../contexts/PolicyViolations';
-import { PolicyTable } from '@weaveworks/weave-gitops';
+import {
+  Flex,
+  PolicyTable,
+  PolicyViolationsList,
+  RouterTab,
+  SubRouterTabs,
+} from '@weaveworks/weave-gitops';
+import { useRouteMatch } from 'react-router-dom';
 
 const Policies = () => {
   const { data, isLoading } = useListPolicies({});
+  const { path } = useRouteMatch();
 
   return (
     <Page loading={isLoading} path={[{ label: 'Policies' }]}>
       <NotificationsWrapper errors={data?.errors}>
-        {data?.policies && (
-          <div id="policy-list">
-            <PolicyTable policies={data.policies} />
-          </div>
-        )}
+        <SubRouterTabs rootPath={`${path}/`}>
+          <RouterTab name="Policies" path={`${path}/list/`}>
+            <>
+              {data?.policies && (
+                <Flex wide id="policy-list">
+                  <PolicyTable policies={data.policies} />
+                </Flex>
+              )}
+            </>
+          </RouterTab>
+          <RouterTab name="Audit Violations" path={`${path}/auditViolations/`}>
+            <div>violations</div>
+          </RouterTab>
+          <RouterTab
+            name="Enforcement Events"
+            path={`${path}/enforcementEvent/`}
+          >
+            <PolicyViolationsList req={{}} />
+          </RouterTab>
+        </SubRouterTabs>
       </NotificationsWrapper>
     </Page>
   );
