624272d5721e4baebf1cb4c9c21a5cdda28e771a
Eneko Fernandez <eneko@weave.works>
2023-07-31 11:01:16 +0100

    generate v1 kustomizations when adding apps

--- cmd/clusters-service/pkg/server/clusters.go
+++ cmd/clusters-service/pkg/server/clusters.go
@@ -15,7 +15,7 @@ import (
 	"time"
 
 	helmv2 "github.com/fluxcd/helm-controller/api/v2beta1"
-	kustomizev1 "github.com/fluxcd/kustomize-controller/api/v1beta2"
+	kustomizev1 "github.com/fluxcd/kustomize-controller/api/v1"
 	"github.com/fluxcd/pkg/apis/meta"
 	sourcev1 "github.com/fluxcd/source-controller/api/v1beta2"
 	"github.com/imdario/mergo"
