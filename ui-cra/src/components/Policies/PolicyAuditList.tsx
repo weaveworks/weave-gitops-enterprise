dc9f3632d016d64b522096c1bb15894f0537c84b
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2023-08-03 09:05:20 +0300

    push for test

--- ui-cra/src/components/Policies/PolicyAuditList.tsx
+++ ui-cra/src/components/Policies/PolicyAuditList.tsx
@@ -7,8 +7,10 @@ import { QueryStateProvider } from '../Explorer/hooks';
 
 // @ts-ignore
 import PaginationControls from '../Explorer/PaginationControls';
+import { useEffect, useState } from 'react';
 
 const PolicyAuditList = () => {
+  const [violations, setViolations] = useState<any[]>([]);
   const history = useHistory();
   const manager = new URLQueryStateManager(history);
 
@@ -23,22 +25,77 @@ const PolicyAuditList = () => {
     orderBy: queryState.orderBy,
     ascending: queryState.orderAscending,
   });
+useEffect(()=>{
 
- const rows= data?.objects?.map(obj => {
-    const fObj = new FluxObject({ payload: obj.unstructured || '' });
-    return {
-      ...fObj.obj,
-      ...obj,
-    };
- });
-  console.log(rows)
+})
+  const rows = data?.objects?.map(obj => {
+    const details = JSON.parse(obj.unstructured || '');
+
+    setViolations({ ...obj, ...details });
+    // new FluxObject({ payload: obj.unstructured || '' });
+    console.log(violations);
+  });
+
+  // const fields: Field[] = [
+  //   {
+  //     label: "Message",
+  //     value:"message",
+  //     textSearchable: true,
+  //     sortValue: ({ message }) => message,
+  //     maxWidth: 300,
+  //   },
+  //   ...(isFlagEnabled("WEAVE_GITOPS_FEATURE_CLUSTER")
+  //     ? [
+  //         {
+  //           label: "Cluster",
+  //           value: "clusterName",
+  //           sortValue: ({ clusterName }) => clusterName,
+  //         },
+  //       ]
+  //     : []),
+  //   ...(!req.kind || req.kind === Kind.Policy
+  //     ? [
+  //         {
+  //           label: "Application",
+  //           value: ({ namespace, entity }) => `${namespace}/${entity}`,
+  //           sortValue: ({ namespace, entity }) => `${namespace}/${entity}`,
+  //         },
+  //       ]
+  //     : []),
+  //   {
+  //     label: "Severity",
+  //     value: ({ severity }) => <Severity severity={severity || ""} />,
+  //     sortValue: ({ severity }) => severity,
+  //   },
+  //   {
+  //     label: "Category",
+  //     value: "category",
+  //     sortValue: ({ category }) => category,
+  //   },
+  //   ...(!req.kind || req.kind !== Kind.Policy
+  //     ? [
+  //         {
+  //           label: "Violated Policy",
+  //           value: "name",
+  //           sortValue: ({ name }) => name,
+  //         },
+  //       ]
+  //     : []),
+  //   {
+  //     label: "Violation Time",
+  //     value: ({ createdAt }) => <Timestamp time={createdAt} />,
+  //     defaultSort: true,
+  //     sortValue: ({ createdAt }) => {
+  //       const t = createdAt && new Date(createdAt).getTime();
+  //       return t * -1;
+  //     },
+  //   },
+  // ];
   return (
     <QueryStateProvider manager={manager}>
       <div>
         {error && <Alert severity="error">{error.message}</Alert>}
-        <Flex wide>
-
-        </Flex>
+        <Flex wide></Flex>
 
         <PaginationControls
           queryState={queryState}
