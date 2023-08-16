1a64badef0903a292f0741ccd8b114373693af19
Rana Tarek Hassan <rana.hassan@weave.works>
2023-08-16 13:01:23 +0300

    WIP Creating config with token

--- pkg/cluster/connector/config_test.go
+++ pkg/cluster/connector/config_test.go
@@ -1,9 +1,14 @@
 package connector
 
 import (
+	"context"
 	"testing"
 
+	"github.com/go-logr/logr"
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -34,5 +39,53 @@ func TestConfigForContext(t *testing.T) {
 	if string(restCfg.KeyData) != "USER1_CKDATA" {
 		t.Fatalf("expected KeyData = %s, got %s", "USER1_CKDATA", restCfg.KeyData)
 	}
+
+}
+
+func AddClusterToConfig(config *rest.Config, config2 *rest.Config, token []byte) (*rest.Config, error) {
+	return nil, nil
+
+}
+
+func TestAddClusterToConfig(t *testing.T) {
+
+	// opts := clientcmd.NewDefaultPathOptions()
+	// opts.LoadingRules.ExplicitPath = "testdata/kube-config.yaml"
+
+	// restCfg, err := ConfigForContext(opts, "context1")
+	// assert.NoError(t, err)
+	// t.Logf("cfg : %s", restCfg.String())
+
+	remoteClientSet := fake.NewSimpleClientset()
+	serviceAccountName := "test-service-account"
+	clusterConnectionOpts := ClusterConnectionOptions{
+		ServiceAccountName:     serviceAccountName,
+		ClusterRoleName:        serviceAccountName + "-cluster-role",
+		ClusterRoleBindingName: serviceAccountName + "-cluster-role-binding",
+		Namespace:              corev1.NamespaceDefault,
+	}
+	token, err := ReconcileServiceAccount(context.Background(), remoteClientSet, clusterConnectionOpts, logr.Logger{})
+
+	opts2 := clientcmd.NewDefaultPathOptions()
+	opts2.LoadingRules.ExplicitPath = "new-config.yaml"
+
+	restCfg2, err := ConfigForContext(opts2, "context2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// restCfg2.CertData
+	// restCfg2.
+	// host := restCfg2.Host
+
+	t.Logf("cfg2 : %s", restCfg2.String())
+	t.Logf("cfg2 token : %s", restCfg2.BearerToken)
+
+	newConfig := rest.Config{
+		Host:        restCfg2.Host,
+		BearerToken: string(token),
+	}
+	t.Logf("newConfig : %s", newConfig.String())
+
+	// config, err := AddClusterToConfig(restCfg, restCfg2, token)
 
 }
