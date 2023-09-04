dfa18fb1648c8b4f5c755d6807c71a94dc1058dd
TheGostKasper <thegostkasper@gmail.com>
2023-09-04 04:03:06 +0300

    Metric template details renders all the metrics templates in the canary instead of the one clicked

--- ui-cra/src/components/ProgressiveDelivery/CanaryDetails/Analysis/CanaryMetricsTable.tsx
+++ ui-cra/src/components/ProgressiveDelivery/CanaryDetails/Analysis/CanaryMetricsTable.tsx
@@ -1,86 +1,64 @@
+import { CanaryMetric } from '@weaveworks/progressive-delivery/api/prog/types.pb';
 import {
-  CanaryMetric,
-  CanaryMetricTemplate,
-} from '@weaveworks/progressive-delivery/api/prog/types.pb';
-import { Dispatch, FC, SetStateAction, useState } from 'react';
-import { MetricTemplateModal } from './MetricTemplateModal';
-import { DataTable } from '@weaveworks/weave-gitops';
+  AppContext,
+  DataTable,
+  FluxObject,
+  Text,
+} from '@weaveworks/weave-gitops';
+import { useContext } from 'react';
 import { TableWrapper } from '../../../Shared';
-
-type Props = {
-  metricTemplate: CanaryMetricTemplate;
-  openMetricTemplate: boolean;
-  setOpenMetricTemplate: Dispatch<SetStateAction<any>>;
-};
-
-const MetricTemplateModalWrapper: FC<Props> = ({
-  metricTemplate,
-  openMetricTemplate,
-  setOpenMetricTemplate,
-}) => {
-  return (
-    <div>
-      <a
-        target="_self"
-        href="#metric-template-dialog"
-        onClick={() => setOpenMetricTemplate(true)}
-      >
-        {metricTemplate.name}
-      </a>
-      <MetricTemplateModal
-        open={openMetricTemplate}
-        metricTemplate={metricTemplate}
-        setOpenMetricTemplate={setOpenMetricTemplate}
-      />
-    </div>
-  );
-};
 
 export const CanaryMetricsTable = ({
   metrics,
 }: {
   metrics: CanaryMetric[];
 }) => {
-  const [openMetricTemplate, setOpenMetricTemplate] = useState(false);
+  const { setDetailModal } = useContext(AppContext);
 
   return (
-      <TableWrapper id="canary-analysis-metrics">
-        <DataTable
-          rows={metrics}
-          fields={[
-            {
-              label: 'Name',
-              value: 'name',
-            },
-            {
-              label: 'Metric Template',
-              value: (c: CanaryMetric) =>
-                c.metricTemplate ? (
-                  <MetricTemplateModalWrapper
-                    metricTemplate={c.metricTemplate}
-                    openMetricTemplate={openMetricTemplate}
-                    setOpenMetricTemplate={setOpenMetricTemplate}
-                  />
-                ) : (
-                  '-'
-                ),
-            },
-            {
-              label: 'Threshold Min',
-              value: (c: CanaryMetric) =>
-                c.thresholdRange?.min ? '' + c.thresholdRange?.min : '-',
-            },
-            {
-              label: 'Threshold Max',
-              value: (c: CanaryMetric) =>
-                c.thresholdRange?.max ? '' + c.thresholdRange?.max : '-',
-            },
-            {
-              label: 'Interval',
-              value: 'interval',
-            },
-          ]}
-        />
-      </TableWrapper>
+    <TableWrapper id="canary-analysis-metrics">
+      <DataTable
+        rows={metrics}
+        fields={[
+          {
+            label: 'Name',
+            value: 'name',
+          },
+          {
+            label: 'Metric Template',
+            value: (c: CanaryMetric) =>
+              c.metricTemplate ? (
+                <Text
+                  onClick={() =>
+                    setDetailModal({
+                      object: c.metricTemplate as FluxObject,
+                    })
+                  }
+                  color="primary10"
+                  pointer
+                >
+                  {c.metricTemplate?.name}
+                </Text>
+              ) : (
+                ''
+              ),
+          },
+          {
+            label: 'Threshold Min',
+            value: (c: CanaryMetric) =>
+              c.thresholdRange?.min ? '' + c.thresholdRange?.min : '-',
+          },
+          {
+            label: 'Threshold Max',
+            value: (c: CanaryMetric) =>
+              c.thresholdRange?.max ? '' + c.thresholdRange?.max : '-',
+          },
+          {
+            label: 'Interval',
+            value: 'interval',
+          },
+        ]}
+      />
+    </TableWrapper>
   );
 };
