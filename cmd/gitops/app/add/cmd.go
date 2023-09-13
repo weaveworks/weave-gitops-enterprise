b381e87d3cf7bb9d87dcd60925c465bb3d109d4e
Samra10 <ahmadsamir1194@gmail.com>
2023-09-13 13:04:18 +0300

    Move controllers under gitops add

--- cmd/gitops/app/add/cmd.go
+++ cmd/gitops/app/add/cmd.go
@@ -3,6 +3,7 @@ package add
 import (
 	"github.com/spf13/cobra"
 	"github.com/weaveworks/weave-gitops-enterprise/cmd/gitops/app/add/clusters"
+	"github.com/weaveworks/weave-gitops-enterprise/cmd/gitops/app/add/controllers"
 	"github.com/weaveworks/weave-gitops-enterprise/cmd/gitops/app/add/profiles"
 	"github.com/weaveworks/weave-gitops-enterprise/cmd/gitops/app/add/terraform"
 	"github.com/weaveworks/weave-gitops-enterprise/cmd/gitops/pkg/adapters"
@@ -21,6 +22,6 @@ gitops add cluster`,
 	cmd.AddCommand(clusters.AddCommand(opts, client))
 	cmd.AddCommand(profiles.AddCommand(opts, client))
 	cmd.AddCommand(terraform.AddCommand(opts, client))
-
+	cmd.AddCommand(controllers.Command(opts))
 	return cmd
 }
