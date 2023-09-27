8144469c096445b9e2ce2300a1a7e84491b716ae
Samra10 <ahmadsamir1194@gmail.com>
2023-09-27 14:59:30 +0300

    add tests for getting OIDC configs

--- pkg/bootstrap/commands/oidc_test.go
+++ pkg/bootstrap/commands/oidc_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/alecthomas/assert"
+	"github.com/weaveworks/weave-gitops-enterprise/pkg/bootstrap/domain"
 )
 
 func TestGetIssuer(t *testing.T) {
@@ -22,4 +23,60 @@ func TestGetIssuer(t *testing.T) {
 
 	expectedIssuer := "https://example.com/issuer"
 	assert.Equal(t, expectedIssuer, issuer, "Expected issuer %s, got %s", expectedIssuer, issuer)
+}
+
+func TestGetOIDCSecrets(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		input  domain.OIDCConfigParams
+		expect domain.OIDCConfig
+		err    error
+	}{
+		{
+			name: "OIDCConfigParams with all fields",
+			input: domain.OIDCConfigParams{
+				DiscoveryURL: "https://dex-01.wge.dev.weave.works/.well-known/openid-configuration",
+				ClientID:     "client-id",
+				ClientSecret: "client-secret",
+				UserDomain:   "localhost",
+			},
+			expect: domain.OIDCConfig{
+				IssuerURL:    "https://dex-01.wge.dev.weave.works",
+				ClientID:     "client-id",
+				ClientSecret: "client-secret",
+				RedirectURL:  "http://localhost:8000/oauth2/callback",
+			},
+			err: nil,
+		},
+		{
+			name: "OIDCConfigParams with invalid DiscoveryURL",
+			input: domain.OIDCConfigParams{
+				DiscoveryURL: "https://dex-01.wge.dev.weave.works/.well-known/openid-configuration-invalid",
+				ClientID:     "client-id",
+				ClientSecret: "client-secret",
+				UserDomain:   "localhost",
+			},
+			expect: domain.OIDCConfig{
+				IssuerURL:    "https://dex-01.wge.dev.weave.works",
+				ClientID:     "client-id",
+				ClientSecret: "client-secret",
+				RedirectURL:  "http://localhost:8000/oauth2/callback",
+			},
+			err: fmt.Errorf("error: OIDC discovery URL returned status 404"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := getOIDCSecrets(tt.input)
+			if err != nil {
+				assert.NotNil(t, err)
+				return
+			}
+
+			assert.Equal(t, tt.expect, result)
+		})
+	}
+
 }
