d7a3c894325533c0e7dfd2a980c6c5725b548ba6
ahussein3 <ahmed.magdy@weave.works>
2023-11-13 12:37:09 +0200

    remove tanent and cluster coluymn from automated cluster discovery

--- ui/src/components/ClusterDiscovery/index.tsx
+++ ui/src/components/ClusterDiscovery/index.tsx
@@ -2,8 +2,11 @@ import { FC } from 'react';
 import { EnabledComponent } from '../../api/query/query.pb';
 import { useIsEnabledForComponent } from '../../hooks/query';
 import Explorer from '../Explorer/Explorer';
+import { defaultExplorerFields } from '../Explorer/ExplorerTable';
 import WarningMsg from '../Explorer/WarningMsg';
 import { Page } from '../Layout/App';
+
+const excludeFields = ['tenant', 'clusterName'];
 
 const ClusterDiscovery: FC = () => {
   const isExplorerEnabled = useIsEnabledForComponent(
@@ -19,7 +22,13 @@ const ClusterDiscovery: FC = () => {
       ]}
     >
       {isExplorerEnabled ? (
-        <Explorer category="clusterdiscovery" enableBatchSync={false} />
+        <Explorer
+          fields={defaultExplorerFields.filter(
+            f => !excludeFields.includes(f.id),
+          )}
+          category="clusterdiscovery"
+          enableBatchSync={false}
+        />
       ) : (
         <WarningMsg />
       )}
