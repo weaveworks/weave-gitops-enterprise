cc7700a5d33eb722ec3075c6c6ad5e69540640a9
Eneko Fernandez <eneko@weave.works>
2023-12-18 09:28:37 +0100

    added repo path to configuration

--- pkg/bootstrap/steps/flux.go
+++ pkg/bootstrap/steps/flux.go
@@ -1,9 +1,11 @@
 package steps
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
+	kustomizev1 "github.com/fluxcd/kustomize-controller/api/v1"
 	"github.com/weaveworks/weave-gitops-enterprise/pkg/bootstrap/utils"
 	"github.com/weaveworks/weave-gitops/pkg/logger"
 	"github.com/weaveworks/weave-gitops/pkg/runner"
@@ -27,6 +29,8 @@ type FluxConfig struct {
 	Scheme string
 	// Branch flux-system git repository branch
 	Branch string
+	// Path flux-system kustomization path to reconcile
+	Path string
 	// IsInstalled indicates whether flux is already installed
 	IsInstalled bool
 }
@@ -64,15 +68,35 @@ func NewFluxConfig(logger logger.Logger, client k8s_client.Client) (FluxConfig, 
 		return FluxConfig{}, fmt.Errorf("failed to parse flux repository: %v", err)
 	}
 
+	repoPath, err := getRepoPath(client, WGEDefaultRepoName, WGEDefaultNamespace)
+	if err != nil {
+		return FluxConfig{}, fmt.Errorf("failed to get flux repo path: %v", err)
+	}
+
 	logger.Successf("detected git scheme: %s", scheme)
 
 	return FluxConfig{
 		Url:    repoUrl,
 		Scheme: scheme,
 		Branch: repo.Spec.Reference.Branch,
+		Path:   repoPath,
 
 		IsInstalled: true,
 	}, nil
+}
+
+// getRepoPath get the path for flux installation (flux-system) Kustomization.
+func getRepoPath(client k8s_client.Client, repoName string, namespace string) (string, error) {
+	kustomization := &kustomizev1.Kustomization{}
+
+	if err := client.Get(context.Background(), k8s_client.ObjectKey{
+		Namespace: namespace,
+		Name:      repoName,
+	}, kustomization); err != nil {
+		return "", err
+	}
+
+	return kustomization.Spec.Path, nil
 }
 
 // VerifyFluxInstallation checks that Flux is present in the cluster. It fails in case not and returns next steps to install it.
